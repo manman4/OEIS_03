86733e7832de682f5e330515764d1411332bf580
manman4 <manchanr4@gmail.com>
2021-01-24 04:18:05 +0900

    Square array T(n,k), n >= 2, k >= 2, read by antidiagonals, where T(n,k) is the number of (undirected) Hamiltonian cycles on the n X k king graph.

---
+++ 339/339190/339190_01.py
@@ -0,0 +1,28 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_nXk_king_graph(n, k):
+    grids = []
+    for i in range(1, k + 1):
+        for j in range(1, n):
+            grids.append((i + (j - 1) * k, i + j * k))
+            if i < k:
+                grids.append((i + (j - 1) * k, i + j * k + 1))
+            if i > 1:
+                grids.append((i + (j - 1) * k, i + j * k - 1))
+    for i in range(1, k * n, k):
+        for j in range(1, k):
+            grids.append((i + j - 1, i + j))
+    return grids
+
+def A339190(n, k):
+    universe = make_nXk_king_graph(n, k)
+    GraphSet.set_universe(universe)
+    cycles = GraphSet.cycles(is_hamilton=True)
+    return cycles.len()
+
+cnt = 2
+for i in range(12 - 1):
+     for j in range(i + 1):
+         print(str(cnt) + ' ' + str(A339190(j + 2, i - j + 2)))
+         cnt += 1
