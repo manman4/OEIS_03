cc76d8a6fd08358c18f4da9b9047ad78c883ec83
manman4 <manchanr4@gmail.com>
2021-01-24 03:59:58 +0900

    Number of Hamiltonian circuits within parallelograms of size 7 X n on the triangular lattice.

---
+++ 339/339622/339622_01.py
@@ -0,0 +1,24 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_T_nk(n, k):
+    grids = []
+    for i in range(1, n + 1):
+        for j in range(1, k):
+            grids.append((i + (j - 1) * n, i + j * n))
+            if i > 1:
+                grids.append((i + (j - 1) * n, i + j * n - 1))
+    for i in range(1, k * n, n):
+        for j in range(1, n):
+            grids.append((i + j - 1, i + j))
+    return grids
+
+def A339849(n, k):
+    universe = make_T_nk(n, k)
+    GraphSet.set_universe(universe)
+    cycles = GraphSet.cycles(is_hamilton=True)
+    return cycles.len()
+
+for i in range(2, 151):
+    print(str(i) + ' ' + str(A339849(7, i)))
+
