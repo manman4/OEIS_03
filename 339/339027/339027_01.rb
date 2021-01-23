9f9fac9d9dbfdb4be33c21adfb7b1244f2ef672a
manman4 <manchanr4@gmail.com>
2021-01-24 05:10:23 +0900

    E.g.f.: exp(2 * (exp(x) - 1 - x - x^2 / 2 - x^3 / 6 - x^4 / 24)).

--- 339/339027/339027_01.rb
+++ 339/339027/339027_01.rb
@@ -2,6 +2,7 @@ def ncr(n, r)
   return 1 if r == 0
   (n - r + 1..n).inject(:*) / (1..r).inject(:*)
 end
+
 def A(n)
   ary = [1, 0, 0, 0, 0]
   (5..n).each{|i| ary << 2 * (5..i).inject(0){|s, j| s + ncr(i - 1, j - 1) * ary[-j]}}
