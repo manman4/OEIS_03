31f725f25e84db85b3d923e55f8ab87d73087af9
manman4 <manchanr4@gmail.com>
2021-01-24 03:48:49 +0900

    Number of (undirected) paths in the 2 X n king graph.

---
+++ 339/339750/339750_01.py
@@ -0,0 +1,37 @@
+# Using graphillion
+from graphillion import GraphSet
+
+def make_nXk_king_graph(n, k):
+    grids = []
+    for i in range(1, k + 1):
+        for j in range(1, n):
+            grids.append((i + (j - 1) * k, i + j * k))
+            if i < k:
+                grids.append((i + (j - 1) * k, i + j * k + 1))
+            if i > 1:
+                grids.append((i + (j - 1) * k, i + j * k - 1))
+    for i in range(1, k * n, k):
+        for j in range(1, k):
+            grids.append((i + j - 1, i + j))
+    return grids
+
+def A(start, goal, n, k):
+    universe = make_nXk_king_graph(n, k)
+    GraphSet.set_universe(universe)
+    paths = GraphSet.paths(start, goal)
+    return paths.len()
+
+def A307026(n, k):
+    m = k * n
+    s = 0
+    for i in range(1, m):
+        for j in range(i + 1, m + 1):
+            s += A(i, j, n, k)
+    return s
+
+def A339750(n):
+    return A307026(n, 2)
+
+# print([A339750(n) for n in range(1, 21)])
+for i in range(1, 51):
+    print(str(i) + ' ' + str(A339750(i)))
