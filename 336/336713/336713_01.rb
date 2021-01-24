d04af421aab35175b685f7cf64852a2cf3f702b0
manman4 <manchanr4@gmail.com>
2021-01-24 18:39:15 +0900

    a(0) = 1 and a(n) = (1/n) * Sum_{k=1..n} (-1)^(n-k) * binomial(n,k) * binomial(n+(n-1)*k,k-1) for n > 0.

---
+++ 336/336713/336713_01.rb
@@ -0,0 +1,28 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  return 1 if n == 0
+  (1..n).inject(0){|s, i| s + (-1) ** (n - i) * ncr(n, i) * ncr(n + (k - 1) * i, i - 1)} / n
+end
+
+def B(n)
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << A(i - j, j)
+    }
+  }
+  ary
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(i, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
