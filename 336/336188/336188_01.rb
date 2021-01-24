94f1fa1c7ba34253215ba6594e538f71cdb1d8cd
manman4 <manchanr4@gmail.com>
2021-01-24 17:51:50 +0900

    a(n) = Sum_{k=0..n} n^k * binomial(n,k)^n.

---
+++ 336/336188/336188_01.rb
@@ -0,0 +1,20 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n).inject(0){|s, i| s + ncr(n, i) ** k * k ** i}.to_i
+end
+
+
+n = 60
+(0..n).each{|i|
+  j = A(i, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
+
