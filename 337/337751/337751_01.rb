fce2ac0c77f72c0aa8ac4e60a56167225c8185ef
manman4 <manchanr4@gmail.com>
2021-01-24 15:49:23 +0900

    a(n) = n! * Sum_{k=0..floor(n/4)} (-1)^k / (n-4*k)!.

---
+++ 337/337751/337751_01.rb
@@ -0,0 +1,26 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  ary = [1]
+  a, b, c, d = 1, 1, 1, 1
+  cnt = 0
+  while cnt < n
+    cnt += 1
+    a, b, c, d = b, c, d, 1 - (cnt + 3) * (cnt + 2) * (cnt + 1) * cnt * a
+    ary << a
+  end
+  ary
+end
+
+n = 500
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
