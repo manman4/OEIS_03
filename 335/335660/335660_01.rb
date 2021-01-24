544281555370fe07dad33c8d67676c98a88a5a0e
manman4 <manchanr4@gmail.com>
2021-01-24 20:13:49 +0900

    a(n) = n - A334714(n).

---
+++ 335/335660/335660_01.rb
@@ -0,0 +1,36 @@
+require 'prime'
+
+def A(n)
+  ary = [0]
+  s = 0
+  (1..n).each{|i| 
+    s += 1 if i.prime?
+    ary << s
+  }
+  ary
+end
+
+def B(n)
+  ary = A(10 ** 7)
+  a = [0, 1]
+  b = [0, 1]
+  s = 0
+  t = 1
+  (2..n).each{|i|
+    s += a[i - 1]
+    j = ary[i] - ary[s]
+    a << j
+    t += j
+    b << t
+  }
+  b
+end
+n = 10000
+ary = B(n)
+(1..n).each{|i|
+  j = i - ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
