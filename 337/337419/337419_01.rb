d54fa44279b58ef99d51c98bb5665d945df1a0ae
manman4 <manchanr4@gmail.com>
2021-01-24 16:01:09 +0900

    Square array T(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of sqrt((1-(k+4)*x+sqrt(1+2*(k-4)*x+((k+4)*x)^2)) / (2 * (1+2*(k-4)*x+((k+4)*x)^2))).

---
+++ 337/337419/337419_01.rb
@@ -0,0 +1,28 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n).inject(0){|s, i| s + (-k) ** (n - i) * ncr(2 * i, i) * ncr(2 * n, 2 * i)}
+end
+
+def B(n)
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << A(i - j, j)
+    }
+  }
+  ary
+end
+
+n = 139
+ary = B(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
