e45c370e38c3cabc1af54fd38df1dbef2dabddf5
manman4 <manchanr4@gmail.com>
2021-01-24 15:52:23 +0900

    a(n) = n! * Sum_{k=0..floor(n/2)} (-1)^k / (n-2*k)!.

---
+++ 337/337749/337749_01.rb
@@ -0,0 +1,26 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  ary = [1]
+  a, b = 1, 1
+  cnt = 0
+  while cnt < n
+    cnt += 1
+    a, b = b, 1 - (cnt + 1) * cnt * a
+    ary << a
+  end
+  ary
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
