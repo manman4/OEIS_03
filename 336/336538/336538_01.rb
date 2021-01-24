f56c7c35c830b7bb6765ed7a996326bab714836c
manman4 <manchanr4@gmail.com>
2021-01-24 18:25:41 +0900

    G.f. A(x) satisfies: A(x) = 1 + x * A(x)^3 * (2 + A(x)).

---
+++ 336/336538/336538_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (0..n).inject(0){|s, i| s + 2 ** (n - i) * ncr(n, i) * ncr(3 * n + i + 1, n) / (3 * n + i + 1r)}.to_i
+end
+
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
