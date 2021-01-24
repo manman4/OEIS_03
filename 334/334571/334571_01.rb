1796dfbe04ca401c21d224ebf5c64615d1a9bbb4
manman4 <manchanr4@gmail.com>
2021-01-24 20:52:39 +0900

    E.g.f.: exp(-(x + x^2/2 + x^3/3 + x^4/4 + x^5/5)).

---
+++ 334/334571/334571_01.rb
@@ -0,0 +1,22 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (- f(i - 1) * (1..[i, k].min).inject(0){|s, j| s + ary[i - j] / f(i - j).to_r}).to_i
+  }
+  ary
+end
+
+m = 1000
+n = 500
+ary = A(5, m)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
