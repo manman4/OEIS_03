b0dd4287a77d07dc6eead24fa79263f98aa66b01
manman4 <manchanr4@gmail.com>
2021-01-24 23:16:47 +0900

    Number of directed Hamiltonian walks from NW to SW corners of a 9 X (2*n+1) grid.

--- 333/333605/333605_01.py
+++ 333/333605/333605_01.py
@@ -1,25 +1,16 @@
 # Using graphillion
-
 from graphillion import GraphSet
-
 import graphillion.tutorial as tl
 
 def A271592(n, k):
-
     if k == 1: return 1
-
     universe = tl.grid(k - 1, n - 1)
-
     GraphSet.set_universe(universe)
-
     start, goal = 1, n
-
     paths = GraphSet.paths(start, goal, is_hamilton=True)
-
     return paths.len()
 
 def A333605(n):
-
     return A271592(9, 2 * n + 1)
 
 for i in range(201):
