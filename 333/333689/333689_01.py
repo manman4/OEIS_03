11f865d5ce2982749162c4a7bb87640521144f7d
manman4 <manchanr4@gmail.com>
2021-01-24 23:30:07 +0900

    Number of self-avoiding paths in (2*n+1) X 5 grid starting the upper left corner, passing through the center of grid and finishing the lower right corner.

---
+++ 333/333689/333689_01.py
@@ -0,0 +1,21 @@
+# Using graphillion
+from graphillion import GraphSet
+import graphillion.tutorial as tl
+
+def A333685(n, k):
+    if n == 0 or k == 0: return 1
+    universe = tl.grid(2 * n, 2 * k)
+    GraphSet.set_universe(universe)
+    start, goal = 1, (2 * n + 1) * (2 * k + 1)
+    paths = GraphSet.paths(start, goal).including((start + goal) // 2)
+    return paths.len()
+
+def A333689(n):
+    return A333685(n, 2)
+
+cnt = 0
+for i in range(501):
+    print(str(cnt) + ' ' + str(A333689(i)))
+    cnt += 1
+
+
