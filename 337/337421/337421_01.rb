00cf63ef701d4ec07c1463a0359970141ce413d9
manman4 <manchanr4@gmail.com>
2021-01-24 16:00:06 +0900

    Expansion of sqrt((1-6*x+sqrt(1-4*x+36*x^2)) / (2 * (1-4*x+36*x^2))).

---
+++ 337/337421/337421_01.rb
@@ -0,0 +1,35 @@
+# def A(k, n)
+#   a = 1
+#   b = k + 2
+#   ary = [a]
+#   m = 1
+#   while m <= n
+#     m += 1
+#     a, b = b, ((4*m-3)*((4*k+16)*m*m-(6*k+24)*m+k+6)*b - (k-4)**2 * (m-1)*(4*m-1)*(2*m-3)*a)/(m*(2*m-1)*(4*m-5))
+#     ary << a
+#   end
+#   ary
+# end
+
+def A(n)
+  a = 1
+  b = 0
+  ary = [a]
+  m = 1
+  while m <= n
+    m += 1
+    a, b = b, ((4*m-3)*(8*m*m-12*m+4)*b - 36 * (m-1)*(4*m-1)*(2*m-3)*a)/(m*(2*m-1)*(4*m-5))
+    ary << a
+  end
+  ary
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
