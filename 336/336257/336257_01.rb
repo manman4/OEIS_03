e8784239f6d4dfe63319e84d9211e1db04b0b032
manman4 <manchanr4@gmail.com>
2021-01-24 18:04:40 +0900

    a(n) = Catalan(n) mod (2*n+1).

---
+++ 336/336257/336257_01.rb
@@ -0,0 +1,19 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  (ncr(2 * n, n) / (n + 1)) % (2 * n + 1)
+end
+
+
+n = 10000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
