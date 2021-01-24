915f5dc63b90ce60a66116501609804ca094df88
manman4 <manchanr4@gmail.com>
2021-01-24 15:28:48 +0900

    Expansion of Product_{k>0} (1 - 2^(k-1)*x^k).

---
+++ 337/337299/337299_01.rb
@@ -0,0 +1,26 @@
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s += a[j] * ary[-j]} / i}
+  ary
+end
+
+def A299786(n)
+  A(Array.new(n + 1, -1), [0] + (1..n).map{|i| 2 ** (i - 1)}, n)
+end
+
+n = 2000
+ary = A299786(n)
+(0..1000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
