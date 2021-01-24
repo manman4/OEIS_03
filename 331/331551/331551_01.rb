a6cd378decf7b391729c024ce29df4e0f0cf6340
manman4 <manchanr4@gmail.com>
2021-01-25 00:44:22 +0900

    Expansion of (1 + 3*x)/(1 + 2*x + 9*x^2)^(3/2).

---
+++ 331/331551/331551_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  ary = [1, 0]
+  (2..n).each{|i|
+    ary << (4 * (-i * i + 1) * ary[-1] - 9 * i * (2 * i + 1) * ary[-2]) / (i * (2 * i - 1))
+  }
+  ary
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
