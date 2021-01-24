179e11ab8dc480b7b0442eb04ab9313df0ffb9c9
manman4 <manchanr4@gmail.com>
2021-01-25 00:32:22 +0900

    a(n) = Sum_{i=1..n} d_3(i)*d_3(i+1)/3, where d_3(n) = A007425(n).

---
+++ 331/331081/331081_01.rb
@@ -0,0 +1,30 @@
+def A(n)
+  a = [0]
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += 1 if i % j == 0
+    }
+    a << s
+  }
+  ary = [0]
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += a[j] if i % j == 0
+    }
+  ary << s
+  }
+  ary
+end
+
+m = 10100
+n = 10000
+ary = A(m + 1)
+s = 0
+(1..n).each{|i|
+  s += ary[i] * ary[i + 1]
+  print i
+  print ' '
+  puts s / 3
+}
