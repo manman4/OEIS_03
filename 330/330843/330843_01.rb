e3c1edb6d6bf18b58a02a2fb346dd3f915fca1a9
manman4 <manchanr4@gmail.com>
2021-01-25 01:11:43 +0900

    Square array T(n,k) = [x^n] ((1+x)^(k+1) / (1-x)^(k-1))^n, n>=0, k>=0, read by descending antidiagonals.

---
+++ 330/330843/330843_01.rb
@@ -0,0 +1,28 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n).inject(0){|s, i| s + ncr((k + 1) * n, i) * ncr(k * n - i - 1, n - i)}
+end
+
+def A330843(n)
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << A(i - j, j)
+    }
+  }
+  ary
+end
+
+n = 139
+ary = A330843(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
