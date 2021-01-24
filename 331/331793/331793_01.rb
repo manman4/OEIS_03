2ce116d6f40b8a409c370544725207ab8c4a41ad
manman4 <manchanr4@gmail.com>
2021-01-25 00:51:46 +0900

    Expansion of ((1 - 5*x)/sqrt(1 - 10*x + 9*x^2) - 1)/(8*x^2).

---
+++ 331/331793/331793_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1, 2 * k]
+  (2..n).each{|i|
+    ary << (i + 1) * (k * (2 * i + 1) * ary[-1] - (k - 2) ** 2 * i * ary[-2]) / (i * (i + 2))
+  }
+  ary
+end
+
+n = 1000
+ary = A(5, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
