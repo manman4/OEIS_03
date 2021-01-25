34d890baf7fcdac7b2ea74251876ccba823576ad
manman4 <manchanr4@gmail.com>
2021-01-26 01:48:13 +0900

    "Sloping quaternary numbers": write numbers in quaternary under each other (right-justified), read diagonals in upward direction, convert to decimal.

---
+++ 325/325644/325644_01.rb
@@ -0,0 +1,11 @@
+def A(m, n)
+  ary = [0]
+  n.times{|i|
+    (m ** i - i..m ** (i + 1) - i - 2).each{|j|
+      ary << (0..i).inject(0){|s, k| s + (j + k).to_s(m)[-1 - k].to_i * m ** k}
+    }
+  }
+  ary
+end
+
+p A(4, 4)
