0c56c215fd36670886300c3b4af8cd55f6b06528
manman4 <manchanr4@gmail.com>
2021-01-26 00:53:28 +0900

    Expansion of Product_{k>=1} B(x^k), where B(x) is the g.f. of A003114.

---
+++ 327/327690/327690_01.rb
@@ -0,0 +1,46 @@
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def s(k, m, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == m}
+  s
+end
+
+def A(n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| -s(5, 1, i) - s(5, 4, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  ary
+end
+
+def B(n)
+  ary = A(n)
+  a = ary
+  (2..n).each{|i|
+    b = Array.new(n + 1, 0)
+    (0..n / i).each{|j| b[i * j] += ary[j]}
+    a = mul(a, b, n)
+  }
+  a
+end
+
+m = 1100
+n = 1000
+ary = B(m)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
