126d389d339cb9d49795c79ed5d13bc305b97a1f
manman4 <manchanr4@gmail.com>
2021-01-26 00:01:37 +0900

    a(n) = Sum_{i=0..n} (-1)^(n-i)*binomial(n,i)*Sum_{j=0..i} binomial(i,j)^n.

---
+++ 328/328811/328811_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n).inject(0){|s, i| s + (-1) ** (n - i) * ncr(n, i) * (0..i).inject(0){|t, j| t + ncr(i, j) ** k}}
+end
+
+n = 100
+(0..n).each{|i|
+  j = A(i, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
