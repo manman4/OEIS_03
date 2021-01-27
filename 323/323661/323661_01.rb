0b4867514586c3c745904cf12ed1d4c1262ff948
manman4 <manchanr4@gmail.com>
2021-01-27 22:55:45 +0900

    a(n) = Product_{k=0..n} (k^12 + (n-k)^12).

---
+++ 323/323661/323661_01.rb
@@ -0,0 +1,16 @@
+def A(k, n)
+  s = 1
+  (0..n).each{|i|
+    s *= i ** k + (n - i) ** k
+  }
+  s
+end
+
+n = 100
+(0..n).each{|i|
+  j = A(12, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
