b5e8ad03da47c5ff5b28b042dfba20b8ae7706da
manman4 <manchanr4@gmail.com>
2021-01-28 21:43:50 +0900

    Number of permutations sigma such that |sigma(i+j)-sigma(i)| >= 5 for 1 <= i <= n - j, 1 <= j <= 4.

---
+++ 322/322308/322308_01.rb
@@ -0,0 +1,35 @@
+def check(d, a, i)
+  return true if i == 0
+  j = 1
+  d_max = [i, d - 1].min
+  while (a[i] - a[i - j]).abs >= d && j < d_max
+    j += 1
+  end
+  (a[i] - a[i - j]).abs >= d
+end
+
+def solve(d, len, a = [])
+  b = []
+  if a.size == len
+    b << a
+  else
+    (1..len).each{|m|
+      s = a.size
+      if s == 0 || (s > 0 && !a.include?(m))
+        if check(d, a + [m], s)
+          b += solve(d, len, a + [m])
+        end
+      end
+    }
+  end
+  b
+end
+
+def A322308(n)
+  (0..n).map{|i| solve(5, i).size}
+end
+
+p A322308(25)
+# p solve(5, 26).size
+# p solve(5, 27).size
+# p solve(5, 28).size
