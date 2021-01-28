e6ce332a5869c7e989cd014bb5a414da286bd617
manman4 <manchanr4@gmail.com>
2021-01-28 21:56:43 +0900

    Square array A(n,k), n>=0, k>=0, read by antidiagonals, where A(n,k) is the constant term in the expansion of (Sum_{j=0..n} j*(x^j + x^(-j)))^k.

---
+++ 322/322549/322549_01.rb
@@ -0,0 +1,57 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  a = (1..n).to_a
+  ary = a.reverse + [0] + a
+  power(ary, k, 2 * k * n)[k * n]
+end
+
+def B(k, n)
+  (0..n).map{|i| A(k, i)}
+end
+
+def A322549(n)
+  a = []
+  (0..n).each{|i| a << B(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 5
+ary = A322549(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
+n = 20
+(1..10).each{|i|
+  p [i, B(i, n)]
+}
