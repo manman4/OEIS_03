4bff077b42072d89fc0b2b7b8af618af2a1c2260
manman4 <manchanr4@gmail.com>
2021-01-28 22:30:20 +0900

    Main diagonal of array in A322765.

---
+++ 322/322769/322769_01.rb
@@ -0,0 +1,39 @@
+def bell(n)
+  i = 1
+  bell = [1]
+  ary = [1, 1]
+  while i < n
+    next_bell = [bell[-1]]
+    # Bell triangle
+    i.times{|j| next_bell[j + 1] = next_bell[j] + bell[j]}
+    bell = next_bell
+    ary << bell[-1]
+    i += 1
+  end
+  ary
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  k = 0
+  a = [bell(2 * n)]
+  while k < n
+    k += 1
+    a << (0..2 * (n - k)).map{|i| (0..k - 1).inject(a[k - 1][i + 1] + a[k - 1][i + 2]){|s, j| s + ncr(k - 1, j) * a[j][i]} / 2}
+  end
+  a
+end
+
+n = 310
+ary = A(n)
+(0..200).each{|i|
+  j = ary[i][i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
