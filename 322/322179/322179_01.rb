44cc3746e5137f42b38549465c20d521c4c00c85
manman4 <manchanr4@gmail.com>
2021-01-28 21:20:35 +0900

    The number of permutations of {1,2,...,n,1,2,...,n} with the property that there are k or more numbers between the two k's in the set for k=1,...,n.

---
+++ 322/322179/322179_01.rb
@@ -0,0 +1,68 @@
+def search(cards, n, num)
+  if num == 0
+    @count += 1
+#p cards
+  else
+    (2 * n - 1).times{|i|
+      if cards[i] == 0
+        (i + num + 1..2 * n - 1).each{|j|
+          if cards[j] == 0
+            cards[i], cards[j] = num, num
+            search(cards, n, num - 1)
+            cards[i], cards[j] = 0, 0
+          end
+        }
+      end
+    }
+  end
+end
+
+def A(n)
+  cards = [0] * n * 2
+  @count = 0
+  search(cards, n, n)
+  @count
+end
+
+(1..2).each{|i| p A(i)}
+
+s, t = 0, 0
+(1..3).to_a.repeated_permutation(6){|i|
+  if i.count(1) == 2 && i.count(2) == 2 && i.count(3) == 2
+    s += 1
+    flag = true
+    (1..3).each{|k|
+      if k > i.rindex(k) - i.index(k) - 1
+        flag = false
+      end
+    }
+    if flag
+      t += 1
+      p [t, i]
+    else
+     # p i
+    end
+  end
+}
+p s
+
+
+s, t = 0, 0
+(1..4).to_a.repeated_permutation(8){|i|
+  if i.count(1) == 2 && i.count(2) == 2 && i.count(3) == 2 && i.count(4) == 2
+    s += 1
+    flag = true
+    (1..4).each{|k|
+      if k > i.rindex(k) - i.index(k) - 1
+        flag = false
+      end
+    }
+    if flag
+      t += 1
+      p [t, i]
+    else
+     # p i
+    end
+  end
+}
+p s
