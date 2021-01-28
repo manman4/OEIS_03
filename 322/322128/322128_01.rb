40ac17a6281e6007f02e0058f587c196771c6a73
manman4 <manchanr4@gmail.com>
2021-01-28 21:16:36 +0900

    Triangular array, read by rows: T(n,k) = denominator of binomial(n-1, n-k)/k!, 1 <= k <= n.

---
+++ 322/322128/322128_01.rb
@@ -0,0 +1,52 @@
+def C(n, r)
+  r = [r, n - r].min
+  return 1 if r == 0
+  return n if r == 1
+  numerator = (n - r + 1..n).to_a
+  denominator = (1..r).to_a
+  (2..r).each{|p|
+    pivot = denominator[p - 1]
+    if pivot > 1
+      offset = (n - r) % p
+      (p - 1).step(r - 1, p){|k|
+        numerator[k - offset] /= pivot
+        denominator[k] /= pivot
+      }
+    end
+  }
+  result = 1
+  (0..r - 1).each{|k|
+    result *= numerator[k] if numerator[k] > 1
+  }
+  return result
+end
+
+def mul(f_ary, b_ary)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary
+end
+
+def f(k)
+  ary = []
+  (1..k).each{|j|
+    ary << C(k - 1, k - j) / (1..j).inject(:*).to_r
+  }
+  ary
+
+end
+
+n = 140
+ary = (1..n).map{|i| f(i)}.flatten
+(1..ary.size).each{|i|
+  j = ary[i - 1].denominator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
