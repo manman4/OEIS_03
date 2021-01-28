f045e27e8a9d575754d72b6bdd0a82659c457b32
manman4 <manchanr4@gmail.com>
2021-01-28 21:42:29 +0900

    Number of permutations of [n] in which the length of every increasing run is 0 or 1 (mod 10).

---
+++ 322/322283/322283_02.rb
@@ -0,0 +1,30 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A(k, n)
+  a = (0..2 * k - 1).map{|i| (-1) ** (i % 2) * 1r / f(i)} + [0] * (n - 2 * k + 1)
+  b = I(a, n)
+  (0..n).map{|i| (b[i] * f(i)).to_i}
+end
+
+n = 600
+ary = A(5, n)
+(0..n).each{|i|
+   j = ary[i]
+   break if j.to_s.size > 1000
+   print i
+   print ' '
+   puts j
+}
