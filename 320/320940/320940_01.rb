bbe4a37948d05e5fdb50fb4e400b46d85bb95895
manman4 <manchanr4@gmail.com>
2021-01-31 05:08:34 +0900

    a(n) = Sum_{d|n} d*sigma_n(d).

---
+++ 320/320940/320940_01.rb
@@ -0,0 +1,34 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    s += i * sigma(n, i) if n % i == 0
+  }
+  s
+end
+
+n = 500
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
