d14f00f9f34688da0bee8aab7f1a07314fd4be5e
manman4 <manchanr4@gmail.com>
2021-01-30 22:39:22 +0900

    8-dimensional Catalan numbers.

---
+++ 321/321977/321977_01.rb
@@ -0,0 +1,19 @@
+def f(k, m)
+  return 1 if k * m == 0
+  s =(1..k * m).inject(:*)
+  (1..k).each{|i|
+    (1..m).each{|j|
+      s /= i + j - 1
+    }
+  }
+  s
+end
+
+n = 200
+(0..n).each{|i|
+  j = f(8, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
