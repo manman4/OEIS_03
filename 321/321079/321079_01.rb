1df64d3536990345535165bedf0aed237909ef67
manman4 <manchanr4@gmail.com>
2021-01-30 15:48:48 +0900

    Digits of one of the two 11-adic integers sqrt(5).

---
+++ 321/321079/321079_01.rb
@@ -0,0 +1,31 @@
+def A(x, y, p, q, base, n)
+  ary = [x]
+  s = x
+  n.times{|i|
+    m = base ** (i + 1)
+    (0..base - 1).each{|j|
+      k = j * m + s
+      if (q * k ** y - p) % (m * base) == 0
+        ary << j
+        s = k
+        break
+      end
+    }
+  }
+  ary
+end
+
+def A321079(n)
+  A(7, 2, 5, 1, 11, n)
+end
+
+m = 10100
+n = 10000
+ary = A321079(m)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
