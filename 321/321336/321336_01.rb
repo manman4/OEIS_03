13461af81d2207cf869e344b4a9ce35ed7a4ce15
manman4 <manchanr4@gmail.com>
2021-01-30 22:27:12 +0900

    Expansion of Product_{k>=0} (1 - x^(2^k))^(2^(k+1)).

---
+++ 321/321336/321336_01.rb
@@ -0,0 +1,74 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+def S(n)
+  a = [0, 1]
+  (2..n).each{|i|
+    s = 0
+    (1..i - 1).each{|j|
+      s += a[j] * (-1) ** ((i / j) % 2) if i % j == 0
+    }
+    a << s
+  }
+  a
+end
+
+def B(n)
+  ary1 = S(n).map{|i| -i}
+  ary2 = Array.new(n + 1, 1)
+  A(ary1, ary2, n)
+end
+
+def C(n)
+  ary = B(n)
+  a = Array.new(n + 1, 0)
+  a[0] = 1
+  (1..n).each{|i|
+    b = Array.new(n + 1, 0)
+    (0..n / i).each{|j| b[i * j] = ary[j]}
+    a = mul(a, b, n)
+  }
+  a
+end
+
+def D(n)
+  a = C(2 * n)
+  (0..n).map{|i| a[2 * i]}
+end
+
+n = 100
+p a = D(n)
+b = Array.new(n + 1, 0)
+(0..n / 2).each{|i| b[2 * i] = a[i]}
+p c = mul(b, [1, -1] + [0] * (n - 1), n)
+p d = mul(c, c, n)
+p a == d
+(0..5).each{|i|
+  j = a[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
