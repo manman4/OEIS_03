332c5e77c1afc9a20d1e4ba88c17b2322ca929c9
manman4 <manchanr4@gmail.com>
2021-01-30 21:01:08 +0900

    a(n) = [x^n] Product_{k>=1} 1/(1 - x^k)^tau_n(k), where tau_n(k) = number of ordered n-factorizations of k.

---
+++ 321/321191/321191_01.rb
@@ -0,0 +1,51 @@
+require 'prime'
+
+def t(ary, n)
+  a = [0] + ary
+  b = []
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += a[j] if i % j == 0
+    }
+    b << s
+  }
+  b
+end
+
+def tau(k, n)
+  ary = Array.new(n, 1)
+  (k - 1).times{
+    ary = t(ary, n)
+  }
+  ary
+end
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+def B(k, n)
+  ary1 = [0] + tau(k, n)
+  ary2 = Array.new(n + 1, 1)
+  a = A(ary1, ary2, n)
+end
+
+n = 500
+ary = [1] + (1..n).map{|i| B(i, i)[-1]}
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
