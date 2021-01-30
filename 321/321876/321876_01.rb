f405fdfe78fb6360bae9586cb7340922b5bce347
manman4 <manchanr4@gmail.com>
2021-01-30 23:24:04 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of Product_{j>=1} 1/(1 - x^j)^sigma_k(j).

---
+++ 321/321876/321876_01.rb
@@ -0,0 +1,58 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+def sigma(x, i)
+  return (1..i).count{|j| i % j == 0} if x == 0
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+def B(k, n)
+  ary1 = [0] + (1..n).map{|i| sigma(k, i)}
+  ary2 = Array.new(n + 1, 1)
+  A(ary1, ary2, n)
+end
+
+def A321876(n)
+  a = []
+  (0..n).each{|i| a << B(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 139
+ary = A321876(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
