e598e5d354e7d7fc0c563841561e6dc1692277e5
manman4 <manchanr4@gmail.com>
2021-01-31 00:52:27 +0900

    a(n) = Sum_{d divides n} (-1)^(d + n/d) * d^12.

---
+++ 321/321809/321809_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    s += (-1) ** ((i + n / i) % 2) * i ** k if n % i == 0
+  }
+  s
+end
+
+def B(k, n)
+  (1..n).map{|i| A(k, i)}
+end
+
+n = 10000
+ary = B(12, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
