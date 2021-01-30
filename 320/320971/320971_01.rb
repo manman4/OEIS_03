4f5fcf74ee0c6658af2eeb4edcd8baa29b46da2e
manman4 <manchanr4@gmail.com>
2021-01-31 04:02:31 +0900

    Expansion of Product_{k>=1} ((1 - x^k)/(1 + x^k))^(sigma(k)).

---
+++ 320/320971/320971_01.rb
@@ -0,0 +1,58 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+# x > 0
+def sigma(x, i)
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+n = 10000
+ary1 = [0] + (1..n).map{|i| - sigma(1, i)}
+ary2 = Array.new(n + 1, 1)
+a = A(ary1, ary2, n)
+ary3 = ary1.map{|i| -i}
+ary4 = Array.new(n + 1, -1)
+b = A(ary3, ary4, n)
+ary = mul(a, b, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
