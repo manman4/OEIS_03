1134a6d83946332551c8a93386eb5c42d863ccd0
manman4 <manchanr4@gmail.com>
2021-01-31 04:52:12 +0900

    a(1) = 1; a(n > 1) = Sum_{k = 1..n-1} Sum_{d|k, d < k} a(d).

---
+++ 320/320224/320224_01.rb
@@ -0,0 +1,17 @@
+def A(n)
+  ary = [0, 1]
+  (2..n).each{|i|
+    ary << (1..i - 1).inject(0){|s, j| s + ary[j] * ((i - 1) / j - 1)}
+  }
+  ary
+end
+
+n = 10000
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
