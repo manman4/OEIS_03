b0a85af46555b58e26f6484196875afed93b36ce
manman4 <manchanr4@gmail.com>
2021-01-30 21:58:45 +0900

    a(n) = Sum_{d|n} mu(d)*d^n.

---
+++ 321/321222/321222_01.rb
@@ -0,0 +1,24 @@
+require 'prime'
+
+def A008683(n)
+  ary = n.prime_division
+  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
+  0
+end
+
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    s += A008683(i) * i ** n if n % i == 0
+  }
+  s
+end
+
+n = 1000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
