f00f49623bc566a3198264eb650bfd8aee2af34d
manman4 <manchanr4@gmail.com>
2021-01-30 15:53:22 +0900

    Digits of one of the three 13-adic integers 5^(1/3) that is related to A320915.

---
+++ 321/321107/321107_01.rb
@@ -0,0 +1,31 @@
+def A(x, y, p, q, base, n)
+  ary = [x]
+  s = x
+  n.times{|i|
+    m = base ** (i + 1)
+    (0..base - 1).each{|j|
+      k = j * m + s
+      if (q * k ** y - p) % (m * base) == 0
+        ary << j
+        s = k
+        break
+      end
+    }
+  }
+  ary
+end
+
+def A321107(n)
+  A(8, 3, 5, 1, 13, n)
+end
+
+m = 10100
+n = 10000
+ary = A321107(m)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
