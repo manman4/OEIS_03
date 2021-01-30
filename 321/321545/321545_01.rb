ec5d29a10e47aaa4ee1360820233daad5af05b36
manman4 <manchanr4@gmail.com>
2021-01-31 00:21:13 +0900

    a(n) = Sum_{d|n} (-1)^(d-1)*d^6.

---
+++ 321/321545/321545_01.rb
@@ -0,0 +1,21 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    if n % i == 0
+      s += (-1) ** (i % 2 - 1)* i ** k
+    end
+  }
+  s
+end
+
+def B(k, n)
+  (1..n).map{|i| A(k, i)}
+end
+
+n = 10000
+ary = B(6, n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
