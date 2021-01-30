2301e23bda8c4458b78fdae9fb0a078437569aab
manman4 <manchanr4@gmail.com>
2021-01-31 04:29:55 +0900

    Number of partition into distinct parts (a_1, a_2, ... , a_m) (a_1 > a_2 > ... > a_m and Sum_{k=1..m} a_k = n) such that a1 - a2, a2 - a_3, ... , a_{m-1} - a_m are different.

---
+++ 320/320347/320347_01.rb
@@ -0,0 +1,25 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i - 1).map{|rest| [i, *rest]}}
+end
+
+def f(n)
+  a = []
+  partition(n, 1, n).each{|ary|
+    s = ary.size
+    ary1 = ary
+    ary2 = (1..s - 1).map{|i| ary1[i - 1] - ary1[i]}
+#p [ary, ary2]
+    t = ary2.size
+    ary2.uniq!
+    a << ary if ary2.size == t
+  }
+#p ''
+  a
+end
+
+(1..100).each{|i| 
+  print i
+  print ' '
+  puts f(i).size
+}
