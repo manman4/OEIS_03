9da751c9d0078d328e5df710417de02def9d2425
manman4 <manchanr4@gmail.com>
2021-01-31 14:27:23 +0900

    a(n) = [x^n] Product_{k>=1} (1 - x^k)/(1 - n*x^k).

---
+++ 319/319754/319754_01.rb
@@ -0,0 +1,28 @@
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def B(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + s(j[0], j[1], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s + a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def A(k, n)
+  ary1 = Array.new(n + 1, -1)
+  ary2 = Array.new(n + 1,  1)
+  ary3 = Array.new(n + 1, k)
+  B([[ary1, ary2], [ary2, ary3]], n)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i, i)[-1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
