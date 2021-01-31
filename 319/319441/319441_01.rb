e60228a42cdaf3a04c6a65e8c290f2fbf8856a97
manman4 <manchanr4@gmail.com>
2021-01-31 15:11:02 +0900

    Cubes of non-palindromic numbers.

---
+++ 319/319441/319441_01.rb
@@ -0,0 +1,17 @@
+def pl(n)
+  n.to_s.reverse.to_i == n
+end
+
+ary = []
+(1..10 ** 7).each{|i| 
+  j = i * i * i
+  if !pl(i)
+    ary << j
+  end
+  break if ary.size >= 10000
+}
+(1..10000).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
