078f6db2ec8d9b4e02c3c05d7c56b986e98f4a94
manman4 <manchanr4@gmail.com>
2021-01-31 15:19:54 +0900

    Digits of one of the three 7-adic integers 6^(1/3) that is related to A319199.

---
+++ 319/319555/319555_01.rb
@@ -0,0 +1,31 @@
+# x����J�n��y�悵��p/q
+def A(x, y, p, q, base, n)
+  ary = [x]
+  s = x
+  n.times{|i|
+    m = base ** (i + 1)
+    (0..base - 1).each{|j|
+      k = j * m + s
+      if (q * k ** y - p) % (m * base) == 0
+        ary << j
+        s = k
+        break
+      end
+    }
+  }
+  ary
+end
+
+def A319555(n)
+  A(6, 3, 6, 1, 7, n)
+end
+
+m = 10100
+ary = A319555(m)
+(0..10000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
