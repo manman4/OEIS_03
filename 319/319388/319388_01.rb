0914bf38d4ed2b39a10405bed32ecfacb9a055d7
manman4 <manchanr4@gmail.com>
2021-01-31 15:08:19 +0900

    Non-palindromic squares.

---
+++ 319/319388/319388_01.rb
@@ -0,0 +1,17 @@
+def pl(n)
+  n.to_s.reverse.to_i == n
+end
+
+ary = []
+(1..10 ** 7).each{|i| 
+  j = i * i
+  if !pl(j)
+    ary << j
+  end
+  break if ary.size >= 1000
+}
+(1..1000).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
