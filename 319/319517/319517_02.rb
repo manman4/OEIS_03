3e0898a17c97e79ee7e52a480135bd8b39725910
manman4 <manchanr4@gmail.com>
2021-01-31 15:12:08 +0900

    Expansion of Product_{1 <= i_1 <= i_2 <= i_3 <= i_4} (1 - x^(i_1*i_2*i_3*i_4)).

---
+++ 319/319517/319517_02.rb
@@ -0,0 +1,42 @@
+def S4(n)
+  cnt = 0
+  (1..n).each{|i|
+    (i..n / i).each{|j|
+      (j..n / (i * j)).each{|k|
+        if n % (i * j * k) == 0
+          cnt += 1 if n / (i * j * k) >= k
+        end
+      }
+    }
+  }
+  cnt
+end
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+def B4(n)
+  ary1 = (0..n).map{|i| -S4(i)}
+  ary4 = Array.new(n + 1, 1)
+  A(ary1, ary4, n)
+end
+
+n = 10100
+ary = B4(n)
+(0..10000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
