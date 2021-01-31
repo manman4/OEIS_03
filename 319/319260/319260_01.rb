6429e40be3219cc05d8b719f17e4d9afb84561f2
manman4 <manchanr4@gmail.com>
2021-01-31 19:17:19 +0900

    The 10-adic integer w = ...72890754 satisfying w^7 + 1 = x, x^7 + 1 = y, y^7 + 1 = z, and z^7 + 1 = w.

---
+++ 319/319260/319260_01.rb
@@ -0,0 +1,31 @@
+def f(k, m, a)
+  (a ** k + 1) % (10 ** m)
+end
+
+def A(k, m, n)
+  s = 0
+  a = m
+  while s < n
+    s += 1
+    (0..9).each{|i|
+      a0 = i * 10 ** s + a
+      4.times{a0 = f(k, s + 1, a0)}
+      if a0 == (i * 10 ** s + a)
+        a = a0
+        break
+      end
+    }
+  end
+  ary = a.to_s.split('').map(&:to_i)
+  ary.reverse + [0] * (n + 1 - ary.size)
+end
+
+m = 5050
+n = 5000
+ary = A(7, 4, m)
+(0..n).each{|i|
+  j = ary[i]
+  print i
+  print ' '
+  puts j
+}
