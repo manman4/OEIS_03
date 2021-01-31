5e51c2244909612993f09297e2de1e3c7fb95491
manman4 <manchanr4@gmail.com>
2021-01-31 14:29:33 +0900

    a(n) = A319651(n) + A038574(n).

---
+++ 319/319803/319803_01.rb
@@ -0,0 +1,13 @@
+def A(k, n)
+(0..n).map{|i| 
+  i.to_s(k).split('').sort.join.to_i(k) + i.to_s(k).split('').sort.reverse.join.to_i(k)
+}
+end
+
+n = 6560
+ary = A(3, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
