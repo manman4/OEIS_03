e3d7601b20b9f557944302fe0cb1701fe3a5fc6b
manman4 <manchanr4@gmail.com>
2021-02-02 08:29:03 +0900

    a(n) = 1 + 3 * Sum_{k=0..n-1} a(k) * a(n-k-1).

---
+++ 337/337167/337167_01.rb
@@ -0,0 +1,22 @@
+def A337167(n)
+  a, b = 1, 4
+  cnt = 1
+  ary = [1, 4]
+  while cnt < n
+    cnt += 1
+    a, b = b, (2 * (7 * cnt - 3) * b - 13 * (cnt - 1) * a) / (cnt + 1)
+    ary << b
+  end
+  ary
+end
+
+n = 1000
+ary = A337167(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
