f40c1866ca700d0236cf83eb79eb5717acc14f73
manman4 <manchanr4@gmail.com>
2021-02-03 22:03:12 +0900

    Square array T(n,k), n >= 0, k >= 0, read by antidiagonals, where T(n,k) = Sum_{j=0..n} k^j * j! * binomial(n,j)^2.

---
+++ 341/341014/341014_01.rb
@@ -0,0 +1,33 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n).inject(0){|s, i| s + ncr(n, i) ** 2 * k ** i * f(i)}
+end
+
+def B(n)
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << A(i - j, j)
+    }
+  }
+  ary
+end
+
+n = 139
+ary = B(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
