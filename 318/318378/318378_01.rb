919aa35f4e5cdbcb4a91a9d13f356671472a73ca
manman4 <manchanr4@gmail.com>
2021-02-06 23:58:59 +0900

    The 10-adic integer b = ...3944369978 satisfying b^3 + 1 = a, and a^3 + 1 = b.

---
+++ 318/318378/318378_01.rb
@@ -0,0 +1,32 @@
+def f(ary)
+  n = ary.size
+  a = (((ary.join.reverse.to_i) ** 3 + 1) % (10 ** n)).to_s.split('').reverse.map(&:to_i)
+  a += [0] * (n - a.size)
+  a
+end
+
+def A(n)
+  ary = [8]
+  while ary.size < n
+    (0..9).each{|i|
+      ary0 = (ary.clone + [i])
+      2.times{
+        ary0 = f(ary0)
+      }
+      if (ary.clone + [i]) == ary0
+        ary = ary0
+        break
+      end
+    }
+  end
+  ary
+end
+
+n = 1010
+ary = A(n + 1)
+(0..1000).each{|i|
+  j = ary[i]
+  print i
+  print ' '
+  puts j
+}
---
+++ 318/318378/318378_02.rb
@@ -0,0 +1,30 @@
+def f(k, m, a)
+  (a ** k + 1) % (10 ** m)
+end
+
+def A(k, m, n)
+  sa = 0
+  a = m
+  while sa < n
+    sa += 1
+    (0..9).each{|i|
+      a0 = i * 10 ** sa + a
+      2.times{a0 = f(k, sa + 1, a0)}
+      if a0 == (i * 10 ** sa + a)
+        a = a0
+        break
+      end
+    }
+  end
+  ary = a.to_s.split('').map(&:to_i)
+  ary.reverse + [0] * (n + 1 - ary.size)
+end
+
+m = 10010
+ary = A(3, 8, m)
+(0..10000).each{|i|
+  j = ary[i]
+  print i
+  print ' '
+  puts j
+}
