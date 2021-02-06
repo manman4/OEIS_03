c3cac5cd91b40ce7b77b0598e8d065c9c1842013
manman4 <manchanr4@gmail.com>
2021-02-06 23:51:41 +0900

    The 10-adic integer a_2 = ...9345703125000000002 satisfying a_2^9 + 1 = a_3, a_3^9 + 1 = a_4, ... , a_0^9 + 1 = a_1 and a_1^9 + 1 = a_2.

---
+++ 318/318381/318381_02.rb
@@ -0,0 +1,30 @@
+def f(k, m, a)
+  (a ** k + 1) % (10 ** m)
+end
+
+def A(k, m, n)
+  sa = 0
+  a = m
+  while sa < n
+    sa += 1
+    (0..9).each{|i|
+      a0 = i * 10 ** sa + a
+      10.times{a0 = f(k, sa + 1, a0)}
+      if a0 == (i * 10 ** sa + a)
+        a = a0
+        break
+      end
+    }
+  end
+  ary = a.to_s.split('').map(&:to_i)
+  ary.reverse + [0] * (n + 1 - ary.size)
+end
+
+n = 5010
+ary = A(9, 2, n)
+(0..5000).each{|i|
+  j = ary[i]
+  print i
+  print ' '
+  puts j
+}
