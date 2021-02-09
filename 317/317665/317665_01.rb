3e6112bffba46a469d874cb1dd67b2621e0b27cd
manman4 <manchanr4@gmail.com>
2021-02-10 00:57:28 +0900

    Expansion of 1/Sum_{k>=0} x^(k^2).

--- 317/317665/317665_01.rb
+++ 317/317665/317665_01.rb
@@ -2,7 +2,7 @@ def I(ary, n)
   a = [1]
   i = 0
   while i < n
-    a << -(0..i).inject(0){|s, j| s += ary[1 + i - j] * a[j]}
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
     i += 1
   end
   a
@@ -14,9 +14,10 @@ def A(n)
   I(ary, n)
 end
 
-n = 11000
-ary = A(n)
-(0..10000).each{|i|
+m = 11000
+n = 10000
+ary = A(m)
+(0..n).each{|i|
   j = ary[i]
   break if j.to_s.size > 1000
   print i
