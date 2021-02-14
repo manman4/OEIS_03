91c53c2edd9197fa659cab5610c6c407be718181
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-15 00:35:15 +0900

    Expansion of 1/(Sum_{k>=0} (k!)^3 x^k).

--- 316/316862/316862_01.rb
+++ 316/316862/316862_01.rb
@@ -7,7 +7,7 @@ def I(ary, n)
   a = [1]
   i = 0
   while i < n
-    a << -(0..i).inject(0){|s, j| s += ary[1 + i - j] * a[j]}
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
     i += 1
   end
   a
