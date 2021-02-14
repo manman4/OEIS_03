0712ede4fcabd3e2d6304a4329efed44c76f85e8
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-15 00:04:20 +0900

    Expansion of Product_{k>=1} (1 - x^k)^A000593(k).

---
+++ 316/316366/316366_01.rb
@@ -0,0 +1,41 @@
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+def f(n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % 2 == 1}
+  s
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+n = 100
+ary0 = Array.new(n + 1, 1)
+ary1 = (0..n).map{|i| f(i)}
+p A(ary1, ary0, n)
+ary =  I(A(ary1, ary0, n), n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
