97299c05424c62e5343b12fa9e2580beff63eb6a
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-15 00:24:21 +0900

    Inverse Euler transform of n^2.

---
+++ 316/316152/316152_01.rb
@@ -0,0 +1,35 @@
+require 'prime'
+
+def A008683(n)
+  ary = n.prime_division
+  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
+  0
+end
+
+# ary[0] = 1
+def inverse_Euler_transform(ary, n)
+  c = [0]
+  (1..n).each{|i| c << (1..i - 1).inject(i * ary[i]){|s, j| s - ary[j] * c[-j]}}
+  m_ary = [0] + (1..n).map{|i| A008683(i)}
+  a = [0]
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += m_ary[i / j] * c[j] if i % j == 0
+    }
+    # ary�̗v�f�������Ȃ�As / i�͐���
+    a << s / i
+  }
+  a
+end
+
+n = 1000
+a = (0..n).map{|i| i * i}
+ary = inverse_Euler_transform(a, n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
