90a04b415cbbf10a861f1ef3786ee4d20ec8ed18
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-15 00:19:28 +0900

    Expansion of 1/(1 + Sum_{k>=1} k^2 * x^k).

---
+++ 316/316087/316087_01.rb
@@ -0,0 +1,16 @@
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+n = 3000
+a = [1] + (1..n).map{|i| i * i}
+ary = I(a, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
