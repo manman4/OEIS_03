6b2f06bff821fb7ba9c5dee90520720a858b7c3e
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-23 00:29:28 +0900

    Digits of the 10-adic integer (987654321/(1-10^9))^(1/9).

---
+++ 309/309820/309820_01.rb
@@ -0,0 +1,31 @@
+# x����J�n��y�悵��p/q
+def A(x, y, p, q, base, n)
+  ary = [x]
+  s = x
+  n.times{|i|
+    m = base ** (i + 1)
+    (0..base - 1).each{|j|
+      k = j * m + s
+      if (q * k ** y - p) % (m * base) == 0
+        ary << j
+        s = k
+        break
+      end
+    }
+  }
+  ary
+end
+
+def A309820(n)
+  A(1, 9, -987654321, 10 ** 9 - 1, 10, n)
+end
+
+m = 10100
+ary = A309820(m)
+(0..10000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
