ec23abe82d95277758e3e7c7921529a77de19373
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-23 23:53:06 +0900

    Digits of the 10-adic integer (-61/9)^(1/3).

---
+++ 309/309643/309643_02.rb
@@ -0,0 +1,20 @@
+def A309643(n)
+  ary = [1]
+  a = 1
+  n.times{|i|
+    b = (a + 7 * (9 * a ** 3 + 61)) % (10 ** (i + 2))
+    ary << (b - a) / (10 ** (i + 1))
+    a = b
+  }
+  ary
+end
+
+n = 10000
+ary = A309643(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
