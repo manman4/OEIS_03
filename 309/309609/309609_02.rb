61bf83e30299870abbe0bfb854c54706248831e0
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-24 00:22:32 +0900

    Digits of the 10-adic integer (23/9)^(1/3).

---
+++ 309/309609/309609_02.rb
@@ -0,0 +1,20 @@
+def A309609(n)
+  ary = [3]
+  a = 3
+  n.times{|i|
+    b = (a + 3 * (9 * a ** 3 - 23)) % (10 ** (i + 2))
+    ary << (b - a) / (10 ** (i + 1))
+    a = b
+  }
+  ary
+end
+
+n = 10000
+ary = A309609(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
