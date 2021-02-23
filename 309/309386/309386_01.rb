fc64ebb8db92ab21b8d68f86c52f85fd4b2e7a49
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-24 00:10:51 +0900

    Square array A(n,k), n>=0, k>=0, read by antidiagonals, where A(n,k) = Sum_{j=0..n} (-k)^(n-j)*Stirling2(n,j).

---
+++ 309/309386/309386_01.rb
@@ -0,0 +1,31 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << (0..i - 1).inject(0){|s, j| s + (-k) ** (i - 1 - j) * ncr(i - 1, j) * ary[j]}}
+  ary
+end
+
+def A309386(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 10
+p ary = A309386(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
