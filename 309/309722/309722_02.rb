1737f476f3413553713f643b4994081f86295c43
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-23 20:17:14 +0900

    Digits of the 4-adic integer (1/3)^(1/3).

---
+++ 309/309722/309722_02.rb
@@ -0,0 +1,31 @@
+# x����J�n��y�悵��p/q
+def A(x, y, p, q, base, n)
+  ary = [x]
+  s = x
+  n.times{|i|
+    m = base ** (i + 1)
+    (0..base - 1).each{|j|
+      k = j * m + s
+      if (q * k ** y - p) % (m * base) == 0
+        ary << j
+        s = k
+        break
+      end
+    }
+  }
+  ary
+end
+
+def A309722(n)
+  A(3, 3, 1, 3, 4, n)
+end
+
+m = 10100
+ary = A309722(m)
+(0..10000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
