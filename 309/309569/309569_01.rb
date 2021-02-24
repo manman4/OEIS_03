37f61aeec4548599ad88a32ec2982b4f43a26b42
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-24 23:50:57 +0900

    Digits of the 10-adic integer (11/3)^(1/3).

---
+++ 309/309569/309569_01.rb
@@ -0,0 +1,30 @@
+# x����J�n��y�悵��p/q
+def A(x, y, p, q, base, n)
+  s = x
+  n.times{|i|
+    m = base ** (i + 1)
+    (0..base - 1).each{|j|
+      k = j * m + s
+      if (q * k ** y - p) % (m * base) == 0
+        s = k
+        break
+      end
+    }
+  }
+  s
+end
+
+def A309569(n)
+  str = A(3, 3, 11, 3, 10, n).to_s.reverse
+  (0..n).map{|i| str[i].to_i}
+end
+
+m = 10100
+ary = A309569(m)
+(0..10000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
