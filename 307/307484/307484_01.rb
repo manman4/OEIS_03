40294fad245603510039a04be508e0e9eed5a7b7
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-25 23:07:48 +0900

    Expansion of Product_{k>=1} 1/(1+x^k)^((-1)^k*k^2).

---
+++ 307/307484/307484_01.rb
@@ -0,0 +1,23 @@
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+def A307484(n)
+  A((0..n).map{|i| (-1) ** (i % 2) * i * i}, Array.new(n + 1, -1), n)
+end
+n = 10000
+ary = A307484(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
