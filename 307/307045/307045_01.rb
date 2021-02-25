913ac033adb154ba72a6f3c5faeba1e48206611d
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-25 23:54:30 +0900

    a(n) = Sum_{k=0..floor(n/9)} (-1)^k*binomial(n,9*k).

---
+++ 307/307045/307045_01.rb
@@ -0,0 +1,19 @@
+def A(n)
+  a, b, c, d, e, f, g, h = 1, 1, 1, 1, 1, 1, 1, 1
+  ary = [1, 1]
+  while ary.size <= n
+    a, b, c, d, e, f, g, h = b, c, d, e, f, g, h, 9 * h - 36 * g + 84 * f - 126 * e + 126 * d - 84 * c + 36 * b - 9 * a
+    ary << a
+  end
+  ary
+end
+
+n = 3000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
