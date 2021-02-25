a3b7fd2e039f699dfe161e2a395d9087d995d7b0
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-25 23:49:59 +0900

    Square array A(n,k), n >= 0, k >= 1, read by antidiagonals, where column k is the expansion of g.f. ((1-x)^(k-2))/((1-x)^k+x^k).

---
+++ 307/307079/307079_01.rb
@@ -0,0 +1,34 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n / k).inject(0){|s, i| s + (-1) ** i * ncr(n + 1, k * i + 1)}
+end
+
+def B(k, n)
+  (0..n).map{|i| A(k + 1, i)}
+end
+
+def A307079(n)
+  a = []
+  (0..n).each{|i| a << B(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 139
+ary = A307079(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
