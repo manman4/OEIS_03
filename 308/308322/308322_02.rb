f805af22fb3f8f9b8bb7a5dd91ee0e1219388de3
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-25 21:05:06 +0900

    A(n,k) = Sum_{i_1=0..n} Sum_{i_2=0..n} ... Sum_{i_k=0..n} (-1)^(i_1 + i_2 + ... + i_k) * multinomial(i_1 + i_2 + ... + i_k; i_1, i_2, ..., i_k), square array A(n,k) read by antidiagonals, for n >= 0, k >= 0.

---
+++ 308/308322/308322_02.rb
@@ -0,0 +1,51 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŽŸˆÈ‰º‚ðŽæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  ary = (0..n).map{|i| 1r / f(i)}
+  ary = power(ary, k, k * n)
+  (0..k * n).inject(0){|s, i| s + (-1) ** (i % 2) * f(i) * ary[i]}.to_i
+end
+
+def B(n)
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << A(i - j, j)
+    }
+  }
+  ary
+end
+
+n = 50
+ary = B(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
