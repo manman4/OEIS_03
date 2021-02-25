5d7be22a53c5ac520e183299bb0734de52701923
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-25 23:43:53 +0900

    a(n) = Sum_{0<=i<=j<=k<=n} (i+j+k)!/(i!*j!*k!).

---
+++ 307/307352/307352_01.rb
@@ -0,0 +1,28 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  s = 0
+  (0..n).each{|i|
+    (i..n).each{|j|
+      (j..n).each{|k|
+        s += f(i + j + k) / (f(i) * f(j) * f(k))
+      }
+    }
+  }
+  s
+end
+
+n = 20
+b=[]
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+b<<j
+}
+p b
