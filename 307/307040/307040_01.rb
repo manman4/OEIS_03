1bfaefbcb8c720726dd14683b50ea59ea76b7501
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-25 23:32:54 +0900

    a(n) = Sum_{k=0..floor(n/6)} (-1)^k*binomial(n,6*k).

---
+++ 307/307040/307040_01.rb
@@ -0,0 +1,19 @@
+def A(n)
+  a, b, c, d, e, f = 1, 1, 1, 1, 1, 1
+  ary = [1]
+  while ary.size <= n
+    a, b, c, d, e, f = b, c, d, e, f, 6 * f - 15 * e + 20 * d - 15 * c + 6 * b - 2 * a
+    ary << a
+  end
+  ary
+end
+
+n = 3000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
