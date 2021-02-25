1b9957ed22925e2bc052d150840991863b743f6b
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-25 21:57:20 +0900

    Expansion of Product_{k>=1} (1 - x^k*(1 - x))/(1 - x^k*(1 + x)).

---
+++ 307/307676/307676_01.rb
@@ -0,0 +1,47 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A(n)
+  ary1 = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(i + 2, 0)
+    b_ary[0], b_ary[-2], b_ary[-1] = 1, -1, 1
+    ary1 = mul(ary1, b_ary, n)
+  }
+  ary2 = [1]
+  (1..n).each{|i|
+    b_ary = Array.new(i + 2, 0)
+    b_ary[0], b_ary[-2], b_ary[-1] = 1, -1, -1
+    ary2 = mul(ary2, b_ary, n)
+  }
+  mul(ary1, I(ary2, n), n)
+end
+
+m = 1100
+ary = A(m)
+(0..1000).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
