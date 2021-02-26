4d279a3d082aff98c16edf42663a4477f7f1c32a
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 00:32:33 +0900

    Square array A(n,k), n>=0, k>=0, read by antidiagonals, where column k is the expansion of 1/(Sum_{j>=0} (j!)^k * x^j).

---
+++ 306/306629/306629_01.rb
@@ -0,0 +1,40 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A(k, n)
+  I((0..n).map{|i| f(i) ** k}, n)
+end
+
+def A306629(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 59
+ary = A306629(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
