08245187e190bac51b9a18ffc8642b6d5c568682
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-26 22:59:57 +0900

    Expansion of 1/((1 - x)^8 + x^8).

---
+++ 306/306941/306941_01.rb
@@ -0,0 +1,19 @@
+def A(n)
+  a, b, c, d, e, f, g, h = 1, 8, 36, 120, 330, 792, 1716, 3432
+  ary = [1]
+  while ary.size <= n
+    a, b, c, d, e, f, g, h = b, c, d, e, f, g, h, 8 * h - 28 * g + 56 * f - 70 * e + 56 * d - 28 * c + 8 * b - 2 * a
+    ary << a
+  end
+  ary
+end
+
+n = 3000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
