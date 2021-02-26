d1ebbcc5e34ca9ece3afc42b3dc1bd8601d75f23
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 00:48:08 +0900

    A(n,k) = binomial((2*k+1)*n+2, k*n+1)/((2*k+1)*n+2), square array A(n,k) read by antidiagonals, for n >= 0, k >= 0.

---
+++ 306/306444/306444_01.rb
@@ -0,0 +1,30 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n).map{|i| ncr((2 * k + 1) * i + 2, k * i + 1) / ((2 * k + 1) * i + 2)}
+end
+
+def A306444(n)
+  a = []
+  (0..n).each{|i| a << A(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 81
+ary = A306444(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
