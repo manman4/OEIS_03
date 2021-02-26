8b9af4086d42c7f4b749bb7d805d60d72e598bed
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 00:31:26 +0900

    a(n) = [x^n] 1/(Sum_{k=0..n} (k!)^n * x^k).

---
+++ 306/306630/306630_01.rb
@@ -0,0 +1,27 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A(k, n)
+  I((0..n).map{|i| f(i) ** k}, n)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i, i)[-1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
