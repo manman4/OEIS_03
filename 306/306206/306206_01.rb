063a1ac0f64781d72946c0d935e50c2732745ee5
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 01:07:52 +0900

    a(n) = Sum_{k=0..n} (n^2)!/((n^2-n*k)!*n!^k).

---
+++ 306/306206/306206_01.rb
@@ -0,0 +1,17 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  (0..n).inject(0){|s, i| s + f(n * n) / (f(n * n - n * i) * f(n) ** i)}
+end
+
+n = 100
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
