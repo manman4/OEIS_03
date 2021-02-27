647dc47fde39a32251cf0d0e6b0627bcb4633fa3
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-28 01:26:54 +0900

    Real part of Product_{k=1..n} (k+(n-k+1)i), with i=sqrt(-1).

---
+++ 302/302661/302661_01.rb
@@ -0,0 +1,16 @@
+def A(n)
+  a = 1 + n * 1i
+  (2..n).each{|i|
+    a *= i + (n - i + 1) * 1i
+  }
+  a
+end
+
+n = 400
+(0..n).each{|i|
+  j = A(i).real
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
