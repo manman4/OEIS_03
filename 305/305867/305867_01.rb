e8f852895e78744a096104a312e2b0881cd988a4
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 12:05:40 +0900

    Expansion of Product_{k>=1} 1/(1 - x^k)^(2*k-1)!!.

---
+++ 305/305867/305867_01.rb
@@ -0,0 +1,31 @@
+require 'prime'
+
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
+  ary
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).map{|i| 2 * i - 1}.inject(:*)
+end
+
+n = 20
+ary0 = Array.new(n + 1, 1)
+ary2 = (0..n).map{|i| f(i)}
+ary =  A(ary2, ary0, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
