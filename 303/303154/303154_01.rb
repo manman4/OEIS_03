69ee9fd0ac3e2aeca1acd844af0def8572d4b6b8
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-28 00:02:57 +0900

    Expansion of Product_{n>=1} (1 - (25*x)^n)^(1/5).

--- 303/303154/303154_01.rb
+++ 303/303154/303154_01.rb
@@ -7,11 +7,11 @@ end
 def A(f_ary, g_ary, n)
   ary = [1]
   a = [0] + (1..n).map{|i| s(f_ary, g_ary, i).to_i}
-  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s += a[j] * ary[-j]} / i}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + a[j] * ary[-j]} / i}
   ary
 end
 
-n = 500
+n = 50
 ary1 = (0..n).map{|i| -1 / 5r}
 ary2 = (0..n).map{|i| 25 ** i}
 ary = A(ary1, ary2, n)
