b7ad0d604a841a7ec9b5124ef9ea327e01d105fe
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-28 01:06:14 +0900

    Numbers k such that the coefficient of x^k in the expansion of Product_{j>=1} (1 - x^j)^4 is zero.

---
+++ 302/302056/302056_01.rb
@@ -0,0 +1,21 @@
+def s(n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0}
+  s
+end
+
+def A(m, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - m * a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 30000
+ary = A(4, n)
+a = (0..n).select{|i| ary[i] == 0}
+(1..10000).each{|i|
+  print i
+  print ' '
+  puts a[i - 1]
+}
