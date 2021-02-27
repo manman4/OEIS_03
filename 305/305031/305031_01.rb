ea5a0ca829ff3df62a2b8790c912bfae1827d234
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 12:41:59 +0900

    Expansion of ((1 + 2*x)/(1 - 2*x))^(3/2).

---
+++ 305/305031/305031_01.rb
@@ -0,0 +1,21 @@
+def A(k, m, n)
+  a, b = 1, 2 * m
+  ary = [1]
+  cnt = 0
+  while cnt < n
+    a, b = b, (2 * m * b + k * k * cnt * a) / (cnt + 2)
+    ary << a
+    cnt += 1
+  end
+  ary
+end
+
+n = 3000
+ary = A(2, 3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
