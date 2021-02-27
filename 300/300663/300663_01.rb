bf0e704a13f162535564fb31b462100e3173c670
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-28 05:07:54 +0900

    Expansion of 1/(1 - Sum_{k>=1} mu(k)*x^k), where mu() is the Moebius function (A008683).

---
+++ 300/300663/300663_01.rb
@@ -0,0 +1,28 @@
+require 'prime'
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A008683(n)
+  ary = n.prime_division
+  return (-1) ** (ary.size % 2) if ary.all?{|i| i[1] == 1}
+  0
+end
+
+n = 50
+ary0 = [1] + (1..n).map{|i| -A008683(i)}
+ary = I(ary0, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
