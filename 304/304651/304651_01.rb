c6541f39f6cd9e61adcf3432bb95e71fe74436c1
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 14:55:44 +0900

    Number of coprime pairs (x,y) with x^2 + y^2 <= n.

---
+++ 304/304651/304651_01.rb
@@ -0,0 +1,29 @@
+def A(n)
+  n1 = Math.sqrt(n).to_i
+  a = Array.new(n + 1, 0)
+  (-n1).upto(n1){|i|
+    (-n1).upto(n1){|j|
+      if i.gcd(j) == 1
+        k = i * i + j * j
+        if k <= n
+          a[k] += 1
+        end
+      end
+    }
+  }
+  b = []
+  s = 0
+  (0..n).each{|i|
+    s += a[i]
+    b << s
+  }
+  b
+end
+
+n = 20000
+ary = A(n)
+(0..10000).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
