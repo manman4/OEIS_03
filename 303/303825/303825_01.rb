7cab5a06c0fce64a32997cfcd7faa11438ee3d5b
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 21:52:02 +0900

    Number of ways of writing n as a sum of powers of 7, each power being used at most seven times.

---
+++ 303/303825/303825_01.rb
@@ -0,0 +1,17 @@
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    s = ary[i / k]
+    s += ary[i / k - 1] if i % k == 0
+    ary << s
+  }
+  ary
+end
+
+n = 10000
+ary = A(7, n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
