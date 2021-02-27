29d482b6d3f9596ce087273fe6f5fb6ead77f4e5
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-28 01:45:05 +0900

    Hurwitz inverse of [1 followed by primes], [1,2,3,5,7,...].

---
+++ 302/302194/302194_01.rb
@@ -0,0 +1,39 @@
+require 'prime'
+
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def I(ary, n)
+  a = [1]
+  (0..n - 1).each{|i| a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}}
+  a
+end
+
+def A(n)
+  p_ary = [1] + Prime.each.take(n).to_a
+  I((0..n).map{|i| p_ary[i].to_r / f(i)}, n)
+end
+
+n = 500
+ary = A(n)
+(0..n).each{|i|
+  j = (ary[i] * f(i)).to_i
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
