a82ed62dc59be38deeb782998cf54ad33ddc03f9
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-28 05:16:33 +0900

    a(n) is the decimal expansion of A300557(n).

--- 300/300578/300578_01.rb
+++ 300/300578/300578_01.rb
@@ -1,9 +1,9 @@
-def A300558(n)
+def A300557(n)
   a = '1'
   [1] + (2..n).map{|i| a = (a.to_s + i.to_s(2) * i).to_i}
 end
 n = 50
-ary = A300558(n).map{|i| i.to_s.to_i(2)}
+ary = A300557(n).map{|i| i.to_s.to_i(2)}
 (1..n).each{|i|
   j = ary[i - 1]
   break if j.to_s.size > 1000
