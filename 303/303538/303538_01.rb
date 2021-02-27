c4a5197c5ffc2a49615a00afd915a85e6a957000
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 22:54:37 +0900

    Expansion of ((1 + 8*x)/(1 - 8*x))^(1/8).

---
+++ 303/303538/303538_01.rb
@@ -0,0 +1,35 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# ary[0] = 1
+def sqrt_a(ary)
+  n = ary.size - 1
+  a = [1]
+  (0..n - 1).each{|i| a << (ary[i + 1] - (1..i).inject(0){|s, j| s + a[j] * a[-j]}) / 2}
+  a
+end
+
+def A(n)
+  a = [1, 8] + [0] * (n - 1)
+  b = (0..n).map{|i| 8 ** i}
+  sqrt_a(sqrt_a(sqrt_a(mul(a, b, n))))
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i].to_i
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
