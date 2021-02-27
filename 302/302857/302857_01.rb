5cdfd18bd310e3d80dbc3a9fc0da2c1eeca4f09a
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-28 01:04:16 +0900

    Expansion of ((1 + 2 * Sum_{k>=1} q^(k^2))^32 - 1) / 64.

---
+++ 302/302857/302857_01.rb
@@ -0,0 +1,36 @@
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# mŸˆÈ‰º‚ğæ‚èo‚·
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  ary = [1] + Array.new(n, 0)
+  (1..Math.sqrt(n).to_i).each{|i| ary[i * i] = 2}
+  power(ary, k, n)
+end
+
+n = 10000
+ary = A(32, n)
+(1..n).each{|i|
+  j = ary[i] / 64
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
