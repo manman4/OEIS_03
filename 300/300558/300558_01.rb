360cc20c7f9645a47217b953c509a3efa21bc84a
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-28 05:20:22 +0900

    a(n) is the concatenation n written in base 2 n times, n-1 written in base 2 n-1 times, ..., 1 written in base 2 once.

---
+++ 300/300558/300558_01.rb
@@ -0,0 +1,13 @@
+def A300558(n)
+  a = '1'
+  [1] + (2..n).map{|i| a = (i.to_s(2) * i + a.to_s).to_i}
+end
+n = 100
+ary = A300558(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
