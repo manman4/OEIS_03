3ceb7dd4045e92a07193a3e59e73f78f48e06145
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 22:32:12 +0900

    a(n) = [x^n] ((Sum_{k=0..n} (k+7)!*x^k)/(Sum_{k=0..n} (k+7)!*(-x)^k))^(1/8).

---
+++ 303/303569/303569_01.rb
@@ -0,0 +1,50 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+# ary[0] = 1
+def sqrt_a(ary)
+  n = ary.size - 1
+  a = [1]
+  (0..n - 1).each{|i| a << (ary[i + 1] - (1..i).inject(0){|s, j| s + a[j] * a[-j]}) / 2}
+  a
+end
+
+def A(n)
+  a = (0..n).map{|i| f(i + 7) / 5040}
+  b = (0..n).map{|i| (-1) ** (i % 2) * a[i]}
+  sqrt_a(sqrt_a(sqrt_a(mul(a, I(b, n), n))))
+end
+
+n = 50
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i].to_i
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
