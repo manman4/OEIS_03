ce5d500aaba812e78766ded27b6fdf59dff94c25
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 12:40:21 +0900

    Expansion of 1/2 * (((1 + 8*x)/(1 - 8*x))^(1/8) - 1).

---
+++ 305/305609/305609_01.rb
@@ -0,0 +1,22 @@
+def A(k, m, n)
+  a, b = 0, m
+  ary = [0]
+  cnt = 0
+  while cnt < n
+    a, b = b, (2 * m * b + k * k * cnt * a) / (cnt + 2r)
+    a = a.to_i if a.denominator == 1
+    ary << a
+    cnt += 1
+  end
+  ary
+end
+
+n = 1000
+ary = A(8, 1, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
