158dfaafd8cbb231e9a9ed15d3e92b05642219a5
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 15:35:22 +0900

    Number of partitions of n in which the sequence of the sum of the same summands is decreasing.

---
+++ 304/304430/304430_01.rb
@@ -0,0 +1,34 @@
+# ˜aˆöq‚ÍminˆÈãmaxˆÈ‰º
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+
+s_ary = []
+(1..20).each{|i|
+  cnt = 0
+  partition(i, 1, i).each{|ary|
+    a = ary.group_by{|i| i}.sort
+    m = a[0][1].inject(:+)
+    flag = true
+p_ary = [m]
+    if a.size > 1
+      (a[1..-1]).each{|b|
+        mb = b[1].inject(:+)
+        if m > mb
+          m = mb
+        else
+          flag = false
+        end
+p_ary << m
+      }
+    end
+    if flag
+p [a, p_ary]
+      cnt += 1
+    end
+  }
+  s_ary << cnt
+p [i, cnt]
+}
+p s_ary
