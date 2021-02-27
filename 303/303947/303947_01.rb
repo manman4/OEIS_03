adcdb66d2949313d3b6a4a33301b79ee3b8fdcb1
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 21:43:32 +0900

    Number of partitions of n into at most 1 copy of 2^2, 2 copies of 3^2, 3 copies of 4^2, ... .

---
+++ 303/303947/303947_01.rb
@@ -0,0 +1,57 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A(n)
+  ary = [1]
+  s = 0
+  (1..n).each{|i|
+    i2 = i * i
+    break if i2 > n
+    s += i2
+    b_ary = Array.new(i2 + 1, 0)
+    b_ary[0], b_ary[-1] = 1, -1
+    ary = mul(ary, b_ary, s)
+  }
+  ary[0..n]
+end
+
+def B(n)
+  ary = [1]
+  s = 0
+  (1..n).each{|i|
+    i3 = i * i * i
+    break if i3 > n
+    s += i3
+    b_ary = Array.new(i3 + 1, 0)
+    b_ary[0], b_ary[-1] = 1, -1
+    ary = mul(ary, b_ary, s)
+  }
+  ary[0..n]
+end
+
+n = 1000
+ary = mul(I(A(n), n), B(n), n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
