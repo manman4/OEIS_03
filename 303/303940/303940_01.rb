b95c7ab4495659215465de0b4d51b059f700746e
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 21:45:55 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals, where column k is the expansion of Product_{j>=1} (1-x^(j*(j+k)))/(1-x^j). in powers of x.

---
+++ 303/303940/303940_01.rb
@@ -0,0 +1,62 @@
+# m���ȉ������o��
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+def s(k, n)
+  s = 0
+  (1..n).each{|i| s += i if n % i == 0 && i % k == 0}
+  s
+end
+
+def A(ary, n)
+  a_ary = [1]
+  a = [0] + (1..n).map{|i| ary.inject(0){|s, j| s + j[1] * s(j[0], i)}}
+  (1..n).each{|i| a_ary << (1..i).inject(0){|s, j| s - a[j] * a_ary[-j]} / i}
+  a_ary
+end
+
+def B(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    j = i * (i + k)
+    break if j > n
+    b_ary = Array.new(j + 1, 0)
+    b_ary[0], b_ary[-1] = 1, -1
+    ary = mul(ary, b_ary, n)
+  }
+  ary
+end
+
+def C(k, n)
+  mul(A([[1, -1]], n), B(k, n), n)
+end
+
+def A303940(n)
+  a = []
+  (0..n).each{|i| a << C(i, n - i)}
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << a[i - j][j]
+    }
+  }
+  ary
+end
+
+n = 139
+ary = A303940(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
