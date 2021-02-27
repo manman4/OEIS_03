931131cdfb5e453718fa360e70c00f63619ed0a1
manman4 <7023566+manman4@users.noreply.github.com>
2021-02-27 22:58:42 +0900

    Expansion of Product_{k>=1} ((1 + (8^k*x)^k)/(1 - (8^k*x)^k))^(1/8^k).

---
+++ 303/303445/303445_01.rb
@@ -0,0 +1,29 @@
+def s(f_ary, g_ary, n)
+  s = 0
+  (1..n).each{|i| s += i * f_ary[i] * g_ary[i] ** (n / i) if n % i == 0}
+  s
+end
+
+def A(f_ary, g_ary, n)
+  ary = [1]
+  a = [0] + (1..n).map{|i| s(f_ary, g_ary, i)}
+  b = [0] + (1..n).map{|i| s(f_ary.map{|i| -i}, g_ary.map{|i| -i}, i)}
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + (a[j] + b[j]) * ary[-j]} / i}
+  ary
+end
+
+def B(n)
+  ary1 = (0..n).map{|i| 1r / 8 ** i}
+  ary2 = (0..n).map{|i| 8 ** (i * i)}
+  ary = A(ary1, ary2, n)
+end
+
+n = 50
+ary = B(n)
+(0..n).each{|i|
+  j = ary[i].to_i
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
