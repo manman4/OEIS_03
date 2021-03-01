da6a0cfc0d629a7d404c6c08c41dd8ba6bb6274a
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-02 01:17:12 +0900

    Square array T(n,k), n>=0, k>=0, read by antidiagonals, where column k is the expansion of g.f. 1/(1 - 2*k*x + k*x^2).

---
+++ 342/342133/342133_01.rb
@@ -0,0 +1,28 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n / 2).inject(0){|s, i| s + (2 * k) ** (n - i) * (-1 / 2r) ** i * ncr(n - i, i)}.to_i
+end
+
+def B(n)
+  ary = []
+  (0..n).each{|i|
+    (0..i).each{|j|
+      ary << A(i - j, j)
+    }
+  }
+  ary
+end
+
+n = 139
+ary = B(n)
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
