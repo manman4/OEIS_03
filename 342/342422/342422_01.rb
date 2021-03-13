642e1c202e131097340f9f7acbce6f4d0550e5ee
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-14 00:35:12 +0900

    a(n) = Sum_{k=1..n} (n/gcd(k,n))^gcd(k,n).

---
+++ 342/342422/342422_01.rb
@@ -0,0 +1,13 @@
+def A(n)
+  (1..n).inject(0){|s, i| s + (n / (i.gcd(n))) ** (i.gcd(n))}
+end
+
+n = 5000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+  
