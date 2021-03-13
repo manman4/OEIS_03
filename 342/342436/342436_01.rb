69d67f09abf82e046764ad7a35e907b838c5df85
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-14 00:38:24 +0900

    a(n) = Sum_{k=1..n} gcd(k,n)^(gcd(k,n) - 1).

---
+++ 342/342436/342436_01.rb
@@ -0,0 +1,13 @@
+def A(n)
+  (1..n).inject(0){|s, i| s + (i.gcd(n)) ** (i.gcd(n) - 1)}
+end
+
+n = 500
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+  
