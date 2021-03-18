047b7e2e92551120ed77bba3efc73a8e827185c1
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-18 20:38:03 +0900

    a(n) = Sum_{d|n} phi(d)^(n-d+1).

---
+++ 342/342617/342617_01.rb
@@ -0,0 +1,23 @@
+require 'prime'
+
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def A(n)
+  (1..n).inject(0){|s, i| s + phi(n / (i.gcd(n))) ** (n - n / (i.gcd(n)))}
+end
+
+n = 1000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  # FORMURLA確認
+  if i.prime?
+    k = i
+    break if j != k
+  end
+  print i
+  print ' '
+  puts j
+}
