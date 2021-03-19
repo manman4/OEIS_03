83affb182092c6f2f93c194fd10e096f2d24c167
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-19 19:24:54 +0900

    a(n) = Sum_{d|n} phi(d)^(n-d).

--- 342/342607/342607_01.rb
+++ 342/342607/342607_01.rb
@@ -5,10 +5,18 @@ def phi(n)
 end
 
 def A(n)
+  # 計算途中に分数が出るが、最終的に整数値
   (1..n).inject(0){|s, i| s + phi(n / (i.gcd(n))) ** (n - n / (i.gcd(n)) - 1)}.to_i
+  
+  # s = 0
+  # (1..n).each{|i|
+  #   p s
+  #   s += phi(n / (i.gcd(n))) ** (n - n / (i.gcd(n)) - 1)
+  # }
+  # s
 end
 
-n = 700
+n = 10
 (1..n).each{|i|
   j = A(i)
   break if j.to_s.size > 1000
