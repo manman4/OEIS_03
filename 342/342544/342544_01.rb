7862bfbc9c6da0f5de2ab25309b566e5ad66de3d
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-19 18:14:20 +0900

    a(n) = Sum_{k=1..n} phi(gcd(k, n))^(gcd(k, n) - 1).

---
+++ 342/342544/342544_01.rb
@@ -0,0 +1,23 @@
+require 'prime'
+
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def A(n)
+  (1..n).inject(0){|s, i| s + phi(i.gcd(n)) ** (i.gcd(n) - 1)}
+end
+
+n = 400
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  # FORMURLA確認
+  if i.prime?
+    k = i - 1 + (i - 1) ** (i - 1)
+    break if j != k
+  end
+  print i
+  print ' '
+  puts j
+}
