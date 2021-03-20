ba7642b6294e916d45b803bf361275c89da13b32
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-20 15:53:33 +0900

    a(n) = Sum_{k=1..n} phi(gcd(k, n))^2.

---
+++ 342/342534/342534_01.rb
@@ -0,0 +1,17 @@
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def A(n)
+  (1..n).inject(0){|s, i| s + phi(i.gcd(n)) ** 2}
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
