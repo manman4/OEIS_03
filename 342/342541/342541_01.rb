b69fc611affedf0823a0eb7ce445583e6bfff0e9
manman4 <7023566+manman4@users.noreply.github.com>
2021-03-20 15:37:53 +0900

    a(n) = Sum_{k=1..n} phi(gcd(k, n))^(n/gcd(k, n)).

---
+++ 342/342541/342541_01.rb
@@ -0,0 +1,24 @@
+require 'prime'
+
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def A(n)
+  (1..n).inject(0){|s, i| s + phi(i.gcd(n)) ** (n / (i.gcd(n)))}
+end
+
+n = 5000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  # FORMURLA確認
+  if i.prime?
+    k = 2 * (i - 1)
+    break if j != k
+  end
+  print i
+  print ' '
+  puts j
+}
+
