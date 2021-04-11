c82e21d5cefc8adcd2749c5353bf7c621a11a603
manman4 <manchanr4@gmail.com>
2021-04-11 22:17:20 +0900

    a(n) = 2 * T(n,(n+1)/2) where T(n,x) is a Chebyshev polynomial of the first kind.

---
+++ 343/343260/343260_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  return 2 if n == 0
+  (2 * n * (0..n).inject(0){|s, i| s + (n - 1) ** i * ncr(n + i, 2 * i) / (n + i.to_r)}).to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
