d3ef1bbedb89d56faec4ccc5da7ba9644184e971
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-19 23:35:24 +0900

    a(n) = n * Sum_{d|n} binomial(d+n-2,n-1)/d.

---
+++ 343/343547/343547_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# Expansion of Sum_{j>=1} j*x^j/(1 - x^j)^k
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    s += ncr(i + k - 2, k - 1) * (n / i)if n % i == 0
+  }
+  s
+end
+
+n = 30
+(1..n).each{|i|
+  j = A(i, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
