fbd0c97b3952b9cda7329f49838a97c2dea18b8e
manman4 <7023566+manman4@users.noreply.github.com>
2021-04-26 20:17:47 +0900

    a(n) = Sum_{d|n, d odd, d <= n/d} binomial(n/d, d).

---
+++ 340/340625/340625_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    # n / i < i のときは0
+    s += (i % 2) * ncr(n / i, i) if n % i == 0
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts A(i)
+}
