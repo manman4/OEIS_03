3c88968e38c8d1f031eba714d24309b9a3a6ff95
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-02 18:03:00 +0900

    a(n) = (n!)^n * Sum_{k=0..n} 1 / (k!)^n.

---
+++ 336/336247/336247_01.rb
@@ -0,0 +1,18 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  m = f(n) ** n
+  (0..n).inject(0){|s, i| s + m / f(i) ** n}
+end
+
+n = 100
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
