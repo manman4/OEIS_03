5e75775cd151c34c5da5a6972eaf016105308c15
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-15 23:18:40 +0900

    Array read by antidiagonals where A(n,k) is the number of divisors of n^k.

---
+++ 343/343656/343656_01.rb
@@ -0,0 +1,32 @@
+require './linear'
+
+require 'prime'
+
+def d(n)
+  return 1 if n == 1
+  n.prime_division.inject(1){|s, i| s * (i[1] + 1)}
+end
+
+def T(n, k)
+  d(n ** k)
+end
+
+def A343656(n)
+  ary = []
+  (1..n).each{|i|
+    (1..i).each{|j|
+      ary << T(j, i - j)
+    }
+  }
+  ary
+end
+
+n = 140
+ary = A343656(n)
+(1..ary.size).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
