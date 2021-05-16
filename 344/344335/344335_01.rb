728f6bcb5227650c3613aa709e5403a6af3f6ba9
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-16 20:32:20 +0900

    Number of divisors of n^8.

---
+++ 344/344335/344335_01.rb
@@ -0,0 +1,14 @@
+require 'prime'
+
+def tau(n)
+  n.prime_division.inject(1){|s, i| s * (i[1] + 1)}
+end
+
+n = 10000
+(1..n).each{|i|
+  j = tau(i ** 8)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
