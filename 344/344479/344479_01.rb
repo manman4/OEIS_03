c468c50369555587f8f7764a7c5ec4019a76bbff
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-23 18:35:32 +0900

    Square array T(n,k), n >= 1, k >= 1, read by antidiagonals, where T(n,k) = Sum_{1 <= x_1 <= x_2 <= ... <= x_k <= n} gcd(x_1, x_2, ... , x_k).

---
+++ 344/344479/344479_01.rb
@@ -0,0 +1,27 @@
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def A(k, n)
+  (1..n).inject(0){|s, i| s + phi(i) * (n / i) ** k}
+end
+
+def A344479(n)
+  ary = []
+  (1..n).each{|i|
+    (1..i).each{|j|
+      ary << A(i - j + 1, j)
+    }
+  }
+  ary
+end
+
+n = 140
+ary = A344479(n)
+(1..ary.size).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
