850f078efb8af3cd176f2b089ec32537c0099547
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-29 01:12:44 +0900

    a(n) = Sum_{k=1..n} (-1)^(k+1) * floor(n/k)^n.

---
+++ 344/344724/344724_01.rb
@@ -0,0 +1,13 @@
+def A(n)
+  ary = [0] + (1..n).map{|i| (1..i).inject(0){|s, j| s + (-1) ** ((j + 1) % 2) * (i / j) ** i}}
+end
+
+n = 1000
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
