ed5e3351b6031d1108ca6d67bbd1a7e37ea7f4bd
manman4 <7023566+manman4@users.noreply.github.com>
2021-05-30 19:54:12 +0900

    a(n) = Sum_{k=1..n} floor(n/k) * 3^(k-1).

---
+++ 344/344814/344814_01.rb
@@ -0,0 +1,12 @@
+def A(k, n)
+  (1..n).inject(0){|s, i| s + (n / i) * k ** (i - 1)}
+end
+
+n = 1000
+(1..n).each{|i|
+  j = A(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
