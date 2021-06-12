e5fc783aa54aee2abbd6ce63decb7000da388f87
manman4 <7023566+manman4@users.noreply.github.com>
2021-06-12 22:00:53 +0900

    a(n) = Sum_{1 <= x_1 <= x_2 <= ... <= x_n <= n} gcd(x_1, x_2, ..., x_n).

---
+++ 345/345230/345230_01.rb
@@ -0,0 +1,28 @@
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  s = 0
+  (1..n).each{|i| 
+    (1..i).each{|j|
+      s += phi(i / j) * ncr(j + k - 2, j - 1) if i % j == 0
+    }
+  }
+  s
+end
+
+
+n = 1000
+(0..n).each{|i|
+  j = A(i, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
