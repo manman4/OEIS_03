d4b1559ba2158aa0698eb4573d614cbfc11090ab
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-12 23:47:44 +0900

    a(n) = 1 - Sum_{k=2..n} (-1)^k * a(floor(n/k)).

---
+++ 347/347031/347031_01.rb
@@ -0,0 +1,15 @@
+def A(n)
+  ary = [0, 1]
+  (2..n).each{|i|
+    ary << (2..i).inject(1){|s, j| s - (-1) ** (j % 2) * ary[i / j]}
+  }
+  ary
+end
+
+n = 10000
+ary = A(n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
