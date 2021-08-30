c47696d2c03f20d50ec36321758ce91fdac71700
manman4 <7023566+manman4@users.noreply.github.com>
2021-08-30 15:16:59 +0900

    a(n) = n + 2^2 * floor(n/2^2) + 3^2 * floor(n/3^2) + 4^2 * floor(n/4^2) + ...

---
+++ 309/309125/309125_01.rb
@@ -0,0 +1,17 @@
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    j = i * i
+    s += j * (n / j)
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
