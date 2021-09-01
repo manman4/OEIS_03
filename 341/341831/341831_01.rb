691e3dab08015117bc651d37eaff9c786449c821
manman4 <7023566+manman4@users.noreply.github.com>
2021-09-01 16:01:38 +0900

    Dirichlet g.f.: 1 / zeta(s)^5.

---
+++ 341/341831/341831_01.rb
@@ -0,0 +1,19 @@
+require 'prime'
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  n.prime_division.inject(1){|s, i| s * (-1) ** (i[1] % 2) * ncr(k, i[1])}
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(5, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
