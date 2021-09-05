cfb9279aba3c4468d5a38f765303602be576b8e9
manman4 <7023566+manman4@users.noreply.github.com>
2021-09-05 19:46:54 +0900

    Partial sums of A347516.

---
+++ 347/347517/347517_01.rb
@@ -0,0 +1,16 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    s += 1 if i ** k <= n && n % i == 0
+  }
+  s
+end
+
+n = 10000
+s = 0
+(1..n).each{|i|
+  s += A(3, i)
+  print i
+  print ' '
+  puts s
+}
