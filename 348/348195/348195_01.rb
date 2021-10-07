da94e3e1bf01e2e22120e2b55965de0a017af3c0
manman4 <manchanr4@gmai.com>
2021-10-07 19:32:53 +0900

    Number of primes of the form 4k+3 < n^2.

---
+++ 348/348195/348195_01.rb
@@ -0,0 +1,20 @@
+require 'prime'
+
+def A(k, n)
+  s = 0
+  ((n - 1) ** 2..n ** 2 - 1).each{|i|
+    if i.prime?
+      s += 1 if i % 4 == k
+    end
+  }
+  s
+end
+
+n = 10000
+s = 0
+(1..n).each{|i| 
+  s += A(3, i)
+  print i
+  print ' '
+  puts s
+}
