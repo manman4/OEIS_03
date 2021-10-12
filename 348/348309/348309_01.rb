f1a918d353aea708390af1c72d8aa5b150f3fded
manman4 <7023566+manman4@users.noreply.github.com>
2021-10-12 19:48:32 +0900

    a(n) = Sum_{k=0..floor(n/8)} (-1)^k * binomial(n-4*k,4*k).

---
+++ 348/348309/348309_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n / (2 * k)).inject(0){|s, i| s + (-1) ** (i % 2) * ncr(n - k * i, k * i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(4, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
