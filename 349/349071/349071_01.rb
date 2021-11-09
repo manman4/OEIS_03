4fce7bcd5cacd03be34f26fcfa409798553d4007
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-09 18:23:50 +0900

    a(n) = T(n, 2*n), where T(n, x) is the Chebyshev polynomial of the first kind.

---
+++ 349/349071/349071_01.rb
@@ -0,0 +1,22 @@
+def A(k, n)
+  a, b = [1], [0, 1]
+  ary = [1]
+  cnt = 0
+  while cnt + 1 <= n
+    c = ([0] + b).map{|i| i * 2}
+    (0..cnt).map{|i| c[i] -= a[i]}
+    a, b = b, c
+    cnt += 1
+    ary << (0..cnt).inject(0){|s, i| s + k ** i * a[i]}
+  end
+  ary
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(2 * i, i)[-1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
