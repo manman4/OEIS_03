9be748a23a9e813ce47176694f3345f2063747f2
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-13 19:27:03 +0900

    Largest integer that can be expressed as Sum_{k=1..n} k/p(k), where p is a permutation of [n].

---
+++ 349/349257/349257_01.cr
@@ -0,0 +1,22 @@
+require "big"
+
+def a(n)
+return 0 if n == 0
+  k = 1
+  (1..n).each{|i| k *= i}
+  max = 0
+  (1..n).to_a.each_permutation{|i|
+    m = 0.to_big_i
+    (1..n).each{|j| m += k * j // i[j - 1]}
+    if m % k == 0
+      max = m // k if max < m // k
+    end
+  }
+  max
+end
+
+def a349257(n)
+  (0..n).map{|i| a(i)}
+end
+
+p a349257(10)
