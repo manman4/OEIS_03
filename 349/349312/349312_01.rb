6214fa53a2e1abbb3baa8702cd2790211d770a6c
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-15 23:02:01 +0900

    G.f. A(x) satisfies: A(x) = (1 + x * A(x)^6) / (1 - x).

---
+++ 349/349312/349312_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  (0..n).inject(0){|s, i| s + ncr(n + (k - 1) * i, k * i) * ncr(k * i, i) / ((k - 1) * i + 1r)}
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(6, i).to_i
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
