99c8518a2922e152e4e308fb3f95fae3eb4a53b3
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-20 21:00:03 +0900

    a(n) = Sum_{k=0..n} (-1)^(n-k) * binomial(k*n,n).

---
+++ 349/349470/349470_01.rb
@@ -0,0 +1,18 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  return 1 if n < 2
+  (0..n).inject(0){|s, i| s + (-1) ** ((n - i) % 2) * ((i - 1) * n + 1..i * n).inject(:*)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i) / f(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
