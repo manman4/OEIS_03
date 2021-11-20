c2d0c7d5b6644ebca03eb6e3ea8d234e16b152fc
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-20 21:04:16 +0900

    G.f. A(x) satisfies: A(x) = 1 + x * A(x)^8 / (1 + x).

---
+++ 349/349364/349364_01.rb
@@ -0,0 +1,19 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# ncr((k + 1) * i, i) / (k * i + 1)は整数
+def A(k, n)
+  return 1 if n == 0
+  (1..n).inject(0){|s, i| s + (-1) ** ((n - i) % 2) * ncr(n - 1, i - 1) * ncr((k + 1) * i, i) / (k * i + 1)}
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(7, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
