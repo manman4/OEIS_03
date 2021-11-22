8b4ba97ed308a5f9c23b6013102c84d4883ccbd6
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-22 12:30:18 +0900

    a(n) = Sum_{k=0..n} (-1)^(n-k) * (3*k+1)^(k-1) * Stirling2(n,k).

--- 349/349528/349528_01.rb
+++ 349/349528/349528_01.rb
@@ -1,14 +1,14 @@
 def A(k, n)
   a = [1]
   ary = [1]
-  n.times{|i|
+  (1..n).each{|i|
     a << 0
     b = [0]
-    (0..i).each{|j|
+    (0..i - 1).each{|j|
       b[j + 1] = a[j] + (j + 1) * a[j + 1]
     }
     a = b
-    ary << (0..i + 1).inject(0){|s, j| s + (k * j + 1) ** (j - 1) * (-1) ** (i + 1 - j) * a[j]}
+    ary << (0..i).inject(0){|s, j| s + (k * j + 1) ** (j - 1) * (-1) ** (i - j) * a[j]}
   }
   ary
 end
