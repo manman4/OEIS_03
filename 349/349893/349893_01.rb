acd829dd61c45d4b0258e69471097c2227b5ef8d
manman4 <7023566+manman4@users.noreply.github.com>
2021-12-08 20:12:00 +0900

    a(n) = Sum_{k=0..n} k^(k*(n-k)).

--- 349/349893/349893_01.rb
+++ 349/349893/349893_01.rb
@@ -1,13 +1,8 @@
-def ncr(n, r)
-  return 1 if r == 0
-  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
-end
-
 def A(n)
   (0..n).inject(0){|s, i| s + i ** (i * (n - i))}
 end
 
-n = 100
+n = 14
 (0..n).each{|i|
   j = A(i)
   break if j.to_s.size > 1000
