5059c22e539f8efb4bc88352eb77310d89643663
manman4 <7023566+manman4@users.noreply.github.com>
2021-12-11 18:15:51 +0900

    Expansion of Sum_{k>=0} x^k/(1 + k^2 * x).

---
+++ 349/349856/349856_01.rb
@@ -0,0 +1,13 @@
+def A(k, n)
+  (0..n).inject(0){|s, i| s + (-i ** k) ** (n - i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(2, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
