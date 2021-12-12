41f8bb10f8b1f13ad33d8f0ade19b8346e711d64
manman4 <7023566+manman4@users.noreply.github.com>
2021-12-12 15:52:26 +0900

    Numbers that are the perimeter of a primitive 120-degree integer triangle.

---
+++ 350/350045/350045_02.rb
@@ -0,0 +1,21 @@
+def A(n)
+  ary = []
+  (1..n).each{|i|
+    (i + 1..n).each{|j|
+      if i.gcd(j) == 1 && (i - j) % 3 > 0
+        ary << 2 * j * j + 3 * i * j + i * i
+      end
+    }
+  }
+  ary
+end
+
+n = 10000
+# include?()でlistに入れていくより、あとで数えた方が速い
+
+ary = A(2000).uniq.sort[0..n - 1]
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
