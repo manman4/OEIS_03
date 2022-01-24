7f5eb43628d9388876c5da96952ff53539170b24
manman4 <7023566+manman4@users.noreply.github.com>
2022-01-24 19:17:12 +0900

    Triangle T(n,k), n >= 1, 1 <= k <= n, read by rows, where T(n,k) is the number of partitions of n such that (smallest part) = k*(number of parts).

---
+++ 350/350890/350890_01.rb
@@ -0,0 +1,20 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+
+def A(n)
+  a = Array.new(n, 0)
+  partition(n, 1, n).each{|ary|
+    (1..n).each{|i|
+      a[i - 1] += 1 if ary[-1] == i * ary.size
+    }
+  }
+  a
+end
+
+def A350890(n)
+  (1..n).map{|i| A(i)}.flatten
+end
+
+p A350890(14)
