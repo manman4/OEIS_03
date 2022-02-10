9c63c4956a543d3fbcf19c8716c16ac59babb727
manman4 <7023566+manman4@users.noreply.github.com>
2022-02-10 22:21:58 +0900

    a(n) = Sum_{p|n, p prime} p^p.

---
+++ 351/351366/351366_01.rb
@@ -0,0 +1,18 @@
+require 'prime'
+
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    s += i ** i if i.prime? && n % i == 0
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
