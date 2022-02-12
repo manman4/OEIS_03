737bc7e33ae96110793a82fd9972a6bab718852a
manman4 <7023566+manman4@users.noreply.github.com>
2022-02-12 17:55:07 +0900

    Sum of the 10th powers of the square divisors of n.

---
+++ 351/351316/351316_01.rb
@@ -0,0 +1,18 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    j = i * i
+    break if j > n
+    s += i ** (2 * k) if n % j == 0
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(10, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
