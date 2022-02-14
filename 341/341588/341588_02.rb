a61a854f9922791aa8fff7631d5262cf4fe9f7f8
manman4 <7023566+manman4@users.noreply.github.com>
2022-02-14 23:43:08 +0900

    E.g.f.: -log(1 + log(1 - x))^3 / 6.

---
+++ 341/341588/341588_02.rb
@@ -0,0 +1,24 @@
+def A(n)
+  a = [1]
+  ary = [[1]]
+  (1..n).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (i - 1) * a[j + 1]
+    }
+    a = b
+    ary << b.clone # ary << bではなく、ary << b.cloneを書くことで、a, bを変更しても、aryには影響を与えない
+  }
+  ary
+end
+
+n = 500
+ary = A(n)
+(3..n).each{|i|
+  j = (3..i).inject(0){|s, j| s + ary[i][j] * ary[j][3]}
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
