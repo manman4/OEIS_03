b4df34bdba3ab4b2e0ddeb0ca54ed3509df2a647
manman4 <7023566+manman4@users.noreply.github.com>
2022-02-15 22:54:01 +0900

    a(n) = n^5 * Sum_{d^2|n} 1 / d^5.

---
+++ 351/351603/351603_01.rb
@@ -0,0 +1,19 @@
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    j = i * i
+    break if j > n
+    s += (n / i.to_r) ** k if n % j == 0
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(5, i)
+  break if j.denominator > 1
+  print i
+  print ' '
+  puts j.to_i
+}
+
