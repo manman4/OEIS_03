8c87c347d7e9e7cd9c1be6450f53223a826f9a98
manman4 <7023566+manman4@users.noreply.github.com>
2022-02-19 16:01:36 +0900

    a(n) = Sum_{k=0..n} binomial(n,k) * k! * k^n.

---
+++ 332/332408/332408_01.rb
@@ -0,0 +1,18 @@
+def f(n)
+  return 1 if n == 0
+  (1..n).inject(:*)
+end
+
+def A(n)
+  m = f(n)
+  (0..n).inject(0){|s, i| s + i ** n * m / f(n - i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
