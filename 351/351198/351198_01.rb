be312ff1cbe713d6d9e610ead482cdd9496f5f52
manman4 <7023566+manman4@users.noreply.github.com>
2022-03-02 23:32:52 +0900

    Sum of the 10th powers of the primes dividing n.

---
+++ 351/351198/351198_01.rb
@@ -0,0 +1,16 @@
+require 'prime'
+
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    s += i ** k if i.prime? && n % i == 0
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(10, i)
+}
