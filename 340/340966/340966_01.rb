45b60055a0d58ce91ea8ee2c9d4e44a8f9ba42f6
manman4 <7023566+manman4@users.noreply.github.com>
2022-03-19 06:05:45 +0900

    Number of ways to write n as an ordered sum of 10 primes.

---
+++ 340/340966/340966_01.rb
@@ -0,0 +1,39 @@
+require 'prime'
+
+# m次以下を取り出す
+def mul(f_ary, b_ary, m)
+  s1, s2 = f_ary.size, b_ary.size
+  ary = Array.new(s1 + s2 - 1, 0)
+  (0..s1 - 1).each{|i|
+    (0..s2 - 1).each{|j|
+      ary[i + j] += f_ary[i] * b_ary[j]
+    }
+  }
+  ary[0..m]
+end
+
+# m次以下を取り出す
+def power(ary, n, m)
+  return [1] if n == 0
+  k = power(ary, n >> 1, m)
+  k = mul(k, k, m)
+  return k if n & 1 == 0
+  return mul(k, ary, m)
+end
+
+def A(k, n)
+  ary = [0] * (n + 1)
+  (1..n).each{|i|
+    ary[i] = 1 if i.prime?
+  }
+  power(ary, k, n)
+end
+
+n = 10100
+m = 10000
+ary = A(10, n)
+(20..m).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
