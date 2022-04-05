42a921c6bf88db1affc90eb10919a78df9dc4ae6
manman4 <7023566+manman4@users.noreply.github.com>
2022-04-06 00:42:45 +0900

    Expansion of e.g.f. 1 / (1 - Sum_{k>=1} sigma_2(k) * x^k/k!).

---
+++ 352/352693/352693_01.rb
@@ -0,0 +1,32 @@
+def sigma_2(n)
+  cnt = 0
+  (1..n).each{|i|
+    cnt += i * i if n % i == 0
+  }
+  cnt
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  s_ary = (0..n).map{|i| sigma_2(i)}
+  ary = [1]
+  (1..n).each{|i|
+    ary << (1..i).inject(0){|s, j| s + ncr(i, j) * s_ary[j] * ary[-j]}
+  }
+  ary
+end
+
+n = 500
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+  
