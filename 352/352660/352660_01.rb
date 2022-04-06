5a922eb45abc57a1e9e84503801b5b73c8a09840
manman4 <7023566+manman4@users.noreply.github.com>
2022-04-06 22:35:37 +0900

    a(n) = n! * Sum_{k=0..floor(n/4)} 1 / (4*k)!.

---
+++ 352/352660/352660_01.rb
@@ -0,0 +1,18 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  m = f(n)
+  (0..n / k).inject(0){|s, i| s + m / f(k * i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(4, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
