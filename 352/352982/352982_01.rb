651846c9ad5bab57f03c36d6560c656476a4ef54
manman4 <7023566+manman4@users.noreply.github.com>
2022-04-16 19:36:45 +0900

    a(n) = Sum_{k=0..floor(n/3)} k^n.

---
+++ 352/352982/352982_01.rb
@@ -0,0 +1,12 @@
+def A(k, m, n)
+  (0..n / k).inject(0){|s, i| s + i ** (m * n)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, 1, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
