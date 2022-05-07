8b29302b9dac489d7dcef0510077b84e2eae43ca
manman4 <7023566+manman4@users.noreply.github.com>
2022-05-08 05:07:28 +0900

    Expansion of e.g.f. 1 / (1 - (exp(x) - 1)^3 / 3!).

---
+++ 346/346894/346894_01.rb
@@ -0,0 +1,30 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  m = f(k)
+  a = [1]
+  ary = [1]
+  (1..n).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (j + 1) * a[j + 1]
+    }
+    a = b
+    ary << (0..i / k).inject(0){|s, j| s + f(k * j) * a[k * j] / (m ** j).to_r}.to_i
+  }
+  ary
+end
+
+n = 500
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
