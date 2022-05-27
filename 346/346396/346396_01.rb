b0faa536d69309a572380910c3be7d3f9caf48f3
manman4 <7023566+manman4@users.noreply.github.com>
2022-05-28 02:35:55 +0900

    Expansion of e.g.f. -log(1 - x) * exp(4*x).

---
+++ 346/346396/346396_01.rb
@@ -0,0 +1,17 @@
+def A(k, n)
+  ary = [0, 1]
+  (2..n).each{|i|
+    ary << (i + k - 1) * ary[-1] - k * (i - 1) * ary[-2] + k ** (i - 1)
+  }
+  ary
+end
+
+n = 1000
+ary = A(4, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
