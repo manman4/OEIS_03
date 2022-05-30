5a23bb83e63cd98d9cf2dd05290416dfb29ec922
manman4 <7023566+manman4@users.noreply.github.com>
2022-05-30 22:46:20 +0900

    a(0) = 1; a(n) = Sum_{k=0..floor(n/4)} binomial(n,4*k) * a(k).

---
+++ 351/351971/351971_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| 
+    ary << (0..i / k).inject(0){|s, j| s + ncr(i, k * j) * ary[j]}
+  }
+  ary
+end
+
+n = 1000
+ary = A(4, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
