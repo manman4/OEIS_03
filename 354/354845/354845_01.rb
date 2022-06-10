cf751f949128d2c8ae8a679922ed888ea525a906
manman4 <7023566+manman4@users.noreply.github.com>
2022-06-10 21:42:43 +0900

    a(n) = n! * Sum_{d|n} (n/d)^(d-1) / d!.

---
+++ 354/354845/354845_01.rb
@@ -0,0 +1,22 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  m = f(n)
+  s = 0
+  (1..n).each{|i|
+    s += (n / i) ** (i - 1) * m / f(i) if n % i == 0 
+  }
+  s
+end
+
+n = 500
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
