2f27af1ec198669309212ce607874f2643116d93
manman4 <7023566+manman4@users.noreply.github.com>
2022-06-13 04:09:31 +0900

    Let p1 <= p2 <= ... <= pk be the prime factors of n, with repetition; let s = 1/p1 + 1/(p1*p2) + 1/(p1*p2*p3) + ... + 1/(p1*p2*...*pk); a(n) = denominator of s. a(1)=1 by convention.

---
+++ 322/322035/322035_01.rb
@@ -0,0 +1,25 @@
+require 'prime'
+
+def A(n)
+  n.prime_division.map{|i| [i[0]] * i[1]}.flatten
+end
+
+def B(n)
+  return 0 if n == 1
+  s = 1
+  sum = 0
+  A(n).each{|i|
+    s /= i.to_r
+    sum += s
+  }
+  sum
+end
+
+n = 2 ** 14
+(1..n).each{|i|
+  j = B(i).denominator
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
