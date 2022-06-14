0c66b557e20faa87b525715fe1be03e33a87137a
manman4 <7023566+manman4@users.noreply.github.com>
2022-06-14 09:44:54 +0900

    Number of acyclic n-multidigraphs on n labeled vertices.

---
+++ 354/354962/354962_01.rb
@@ -0,0 +1,23 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + (-1) ** (j + 1) * (k + 1) ** (j * (i - j)) * ncr(i, j) * ary[-j]}}
+  ary[-1]
+end
+
+def A354962(n)
+  (0..n).map{|i| A(i, i)}
+end
+
+p A354962(10)
+# (0..ary.size - 1).each{|i|
+#   j = ary[i]
+#   break if j.to_s.size > 1000
+#   print i
+#   print ' '
+#   puts j
+# }  
