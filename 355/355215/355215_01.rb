b0a68506f3d152879939add444da9111afce03b9
manman4 <7023566+manman4@users.noreply.github.com>
2022-06-25 20:15:28 +0900

    E.g.f. A(x) satisfies A'(x) = 1 + A(-2 * log(1-x))/2.

---
+++ 355/355215/355215_01.rb
@@ -0,0 +1,24 @@
+def A(n)
+  a = [1]
+  ary = [0, 1]
+  (1..n - 1).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+        b[j + 1] = a[j] + (i - 1) * a[j + 1]
+    }
+    a = b
+    ary << (1..i).inject(0){|s, j| s + 2 ** (j - 1) * a[j] * ary[j]}
+  }
+  ary
+end
+
+n = 400
+ary = A(n)
+(1..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
