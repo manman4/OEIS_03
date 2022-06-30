80f98041d1c24338b525787dc68f34867eb1b171
manman4 <7023566+manman4@users.noreply.github.com>
2022-07-01 00:47:20 +0900

    Expansion of e.g.f. exp(exp(3*x) - exp(x)).

---
+++ 355/355378/355378_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, m, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (1..i).inject(0){|s, j| s + (k ** j - m ** j) *  ncr(i - 1, j - 1) * ary[-j]}
+  }
+  ary
+end
+
+n = 500
+ary = A(3, 1, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
