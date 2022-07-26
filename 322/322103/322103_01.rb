7f378156b68215d4c337b6efda5e87a8e883fb3f
manman4 <7023566+manman4@users.noreply.github.com>
2022-07-26 21:48:49 +0900

    Square array A(n,k), n >= 1, k >= 0, read by antidiagonals: A(n,k) = Sum_{d|n} sigma_k(d).

---
+++ 322/322103/322103_01.rb
@@ -0,0 +1,39 @@
+require 'prime'
+
+def power0(a, n)
+  return 1 if n == 0
+  k = power0(a, n >> 1)
+  k *= k
+  return k if n & 1 == 0
+  return k * a
+end
+
+def sigma(x, i)
+  return (1..i).select{|j| i % j == 0}.size if x == 0
+  sum = 1
+  pq = i.prime_division
+  pq.each{|a, n| sum *= (power0(a, (n + 1) * x) - 1) / (power0(a, x) - 1)}
+  sum
+end
+
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    s += sigma(k, i) if n % i == 0
+  }
+  s
+end
+
+n = 140
+cnt = 1
+(1..n).each{|i|
+  (1..i).each{|j|
+    k = A(i - j, j)
+    break if k.to_s.size > 1000
+    print cnt
+    print ' '
+    puts k
+    cnt += 1
+  }
+}
+
