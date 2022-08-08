628bda30dbbce64cfcce60e63bea0f6435bb1f65
manman4 <7023566+manman4@users.noreply.github.com>
2022-08-09 00:59:32 +0900

    a(n) = n! * Sum_{k=1..n} floor(n/k)/k!.

---
+++ 355/355886/355886_01.rb
@@ -0,0 +1,19 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  m = f(n)
+  (1..n).inject(0){|s, i| s + (n / i) * m / f(i)}
+end
+
+n = 1000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
