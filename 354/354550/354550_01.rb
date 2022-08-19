8930d21eed745eb91b02accc96d70f743753c0a5
manman4 <7023566+manman4@users.noreply.github.com>
2022-08-19 12:57:49 +0900

    Expansion of e.g.f. exp( x * exp(x^2/2) ).

---
+++ 354/354550/354550_01.rb
@@ -0,0 +1,19 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  a = f(n)
+  b = f(k).to_r
+  (0..n / k).inject(0){|s, i| s + a * (n - k * i) ** i / (b ** i * f(i) * f(n - k * i))}.to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(2, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j 
+}
