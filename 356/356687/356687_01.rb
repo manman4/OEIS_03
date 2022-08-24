93dadd2edc12a05d941b207ac33719d979940b39
manman4 <7023566+manman4@users.noreply.github.com>
2022-08-24 22:48:10 +0900

    a(n) = n! * Sum_{k=0..n} k^(2*n)/k!.

---
+++ 356/356687/356687_01.rb
@@ -0,0 +1,17 @@
+def f(n, k)
+  return 1 if  n == k
+  (k + 1..n).inject(:*)
+end
+
+def A(k, n)
+  (0..n).inject(0){|s, i| s + f(n, i) * i ** (k * n)}
+end
+
+n = 200
+(0..n).each{|i|
+  j = A(2, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
