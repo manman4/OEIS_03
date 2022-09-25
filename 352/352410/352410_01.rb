d83deed717d6062922a6106936d948644056b0de
manman4 <7023566+manman4@users.noreply.github.com>
2022-09-25 11:38:56 +0900

    Expansion e.g.f. LambertW( -x/(1-x) ) / (-x).

---
+++ 352/352410/352410_01.rb
@@ -0,0 +1,24 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n)
+  m = f(n)
+  (0..n).inject(0){|s, i| s + (i + 1) ** (i - 1) * ncr(n, i) * m / f(i)}
+end
+
+n = 500
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
