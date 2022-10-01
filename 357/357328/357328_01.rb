9f2678bfe232c969b1b548241f6e7914e8812271
manman4 <7023566+manman4@users.noreply.github.com>
2022-10-01 23:40:21 +0900

    Number of permutations p of [n] such that p(i) divides p(j) if i divides j for 1 <= j <= i <= n.

---
+++ 357/357328/357328_01.rb
@@ -0,0 +1,25 @@
+def A(n)
+  cnt = 0
+  (2..n).to_a.permutation{|i|
+    perm = [1] + i
+    flag = true
+    (1..n).each{|j|
+      m = perm[j - 1]
+      (1..j).each{|k|
+        if j % k == 0
+          flag = false if m % perm[k - 1] > 0
+        end
+      }
+    }
+    cnt += 1 if flag
+  }
+  cnt
+end
+
+p (1..13).map{|i| p A(i)}
+
+# かなり時間がかかる
+# [1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 2, 6]
+
+
+
