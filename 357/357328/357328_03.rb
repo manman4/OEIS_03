aedb259584f8430cde57e9eb340720caddc90205
manman4 <7023566+manman4@users.noreply.github.com>
2022-10-03 20:36:29 +0900

    Number of permutations p of [n] such that p(i) divides p(j) if i divides j for 1 <= i <= j <= n.

--- 357/357328/357328_03.rb
+++ 357/357328/357328_03.rb
@@ -8,13 +8,13 @@ end
 def A(n)
   h = {}
   Prime.each(n).each{|i|
-    h[i] = Math.log(n, i).to_i
+    h[i] = n / i
   }
   h.group_by{|k, v| v}.inject(1){|s, i| s * f(i.last.size)}
 end
 
 def A357328(n)
-  [1, 1] + (2..n).map{|i| A(i)}
+  (0..n).map{|i| A(i)}
 end
 
 p A357328(100)
