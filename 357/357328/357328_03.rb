47b123909e875463c5e3db52d81a3818491880d8
manman4 <7023566+manman4@users.noreply.github.com>
2022-10-03 20:37:26 +0900

    Number of permutations p of [n] such that p(i) divides p(j) if i divides j for 1 <= i <= j <= n.

--- 357/357328/357328_03.rb
+++ 357/357328/357328_03.rb
@@ -1,30 +1,37 @@
 require 'prime'
 
-def f(n)
-  return 1 if n < 2
-  (1..n).inject(:*)
+def search(a, num, n)
+  if num == n + 1
+    # p [n, a]
+    @count += 1
+  elsif a[num] > 0
+    search(a, num + 1, n)
+  else
+    ((0..n).to_a - a).each{|i|
+      flag = true
+      (1..num - 1).each{|j|
+        if num % j == 0
+          flag = false if i % a[j] > 0
+        end
+      }
+      if flag
+        a[num] = i
+        search(a, num + 1, n)
+        a[num] = 0
+      end
+    }
+  end
 end
 
 def A(n)
-  h = {}
-  Prime.each(n).each{|i|
-    h[i] = n / i
+  p_ary = Prime.each(Math.sqrt(n).to_i).to_a
+  a = [0, 1] + [0] * (n - 1)
+  (2..n).each{|i|
+    a[i] = i if i.prime_division.map{|j| j[0]}.all?{|j| p_ary.include?(j)}
   }
-  h.group_by{|k, v| v}.inject(1){|s, i| s * f(i.last.size)}
+  @count = 0
+  search(a, 1, n)
+  @count
 end
 
-def A357328(n)
-  (0..n).map{|i| A(i)}
-end
-
-p A357328(100)
-
-# n = 5000
-# ary = A357328(n)
-# (0..n).each{|i| 
-#   j = ary[i]
-#   break if j.to_s.size > 1000
-#   print i
-#   print ' '
-#   puts j
-# }
+p (1..40).map{|i| p A(i)}
---
+++ 357/357328/357328_04.rb
@@ -0,0 +1,30 @@
+require 'prime'
+
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  h = {}
+  Prime.each(n).each{|i|
+    h[i] = n / i
+  }
+  h.group_by{|k, v| v}.inject(1){|s, i| s * f(i.last.size)}
+end
+
+def A357328(n)
+  (0..n).map{|i| A(i)}
+end
+
+p A357328(100)
+
+# n = 5000
+# ary = A357328(n)
+# (0..n).each{|i| 
+#   j = ary[i]
+#   break if j.to_s.size > 1000
+#   print i
+#   print ' '
+#   puts j
+# }
