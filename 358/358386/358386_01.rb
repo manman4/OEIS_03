29cabaff165c954aed6eb36071b83ea2b18e0995
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-16 01:15:43 +0900

    Distinct values of A030717 in order of appearance.

--- 358/358386/358386_01.rb
+++ 358/358386/358386_01.rb
@@ -1,15 +1,15 @@
 def A030717(n)
   ary = [1]
-  (1..n).each{|i|
+  n.times{
     ary += ary.uniq.sort.map{|i| ary.count(i)}
   }
   ary
 end
-
-n = 300
-ary = A030717(n).uniq
-(1..1000).each{|i|
-  print i
-  print ' '
-  puts ary[i - 1]
-}
+p A030717(50).uniq
+# n = 300
+# ary = A030717(n).uniq
+# (1..1000).each{|i|
+#   print i
+#   print ' '
+#   puts ary[i - 1]
+# }
