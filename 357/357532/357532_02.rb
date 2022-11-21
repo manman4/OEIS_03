45cc7398aa2706b36246013c4a4c8d88aea5247b
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-21 22:26:20 +0900

    a(n) = Sum_{0..floor(n/3)} (n-2*k)!/(n-3*k)!.

---
+++ 357/357532/357532_02.rb
@@ -0,0 +1,22 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  ary = [1] * k
+  (k..n).each{|i|
+    ary << ((k - 1) * ary[-1] + i * ary[-k] + 1) / k
+  }
+  ary
+end
+
+n = 1000
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
