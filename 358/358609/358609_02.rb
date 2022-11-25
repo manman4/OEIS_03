97c367d4aa3412fe6de2d3e191595af26926b1a2
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-25 19:00:46 +0900

    a(n) = Sum_{0..floor(n/4)} (-1)^k * (n-4*k)!.

---
+++ 358/358609/358609_02.rb
@@ -0,0 +1,17 @@
+def A(n)
+  ary = [1, 1, 2, 6, 23]
+  (5..n).each{|i|
+    ary << i * ary[-1] - ary[-4] + i * ary[-5]
+  }
+  ary
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
