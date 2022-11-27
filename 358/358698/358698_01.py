a7f08fd364567629296a4429824dabf77bcf429d
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-27 23:06:33 +0900

    Number of self-avoiding closed paths in the 7 X n grid graph which pass through all vertices on four (left, right, upper, lower) sides of the graph.

---
+++ 358/358698/358698_01.py
@@ -0,0 +1,18 @@
+# Using graphillion
+
+from graphillion import GraphSet
+import graphillion.tutorial as tl
+
+def A333758(n, k):
+    universe = tl.grid(n - 1, k - 1)
+    GraphSet.set_universe(universe)
+    cycles = GraphSet.cycles()
+    points = [i for i in range(1, k * n + 1) if i % k < 2 or ((i - 1) // k + 1) % n < 2]
+    for i in points:
+        cycles = cycles.including(i)
+    return cycles.len()
+
+def A358698(n):
+    return A333758(n, 7)
+
+print([A358698(n) for n in range(2, 22)])
