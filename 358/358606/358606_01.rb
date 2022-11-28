c241e009f6fd889befedd3bf56dd253b978fec7e
manman4 <7023566+manman4@users.noreply.github.com>
2022-11-29 01:29:47 +0900

    a(n) = Sum_{k=0..floor(n/5)} (-1)^k * (n-4*k)!/(n-5*k)!.

---
+++ 358/358606/358606_01.rb
@@ -0,0 +1,17 @@
+def A(k, n)
+  ary = [1] * k
+  (k..n).each{|i|
+    ary << ((k - 1) * ary[-1] - i * ary[-k] + 1) / k
+  }
+  ary
+end
+
+n = 1000
+ary = A(5, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
