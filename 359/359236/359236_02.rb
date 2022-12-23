28d7a3157ff913268fb2133849b5bfd34175a1bf
manman4 <7023566+manman4@users.noreply.github.com>
2022-12-24 02:20:51 +0900

    Number of divisors of 5*n-2 of form 5*k+1.

---
+++ 359/359236/359236_02.rb
@@ -0,0 +1,16 @@
+def A001878(n)
+  s = 0
+  (1..n).each{|i|
+    s += 1 if n % i == 0 && i % 5 == 3
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i| 
+  j = A001878(5 * i - 2)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
