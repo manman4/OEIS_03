4f5d0fc51bb3230c2a31fa9965e2831b5e22f7d9
manman4 <7023566+manman4@users.noreply.github.com>
2023-01-21 13:19:08 +0900

    Expansion of Product_{k>=2} (1 - x^(k^2-1)) in powers of x.

---
+++ 359/359966/359966_02.rb
@@ -0,0 +1,34 @@
+require 'prime'
+
+def is_square(n)
+  return true if n < 2
+  n.prime_division.all?{|i| i[1] % 2 == 0}
+end
+
+def A359967(n)
+  a = []
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      s += j if i % j == 0 && is_square(j + 1)
+    }
+    a << s
+  }
+  a
+end
+
+def C(n)
+  ary = [1]
+  a = [0] + A359967(n)
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s - a[j] * ary[-j]} / i}
+  ary
+end
+
+n = 10100
+m = 10000
+ary = C(n)
+(0..m).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
