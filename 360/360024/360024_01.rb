4d1b799d11c1093dc6e5f86121c4629bc69a83b2
manman4 <7023566+manman4@users.noreply.github.com>
2023-01-23 21:24:33 +0900

    a(n) = Sum_{k=0..floor(n/2)} (-1)^k * binomial(n-k,k) * Catalan(k).

---
+++ 360/360024/360024_01.rb
@@ -0,0 +1,18 @@
+def A(k, n)
+  ary = [1] * k
+  (k..n).each{|i|
+    ary << (0..i - k).inject(1){|s, j| s - ary[j] * ary[-k - j]}
+  }
+  ary
+end
+
+n = 1100
+m = 1000
+ary = A(2, n)
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
