dfc945c80484392395b765143be17ff03b455ecb
manman4 <7023566+manman4@users.noreply.github.com>
2023-03-04 20:08:36 +0900

    E.g.f. satisfies A(x) = exp( 3*x*A(x) ) / (1-x).

---
+++ 361/361182/361182_01.rb
@@ -0,0 +1,23 @@
+def f(n)
+  return 1 if n == 0
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  m = f(n)
+  (0..n).inject(0){|s, i| s + m / f(i) * k ** i * (i + 1) ** (i - 1) * ncr(n, i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
