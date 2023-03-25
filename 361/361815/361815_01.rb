fbd5f5529e89c20b03ca12084526436344af6ef8
manman4 <7023566+manman4@users.noreply.github.com>
2023-03-26 00:10:13 +0900

    Expansion of 1/sqrt(1 - 4*x*(1-x)^2).

---
+++ 361/361815/361815_01.rb
@@ -0,0 +1,17 @@
+def A(n)
+  ary = [1, 2, 2]
+  (3..n).each{|i|
+    ary << 2 * ( (2*i-1)*ary[-1] - 2*(2*i-2)*ary[-2] + (2*i-3)*ary[-3] ) / i
+  }
+  ary
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
