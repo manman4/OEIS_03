c408d46288b90ad5a969d76eea65fab0763e7fba
manman4 <7023566+manman4@users.noreply.github.com>
2023-03-29 01:32:02 +0900

    Expansion of 1/sqrt(1 - 4*x/(1+x)^3).

---
+++ 360/360133/360133_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n, k)
+  (0..n).inject(0){|s, i| s + (-1) ** (n - i) * ncr(2 * i, i) * ncr(n + (k - 1) * i - 1, n - i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i, 3)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
