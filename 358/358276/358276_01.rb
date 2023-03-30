b414b2d860a5460062061414e91569c5fd20dfe3
manman4 <7023566+manman4@users.noreply.github.com>
2023-03-31 00:23:59 +0900

    a(1) = 1; a(n) = n * Sum_{d|n, d < n} (-1)^(n/d - 1) * a(d) / d.

---
+++ 358/358276/358276_01.rb
@@ -0,0 +1,22 @@
+# a[0] = 0
+def A(n)
+  ary = [0, 1]
+  (2..n).each{|i|
+    s = 0
+    (1..i - 1).each{|j|
+      if i % j == 0
+        s += (i / j) * (-1) ** (i / j - 1)  * ary[j]
+      end
+    }
+    ary << s
+  }
+  ary
+end
+
+n = 10000
+ary = A(n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
