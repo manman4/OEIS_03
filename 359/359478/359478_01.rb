ddf12cc00a2bbfa320bc1a667ede367b9ebb4f3a
manman4 <7023566+manman4@users.noreply.github.com>
2023-04-01 02:46:56 +0900

    a(1) = 1; a(n) = -Sum_{k=2..n} k * a(floor(n/k)).

---
+++ 359/359478/359478_01.rb
@@ -0,0 +1,16 @@
+def A(n)
+  ary = [0, 1]
+  (2..n).each{|i|
+    ary << -(2..i).inject(0){|s, j| s + j * ary[i / j]}
+  }
+  ary
+end
+
+n = 10010
+m = 10000
+ary = A(n)
+(1..m).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
