2cf2131d67623e3907bd56666f698315ddd9a89b
manman4 <7023566+manman4@users.noreply.github.com>
2023-04-01 22:41:22 +0900

    a(n) = n * mu(n) If n is odd, otherwise n * mu(n) - (n/2) * mu(n/2).

---
+++ 359/359484/359484_02.rb
@@ -0,0 +1,31 @@
+require 'prime'
+
+def A(n)
+  return 1 if n == 1
+  s = 1
+  n.prime_division.each{|i, j|
+    if i == 2
+      if j == 1
+        s *= -3
+      elsif j == 2
+        s *= 2
+      else
+        s *= 0
+      end
+    else
+      if j == 1
+        s *= -i
+      else
+        s *= 0
+      end
+    end
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i| 
+  print i
+  print ' '
+  puts A(i)
+}
