6ab7ac0f36b3ebbe3bf25a6ae876f39991bd379b
manman4 <7023566+manman4@users.noreply.github.com>
2023-04-02 22:24:33 +0900

    a(1) = 1, a(n) = n^2 * Sum_{d|n, d < n} (-1)^(n/d) a(d) / d^2.

---
+++ 361/361987/361987_02.rb
@@ -0,0 +1,25 @@
+require 'prime'
+
+def A(n)
+  return 1 if n == 1
+  s = 1
+  n.prime_division.each{|i, j|
+    if i == 2
+      s *= 2 ** (3 * j - 1)
+    else
+      if j == 1
+        s *= -i * i
+      else
+        s *= 0
+      end
+    end
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i| 
+  print i
+  print ' '
+  puts A(i)
+}
