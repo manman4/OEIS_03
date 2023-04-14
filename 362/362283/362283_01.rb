44fcdddad4598c755653defa109e7f2f8a605cba
manman4 <7023566+manman4@users.noreply.github.com>
2023-04-15 02:36:35 +0900

    Expansion of e.g.f. exp( sqrt(-LambertW(-x^2)) ).

---
+++ 362/362283/362283_01.rb
@@ -0,0 +1,27 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (0..(i - 1) / k).inject(0){|s, j| s + (j + 1r / k) ** j * f(k * j) / f(j) * ncr(i - 1, k * j) * ary[-1 - k * j]}.to_i
+  }
+  ary
+end
+
+n = 500
+ary = A(2, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
