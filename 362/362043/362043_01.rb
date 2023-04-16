2eecf5ced5a4f86fc5223d8edd1a079b642cfb31
manman4 <7023566+manman4@users.noreply.github.com>
2023-04-16 20:42:01 +0900

    Square array T(n,k), n >= 0, k >= 0, read by antidiagonals downwards, where T(n,k) = n! * Sum_{j=0..floor(n/3)} (k/6)^j * binomial(n-2*j,j)/(n-2*j)!.

---
+++ 362/362043/362043_01.rb
@@ -0,0 +1,24 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n, k)
+  m = f(n)
+  (0..n / 3).inject(0){|s, i| s + m * (k / 6r) ** i / (f(i) * f(n - 3 * i))}.to_i
+end
+
+n = 139
+ary = []
+(0..n).each{|i|
+  (0..i).each{|j|
+    ary << A(j, i - j)
+  }
+}
+(0..ary.size - 1).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
