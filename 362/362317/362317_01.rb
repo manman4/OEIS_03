78bf9d4bad80eea6f0328e3501bec19cc6a74e0a
manman4 <7023566+manman4@users.noreply.github.com>
2023-04-18 19:19:54 +0900

    a(n) = n! * Sum_{k=0..floor(n/4)} (n/24)^k /(k! * (n-4*k)!).

---
+++ 362/362317/362317_01.rb
@@ -0,0 +1,18 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n)
+  m = f(n)
+  (0..n / 4).inject(0){|s, i| s + m * (n / 24r) ** i / (f(i) * f(n - 4 * i))}.to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
