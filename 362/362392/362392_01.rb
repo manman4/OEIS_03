bbe432bc44aef2e8e60d9c843fd16dee7de19d0f
manman4 <7023566+manman4@users.noreply.github.com>
2023-04-21 18:17:22 +0900

    E.g.f. satisfies A(x) = exp(x + x^3 * A(x)).

---
+++ 362/362392/362392_01.rb
@@ -0,0 +1,18 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  m = f(n)
+  (0..n / k).inject(0){|s, i| s + m * (i + 1) ** (n - (k - 1) * i - 1) / (f(i) * f(n - k * i))}.to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
