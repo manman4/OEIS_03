38a57142a967a5e65994ac4a0f3f21409428cef1
manman4 <7023566+manman4@users.noreply.github.com>
2023-05-04 21:51:54 +0900

    E.g.f. satisfies A(x) = exp(x * A(x)^(x^2/6)).

---
+++ 362/362573/362573_01.rb
@@ -0,0 +1,25 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  return 1 if n == 0
+  m = f(n)
+  x = f(k)
+  (0..n / k).inject(0){|s, i| s + ((n - (k - 1) * i) / x.to_r) ** i * ncr(n - (k - 1) * i - 1, i) * m / f(n - (k - 1) * i)}.to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
