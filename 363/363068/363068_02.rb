5b36e137e952727637eed317843d275bd5f315f2
manman4 <7023566+manman4@users.noreply.github.com>
2023-05-17 20:14:06 +0900

    Number of partitions p of n such that (1/5)*max(p) is a part of p.

---
+++ 363/363068/363068_02.rb
@@ -0,0 +1,25 @@
+def A(n, k)
+  ary = [[1] * (n + 1)]
+  # S(n,k) = S(n,k-1) + S(n-k,k) for k<=n, S(n,k) = S(n,k-1) for k>n を計算する
+  (1..n).each{|i|
+    a = [0]
+    (1..i).each{|j| a << a[-1] + ary[i - j][j]}
+    (i + 1..n).each{|j| a << a[-1]}  # k > n の場合
+    ary << a
+  }
+  # a(n) = sum(j=0, floor(n/(k+1)), T(n-(k+1)*j,k*j)) を計算する
+  (0..n).map{|i|
+    (0..i / (k + 1)).inject(0){|s, j| s + ary[i - (k + 1) * j][k * j]}
+  }
+end
+
+n = 1100
+m = 1000
+ary = A(n, 5)
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
