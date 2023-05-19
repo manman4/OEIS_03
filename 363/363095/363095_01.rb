a01f16f87d635fa2112817fde76eabb01e1870cd
manman4 <7023566+manman4@users.noreply.github.com>
2023-05-20 03:55:36 +0900

    Number of partitions of n whose least part is a multiple of 4.

---
+++ 363/363095/363095_01.rb
@@ -0,0 +1,26 @@
+# 和因子はmin以上max以下
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+
+def A(k, n)
+  return 1 if n == 0
+  cnt = 0
+  partition(n, 1, n).each{|i|
+    cnt += 1 if i[-1] % k == 0
+  }
+  cnt
+end
+
+n = 50
+b = []
+(0..n).each{|i|
+  j = A(4, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+  b << j
+}
+p b
