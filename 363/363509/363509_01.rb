86f5093bef1dd65ead743caea3679fa1c33927ae
manman4 <7023566+manman4@users.noreply.github.com>
2023-06-07 22:32:52 +0900

    G.f. satisfies A(x) = exp( Sum_{k>=1} (-1)^(k+1) * (3 + A(x^k)) * x^k/k ).

---
+++ 363/363509/363509_01.rb
@@ -0,0 +1,28 @@
+def A(n, k)
+  # a(0) = 1; a(n) = (-1/n) * Sum_{j=1..n} ( k * (-1)^j + Sum_{d|j} (-1)^(j/d) * d * a(d-1) ) * a(n-j)
+  a = [1]
+  (1..n).each{|i|
+    s = 0
+    (1..i).each{|j| 
+      t = k * (-1) ** j
+      (1..j).each{|d|
+        if j % d == 0
+          t += (-1) ** (j / d) * d * a[d - 1]
+        end
+      }
+      s += t * a[i - j]
+    }
+    a[i] = -s / i
+  }
+  a
+end
+
+n = 1000
+ary = A(n, 3)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
