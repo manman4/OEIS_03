d9de568a2eb23cc3873bd87875b13ccac5e9c0cf
manman4 <7023566+manman4@users.noreply.github.com>
2023-06-20 19:34:28 +0900

    Number of ordered partitions of n into cubes > 1.

---
+++ 363/363738/363738_01.rb
@@ -0,0 +1,30 @@
+def I(ary, n)
+  a = [1]
+  i = 0
+  while i < n
+    a << -(0..i).inject(0){|s, j| s + ary[1 + i - j] * a[j]}
+    i += 1
+  end
+  a
+end
+
+def A(n, k)
+  a = [1] + [0] * n
+  (2..n).each{|i|
+    j = i ** k
+    break if j > n
+    a[j] = -1
+  }
+  I(a, n)
+end
+
+n = 10100
+m = 10000
+ary = A(n, 3)
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
