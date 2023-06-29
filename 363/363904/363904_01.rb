aec2ed6430212d74ef351ca79cf2fb9989fefc2c
manman4 <7023566+manman4@users.noreply.github.com>
2023-06-29 21:10:58 +0900

    Expansion of Sum_{k>0} x^(3*k) / (1 - x^(4*k))^2.

---
+++ 363/363904/363904_01.rb
@@ -0,0 +1,17 @@
+def A(n, mod, k)
+  s = 0
+  (1..n).each{|i|
+    if n % i == 0
+      s += i + mod - k if i % mod == k
+    end
+  }
+  s / mod
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(i, 4, 3)
+}
+
