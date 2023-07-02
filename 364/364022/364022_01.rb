d01bdc1be37828f47ac0b846f4fca797acb5e4c4
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-02 14:14:58 +0900

    Expansion of Sum_{k>0} k * x^(4*k) / (1 + x^(5*k)).

---
+++ 364/364022/364022_01.rb
@@ -0,0 +1,16 @@
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    if n % i == 0
+      s += (-1) ** (n / i) * i if (n / i) % 5 == 4
+    end
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(i)
+}
