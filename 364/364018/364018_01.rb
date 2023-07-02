5990ddbe1e601378d60fee541237c2224cd0a9cb
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-02 16:16:59 +0900

    Expansion of Sum_{k>0} (-1)^(k+1) * x^(3*k-1) / (1 - x^(3*k-1))^3.

---
+++ 364/364018/364018_01.rb
@@ -0,0 +1,16 @@
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    if n % i == 0
+      s += (-1) ** (n / i) * i * (i + 1) / 2 if (n / i) % 3 == 2
+    end
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(i)
+}
