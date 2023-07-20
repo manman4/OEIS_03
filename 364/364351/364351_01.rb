d92e249e702a5a195b4c026948d691c334275fac
manman4 <7023566+manman4@users.noreply.github.com>
2023-07-20 23:42:35 +0900

    Expansion of Sum_{k>0} k^2 * x^k/(1 + x^k)^3.

---
+++ 364/364351/364351_01.rb
@@ -0,0 +1,17 @@
+# a(n) = (n/2) * Sum_{d|n} (-1)^(d+1) * (d+1)
+def A(n)
+  s = 0
+  (1..n).each{|i|
+    s += (-1) ** ((n / i + 1) % 2) * (i + n) if n % i == 0
+  }
+  n * s / 2
+end
+
+n = 10000
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
