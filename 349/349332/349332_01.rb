fabe594388ce26e42a285644d600ec5b8dbcb31d
manman4 <7023566+manman4@users.noreply.github.com>
2023-08-07 18:35:39 +0900

    G.f. A(x) satisfies: A(x) = 1 + x * A(x)^5 / (1 - x).

---
+++ 349/349332/349332_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  return 1 if n == 0
+  (1..n).inject(0){|s, i| s + ncr(n - 1, i - 1) * ncr(k * i, i) / ((k - 1) * i + 1)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(5, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
