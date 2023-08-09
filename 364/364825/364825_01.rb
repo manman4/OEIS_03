9a679297804b23c7266726d5c8b5df47963f99a5
manman4 <7023566+manman4@users.noreply.github.com>
2023-08-10 02:29:33 +0900

    G.f. satisfies A(x) = 1 - x*A(x)^3 * (1 - 3*A(x)).

---
+++ 364/364825/364825_01.rb
@@ -0,0 +1,21 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, n)
+  s = 0
+  (0..n).each{|i|
+    s += (-3) ** i * ncr(n, i) * ncr(k * n + i + 1, n) / (k * n + i + 1r)
+  }
+  (-1) ** n * s.to_i
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(3, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
