25c8d1bafc6534aec8fabf4be446694f6a05c6bc
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-07 21:13:05 +0900

    E.g.f.: 1 / (1 + 6 * log(1 - x))^(1/6).

---
+++ 347/347019/347019_01.rb
@@ -0,0 +1,22 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  a = [1]
+  (1..n).each{|i|
+    a << (1..i).inject(0){|s, j| s + (f(i) / f(i - j)) * ((k + 1).to_r / j - k.to_r / i) * a[-j]}.to_i
+  }
+  a
+end
+
+n = 500
+ary = A(5, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
