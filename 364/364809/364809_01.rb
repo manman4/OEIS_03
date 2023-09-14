5e6222991249e1b2ea13b136961021dc50719f83
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-14 21:57:28 +0900

    Number of partitions of n with at most five part sizes.

---
+++ 364/364809/364809_01.rb
@@ -0,0 +1,11 @@
+# 和因子はmin以上max以下
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+
+def A(k, n)
+  (0..n).map{|i| partition(i, 1, i).select{|j| j.uniq.size <= k}.size}
+end
+
+p A(5, 60)
