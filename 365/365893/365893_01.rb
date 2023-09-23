c28b891322d2834341cd9da030ffa81ff34dda2d
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-23 20:15:31 +0900

    Expansion of e.g.f. exp( Sum_{k>=0} x^(5*k+3) / (5*k+3)! ).

---
+++ 365/365893/365893_01.rb
@@ -0,0 +1,22 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, m, n)
+  ary = [1]
+  (1..n).each{|i|
+    ary << (0..(i - m) / k).inject(0){|s, j| s + ncr(i - 1, k * j + m - 1) * ary[i - k * j - m]}
+  }
+  ary
+end
+
+n = 700
+ary = A(5, 3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
