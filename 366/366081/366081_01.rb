2cec6c0bd5a2fc7ed27326f1bcd3e36de1ef6c60
manman4 <7023566+manman4@users.noreply.github.com>
2023-09-30 21:42:03 +0900

    Expansion of (1/x) * Series_Reversion( x*(1-x)^2/(1-x-x^2) ).

---
+++ 366/366081/366081_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(k, m, n)
+  (0..n / k).inject(0){|s, i| s + (-1) ** (i % 2) * ncr(n + 1, i) * ncr(m * n - (k - 1) * i + m - 2, n - k * i)} / (n + 1)
+end
+
+n = 1000
+(0..n).each{|i| 
+  j =A(2, 2, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
