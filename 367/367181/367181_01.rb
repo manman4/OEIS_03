3c95ddbdd600d0902bafd7d80154f2cb01293015
manman4 <7023566+manman4@users.noreply.github.com>
2023-11-10 23:13:54 +0900

    E.g.f. satisfies A(x) = 1 + (exp(x*A(x)^3) - 1)/A(x)^2.

---
+++ 367/367181/367181_01.rb
@@ -0,0 +1,30 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  a = [1]
+  ary = [1]
+  (1..n).each{|i|
+    a << 0
+    b = [0]
+    (0..i - 1).each{|j|
+      b[j + 1] = a[j] + (j + 1) * a[j + 1]
+    }
+    a = b
+    # i > 0 のときa[0] = 0
+    ary << (1..i).inject(0){|s, j| s + f(k * i - (k - 1) * j) / f(k * i - k * j + 1).to_r * a[j]}.to_i
+  }
+  ary
+end
+
+n = 500
+ary = A(3, n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
