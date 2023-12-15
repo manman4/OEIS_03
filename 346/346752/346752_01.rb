513d08fb790f5d3ddb6656200ee8c6546600944b
manman4 <7023566+manman4@users.noreply.github.com>
2023-12-15 19:03:23 +0900

    Expansion of e.g.f. log( 1 + x^4 * exp(x) / 4! ).

---
+++ 346/346752/346752_01.rb
@@ -0,0 +1,19 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(k, n)
+  l = f(n)
+  m = f(k)
+  (1..n / k).inject(0){|s, i| s + (-1) ** (i - 1) * i ** (n - k * i - 1) * l / (m ** i * f(n - k * i))}.to_i
+end
+
+n= 1000
+(0..n).each{|i|
+  j = A(4, i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
