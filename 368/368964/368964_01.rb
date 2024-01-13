b0757cb4a0b87a21c2b91342769b2c81dc5f02bc
manman4 <7023566+manman4@users.noreply.github.com>
2024-01-13 11:53:31 +0900

    Expansion of (1/x) * Series_Reversion( x * (1-x-x^3)^3 ).

---
+++ 368/368964/368964_01.rb
@@ -0,0 +1,17 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n, s, t, u)
+  (0..n / s).inject(0){|sum, i| sum + ncr(t * (n + 1) + i - 1, i) * ncr((t + u + 1) * (n + 1) - (s - 1) * i - 2, n - s * i)} / (n + 1)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i, 3, 3, 0)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
