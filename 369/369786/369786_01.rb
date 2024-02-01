4643056da56db481aea8631cb44cb857201c36a4
manman4 <7023566+manman4@users.noreply.github.com>
2024-02-01 23:37:24 +0900

    Number of different coefficient values in expansion of Product_{k=1..n} (1+x^(k^2)).

---
+++ 369/369786/369786_01.rb
@@ -0,0 +1,23 @@
+def A(n)
+  a = [1]
+  ary = [1]
+  (1..n).each{|i|
+    b = a + [0] * (i * i)
+    (0..a.size - 1).each{|j| b[i * i + j] += a[j]}
+    a = b
+    ary << a.uniq.size
+  }
+  ary
+end
+
+n = 50
+p ary = A(n)
+m = 20
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
