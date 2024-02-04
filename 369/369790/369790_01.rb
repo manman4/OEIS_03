257a93386454c87c1dd97a0a868b28b00a6838e1
manman4 <7023566+manman4@users.noreply.github.com>
2024-02-04 14:57:22 +0900

    Number of different coefficient values in expansion of Product_{k=1..n} (1-x^k)^3.

---
+++ 369/369790/369790_01.rb
@@ -0,0 +1,28 @@
+def A(n)
+  a = [1]
+  ary = [1]
+  (1..n).each{|i|
+    b = a + [0] * (3 * i)
+    (0..a.size - 1).each{|j|
+      k = a[j]
+      b[i + j] -= 3 * k
+      b[2 * i + j] += 3 * k
+      b[3 * i + j] -= k
+    }
+    a = b
+    ary << a.uniq.size
+  }
+  ary
+end
+
+n = 1010
+ary = A(n)
+m = 1000
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
