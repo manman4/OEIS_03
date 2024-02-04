3ed726a52100293e8b2daa8850d931cfe9508551
manman4 <7023566+manman4@users.noreply.github.com>
2024-02-04 23:04:55 +0900

    Sum of products of cubes of parts , counted without multiplicity, in all partitions of n.

---
+++ 369/369888/369888_01.rb
@@ -0,0 +1,15 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+
+def A(n)
+  return 1 if n == 0
+  s = 0
+  partition(n, 1, n).each{|ary|
+    s += ary.uniq.inject(1){|s, i| s * i * i * i}
+  }
+  s
+end
+
+p (0..35).map{|i| A(i)}
