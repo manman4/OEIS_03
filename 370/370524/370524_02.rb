fe9631aa6412928a6844f7bbff8eaecccc227257
manman4 <7023566+manman4@users.noreply.github.com>
2024-02-24 22:25:09 +0900

    Number of permutations of [n] having exactly one adjacent 2-cycle.

---
+++ 370/370524/370524_02.rb
@@ -0,0 +1,19 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+def A(n, k, q)
+  s = 0
+  (0..n / q - k).each{|j| s += (-1) ** j * f(n - (q - 1) * (j + k)) / f(j)}
+  s / f(k)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i, 1, 2)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
