6d28e7fc10ab0e48e845b5230d17e5d4a79b3a31
manman4 <7023566+manman4@users.noreply.github.com>
2024-03-02 13:12:55 +0900

    Triangle read by rows: T(n,k) = 2 * (k mod 2 + 1) * T(n-1,floor(k/2)) + 1 with T(0,0) = 1 for 0 <= k <= 2^n-1.

--- 370/370762/370762_01.rb
+++ 370/370762/370762_01.rb
@@ -1,47 +1,22 @@
-# If n appears so do 2n+1 and 4n+1.
 def A(n)
+  ary = [[1]]
   a = [1]
-  i = 0
-  while i <= n
-    j = a[i]
-    p [j, j * 2 + 1] if a.include?(j * 2 + 1)
-    p [j, j * 4 + 1] if a.include?(j * 4 + 1)
-    a << j * 2 + 1
-    a << j * 4 + 1
-    a.sort!.uniq!
-    i += 1
-  end
-  a[0..n - 1]
+  n.times{
+    b = []
+    a.each{|i|
+      b << 2 * i + 1
+      b << 4 * i + 1
+    }
+    ary << b
+    a = b
+  }
+  ary.flatten
 end
 
-n = 10000
+n = 12
 ary = A(n)
-#p ary.map{|i| i.to_s(2)}
-
-# 2進数表記した時0が連続しないかどうか
-def check_0s(n)
-  n.to_s(2).match(/0{2,}/).nil?
-end
-
-def B(n)
-  a = [1]
-  i = 2
-  while a.size < n
-    if check_0s(i) && i % 2 == 1
-      a << i
-    end
-    i += 1
-  end
-  a
-end
-
-ary1 = B(n)
-p ary == ary1
-
-# (1..n).each{|i|
-#   j = ary[i - 1]
-#   break if j.to_s.size > 1000
-#   print i
-#   print ' '
-#   puts j
-# }
+(0..ary.size - 1).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
