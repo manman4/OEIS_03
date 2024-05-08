ee51abceb449f343c694b01ff114e68a32ca1433
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-08 20:08:55 +0900

    Square array T(n,k), n >= 1, k >= 1, read by antidiagonals downwards, where T(n,k) = Sum_{j=1..n} phi(k*j).

---
+++ 372/372606/372606_01.rb
@@ -0,0 +1,26 @@
+def phi(n)
+  (1..n).count{|i| i.gcd(n) == 1}
+end
+
+# T(n,k) = Sum_{j=1..n} phi(k*j)
+def T(n, k)
+  (1..n).inject(0){|s, i| s + phi(k * i)}
+end
+
+def A(n)
+  a = []
+  (1..n).each{|i|
+    a << (1..i).map{|j| T(j, i - j + 1)}
+  }
+  a.flatten
+end
+
+n = 140
+ary = A(n)
+(1..ary.size).each{|i|
+  j = ary[i - 1]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
