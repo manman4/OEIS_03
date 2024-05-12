b412460fd5c14ba6491a6ebbc45c494152a0d239
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-13 00:41:12 +0900

    E.g.f. A(x) satisfies A(A(A(A(A(x))))) = (-1/5) * log(1 - 5*x).

--- 372/372747/372747_01.rb
+++ 372/372747/372747_01.rb
@@ -52,9 +52,9 @@ def f(n)
   (1..n).inject(:*)
 end
 
-n = 17
-m = 17 
-f_ary = [0] + (1..n).map{|i| 5 ** (i - 1) / f(i - 1).to_r}
+n = 20
+m = 20 
+f_ary = [0] + (1..n).map{|i| 5 ** (i - 1) / i.to_r}
 ary = f5r(f_ary, n)
 a = [0] + (1..n).map{|i| f(i) * ary[i]}
 p [0] + (1..n).map{|i| (f(i) * ary[i]).numerator}
