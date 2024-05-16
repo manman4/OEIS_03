f452ca1b096611ee10c95ca963652b1a205d94ff
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-16 19:18:37 +0900

    a(n) = Sum_{j=1..n} Sum_{k=1..n} gcd(j*k,n).

--- 372/372881/372881_02.rb
+++ 372/372881/372881_02.rb
@@ -10,17 +10,11 @@ def A(n)
   s
 end
 
-# multiplicative function
-def B(n)
-  n.prime_division.inject(1){|s, (p, e)| s * A(p ** e)}
-end
-
 n = 1000
 (1..n).each{|i|
-  j = B(i)
+  j = A(i)  
   break if j.to_s.size > 1000
   print i
   print ' '
   puts j
 }
-
