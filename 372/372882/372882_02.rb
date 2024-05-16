e36e67be5d63674e3fa43f57cc95eba9c8145540
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-16 19:13:50 +0900

    a(n) = Sum_{k=1..n} gcd(k^3,n).

--- 372/372882/372882_02.rb
+++ 372/372882/372882_02.rb
@@ -4,17 +4,11 @@ def A(n)
   (1..n).inject(0){|s, i| s + n.gcd(i * i * i)}
 end
 
-# multiplicative function
-def B(n)
-  n.prime_division.inject(1){|s, (p, e)| s * A(p ** e)}
-end
-
 n = 10000
 (1..n).each{|i|
-  j = B(i)
+  j = A(i)
   break if j.to_s.size > 1000
   print i
   print ' '
   puts j
 }
-
