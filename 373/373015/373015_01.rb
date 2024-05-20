c1af5c3df1ee6667aa11af0ac09d5d681eb6e094
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-20 22:19:37 +0900

    Number of partitions p of n such that max(p) == 2 mod 3.

---
+++ 373/373015/373015_01.rb
@@ -0,0 +1,19 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+
+def A(n, k, m)
+  cnt = 0
+  partition(n, 1, n).each{|i|
+    cnt += 1 if i[0] % k == m
+  }
+  cnt
+end
+
+# p (1..30).map{|i| A(i, 2, 0)}
+# p (1..30).map{|i| A(i, 2, 1)}
+
+# p (1..60).map{|i| A(i, 3, 0)}
+# p [0] + (1..50).map{|i| A(i, 3, 1)}
+p [0] + (1..50).map{|i| A(i, 3, 2)}
