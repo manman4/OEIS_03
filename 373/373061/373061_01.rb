df748c1a7ecac4e628db955ae76df32bb5671e97
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-21 22:09:02 +0900

    a(n) = Sum_{1 <= x_1, x_2, x_3 <= n} gcd(x_1, x_2, n)/gcd(x_1, x_2, x_3, n).

---
+++ 373/373061/373061_01.rb
@@ -0,0 +1,35 @@
+require 'prime'
+
+def A(n)
+  ary = [0, 1]
+  (2..n).each{|i|
+    p_ary = i.prime_division.to_a
+    # n = p^e
+    if p_ary.size == 1
+      s = 0
+      (1..i).each{|j|
+        (1..i).each{|k|
+          (1..i).each{|l|
+            m = (i.gcd(j)).gcd(k)
+            s += m / m.gcd(l)
+          }
+        }
+      }
+      ary << s
+    else
+      # multiplicative function
+      ary << p_ary.inject(1){|s, (p, e)| s * ary[p ** e]}
+    end
+  }
+  ary
+end
+
+n = 200
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
---
+++ 373/373061/373061_02.rb
@@ -0,0 +1,26 @@
+def A(n)
+  ary = [0, 1]
+  (2..n).each{|i|
+    s = 0
+    (1..i).each{|j|
+      (1..i).each{|k|
+        (1..i).each{|l|
+          m = (i.gcd(j)).gcd(k)
+          s += m / m.gcd(l)
+        }
+      }
+    }
+    ary << s
+  }
+  ary
+end
+
+n = 200
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
