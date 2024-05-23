149b0de2c39155fca6bf558597d45d2f385ef9f1
manman4 <7023566+manman4@users.noreply.github.com>
2024-05-24 01:03:01 +0900

    Number of partitions of n into distinct parts such that number of parts is a multiples of 3.

---
+++ 370/370747/370747_01.rb
@@ -0,0 +1,21 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i - 1).map{|rest| [i, *rest]}}
+end
+
+def A(n, k)
+  return 1 if n == 0
+  cnt = 0
+  partition(n, 1, n).each{|ary|
+    cnt += 1 if ary.size % k == 0
+  }
+  cnt
+end 
+
+# p (1..50).map{|i| A(i, 1)}
+# p (1..50).map{|i| A(i, 2)}
+p (0..63).map{|i| A(i, 3)}
+# p (1..70).map{|i| A(i, 4)}
+# p (1..75).map{|i| A(i, 5)}
+
+
