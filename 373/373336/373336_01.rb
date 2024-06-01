1021de951a03e5b6e5985b48daf591cc8475b044
manman4 <7023566+manman4@users.noreply.github.com>
2024-06-02 01:25:46 +0900

    Expansion of Sum_{k>=1} x^k / (1 + x^k + x^(2*k) + x^(3*k) + x^(4*k) + x^(5*k) + x^(6*k)).

---
+++ 373/373336/373336_01.rb
@@ -0,0 +1,18 @@
+# 約数で(1 mod k) - (2 mod k)
+def A(k, n)
+  s = 0
+  (1..n).each{|i|
+    if n % i == 0
+      s += 1 if i % k == 1
+      s -= 1 if i % k == 2
+    end
+  }
+  s
+end
+
+n = 10000
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(7, i)
+}
