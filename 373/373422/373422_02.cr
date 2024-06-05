ad3ba2c521da7af308ca915b84253f25b44f82e3
manman4 <7023566+manman4@users.noreply.github.com>
2024-06-06 00:04:27 +0900

    Triangle read by rows: T(n,k) = number of permutations of [n] starting from k that have zero (n-1)-th differences. (n>=1, 1<=k<=n).

--- 373/373422/373422_02.cr
+++ 373/373422/373422_02.cr
@@ -1,21 +1,28 @@
+require "big"
+
 def ncr(n, r)
   return 1 if r == 0
-  ((n - r + 1)..n).reduce(1){|p, i| p * i} // (1..r).reduce(1){|p, i| p * i}
+  ((n - r + 1)..n).reduce(BigInt.new(1)){|p, i| p * i} / (1..r).reduce(1){|p, i| p * i}
 end
 
 def a(n)
   a = (0..n - 1).map{|i| ncr(n - 1, i)}
   ary = [0] * (n + 1)
-  (1..n).to_a.each_permutation{|i|
-    # iとaの交代和を計算
-    if (0..n - 1).reduce(0){|s, j| s + (-1) ** (j % 2) * a[j] * i[j]} == 0
-      ary[i[0]] += 1
-    end
+  (1..(n + 1) // 2).each{|i0|
+    b = (1..n).to_a.reject{|x| x == i0}
+    b.each_permutation{|i|
+      i = [i0] + i
+      # iとaの交代和を計算
+      if (0..n - 1).reduce(BigInt.new(0)){|s, j| s + (-1) ** (j % 2) * a[j] * i[j]} == 0
+        ary[i0] += 1
+      end
+    }
   }
+  ((n + 1) // 2 + 1..n).each{|i| ary[i] = ary[n + 1 - i]}
   ary[1..-1]
 end
 
-n = 12
+n = 13
 ary = (1..n).map{|i| a(i)}.flatten
 (1..ary.size).each{|i|
   print i
