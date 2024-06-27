46e5d6c5b25ea4e3d6ba28ddafed35a82b884e37
manman4 <7023566+manman4@users.noreply.github.com>
2024-06-27 23:27:29 +0900

    Number of solutions to a^2 + n = 2 * b^2 with 0 < a < b.

---
+++ 374/374061/374061_01.rb
@@ -0,0 +1,21 @@
+def A(n)
+  m = Math.sqrt(n).to_i
+  cnt = 0
+  (1..m).each{|i|
+    (i + 1..m).each{|j|
+      if i * i + n == 2 * j * j
+        # p [i, j]
+        cnt += 1
+      end
+    }
+  }
+  cnt
+end
+
+# p (1..119).map{|i| A(i)}
+n = 200
+(1..n).each{|i|
+  print i
+  print ' '
+  puts A(i)
+}
