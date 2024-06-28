07213a2722b130b1f326e378125c0c0777392611
manman4 <7023566+manman4@users.noreply.github.com>
2024-06-28 23:10:56 +0900

    Number of solutions to n^2 = x^2 + 3*x*y + y^2 with 0 < x < y.

---
+++ 374/374100/374100_01.rb
@@ -0,0 +1,29 @@
+def A(n)
+  m = Math.sqrt(n).floor + 1
+  ary = Array.new(n + 1, 0)
+  (1..m).each{|i|
+    (i + 1..m).each{|j|
+      k = i * i + 3 * i * j + j * j
+      if k <= n
+        ary[k] += 1
+      end
+    }
+  }
+  ary
+end
+
+def B(n)
+  ary = A(n * n)
+  (0..n).map{|i| ary[i * i]}
+end
+
+p B(100)
+
+# n = 11000
+# m = 10000
+# ary = A(n)
+# (0..m).each{|i|
+#   print i
+#   print ' '
+#   puts ary[i]
+# }
