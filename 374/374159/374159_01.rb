699494605a202ba099c08ecd52d32539d231913c
manman4 <7023566+manman4@users.noreply.github.com>
2024-06-29 22:39:58 +0900

    a(n) is the smallest nonnegative integer k where exactly n pairs of positive integers (x, y) exist such that x^2 + 7*y^2 = k.

---
+++ 374/374159/374159_01.rb
@@ -0,0 +1,27 @@
+require 'prime'
+
+def A(n)
+  m = Math.sqrt(n).to_i + 1
+  ary = Array.new(n + 1, 0)
+  (1..m).each{|i|
+    (1..m).each{|j|
+      k = i * i + 7 * j * j
+      if k <= n
+        ary[k] += 1
+      end
+    }
+  }
+  ary
+end
+
+n = 23 * 10 ** 7
+ary = A(n)
+
+p a = (0..100).map{|i| ary.index(i)}
+p (0..17).map{|i| (a[i] / 8r).denominator}
+# (1..10).each{|i|
+#   j = a[i]
+#   print i
+#   print ' '
+#   puts [j, [j.prime_division.to_a]]
+# }
