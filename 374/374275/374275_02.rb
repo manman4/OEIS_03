4b026deb48f1a3648adcb4b77bd8c3a5f95295a4
manman4 <7023566+manman4@users.noreply.github.com>
2024-07-02 23:31:00 +0900

    Smallest k such that k can be written in exactly n ways as x^2 + 3*x*y + y^2 with 0 <= x <= y.

--- 374/374275/374275_02.rb
+++ 374/374275/374275_02.rb
@@ -25,3 +25,5 @@ p a = (0..100).map{|i| ary.find{|k, j| j == i}}
 #   print ' '
 #   puts a[i] 
 # }
+
+# [nil, [0, 1], [121, 2], [2299, 3], [6061, 4], [43681, 5], [66671, 6], [33659659, 7], [187891, 8], [1266749, 9], [8067191, 10], [639533521, 11], [2066801, 12], nil, [36735721, 14], [153276629, 15], [7703531, 16], nil, [39269219, 18], nil, [250082921, 20], nil, nil, nil, [84738841, 24], nil, nil, nil, nil, nil, nil, nil, [454508329, 32], nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil]
