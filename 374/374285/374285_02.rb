72ddadc662ec7180e3abeda73ed34049cef1560b
manman4 <7023566+manman4@users.noreply.github.com>
2024-07-03 21:42:28 +0900

    a(n) is the smallest nonnegative integer k where there are exactly n nonnegative integer solutions to x^2 + 2*y^2 = k.

---
+++ 374/374285/374285_02.rb
@@ -0,0 +1,27 @@
+def A(n)
+  m = Math.sqrt(n).to_i + 1
+  h = {}
+  (0..m).each{|i|
+    (0..m).each{|j|
+      k = i * i + 2 * j * j
+      break if k > n
+      if h.key?(k)
+        h[k] += 1
+      else
+        h[k] = 1
+      end
+    }
+  }
+  h.sort
+end
+
+n = 2 * 10 ** 8
+ary = A(n)
+
+# h[k] = i の 最小のk を出力
+p a = (0..100).map{|i| ary.find{|k, j| j == i}}
+# (0..100).each{|i|
+#   print i
+#   print ' '
+#   puts a[i] 
+# }
