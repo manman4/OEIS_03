e17c8c949f095231955af1167e327ef34669828f
manman4 <7023566+manman4@users.noreply.github.com>
2024-07-04 09:35:07 +0900

    a(n) is the smallest positive integer k such that A002325(k) = n.

---
+++ 374/374294/374294_01.rb
@@ -0,0 +1,29 @@
+def A(n)
+  m = Math.sqrt(n).to_i + 1
+  ary = Array.new(n + 1, 0)
+  (0..m).each{|i|
+    (0..m).each{|j|
+      k = i * i + 2 * j * j
+      if k <= n
+        if i == 0 && j == 0
+          ary[k] += 0
+        elsif i > 0 && j > 0
+          ary[k] += 2
+        else
+          ary[k] += 1
+        end
+      end
+    }
+  }
+  ary
+end
+
+n = 10 * 10 ** 6
+ary = A(n)
+
+p a = (0..100).map{|i| ary.index(i)}
+# (0..100).each{|i|
+#   print i
+#   print ' '
+#   puts a[i]
+# }
