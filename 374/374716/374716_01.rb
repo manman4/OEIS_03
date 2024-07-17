c8bf01c816bd119c069c9bea4475ebcb3f8e923d
manman4 <7023566+manman4@users.noreply.github.com>
2024-07-18 00:09:56 +0900

    Number of distinct sums i^2 + j^2 + k^2 + l^2 for 1<=i<=j<=k<=l<=n.

---
+++ 374/374716/374716_01.rb
@@ -0,0 +1,25 @@
+def A(n)
+  ary = []
+  (1..n).each{|i|
+    (i..n).each{|j|
+      (j..n).each{|k|
+        (k..n).each{|l|
+          ary << i ** 2 + j ** 2 + k ** 2 + l ** 2
+        }
+      }
+    }
+  }
+  ary.uniq.size
+end
+
+n = 54
+b=[]
+(1..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+  b << j
+}
+p b
