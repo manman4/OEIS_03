2cb010664bacdf166e9a193ed8220dfb07e91e87
manman4 <7023566+manman4@users.noreply.github.com>
2024-07-17 23:45:40 +0900

    Number of distinct sums i^3 + j^3 + k^3 + l^3 for 0<=i<=j<=k<=l<=n.

---
+++ 374/374711/374711_02.rb
@@ -0,0 +1,30 @@
+def A(n)
+  h = {}
+  (0..n).each{|i|
+    (i..n).each{|j|
+      (j..n).each{|k|
+        (k..n).each{|l|
+          x = i * i * i + j * j * j + k * k * k + l * l * l
+          if h.has_key?(x)
+            h[x] += 1
+          else
+            h[x] = 1
+          end
+        }
+      }
+    }
+  }
+  h.size
+end
+
+n = 48
+b=[]
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+  b << j
+}
+p b
