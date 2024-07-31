e941d867837cd186ee6873ac47f3d54245b3f703
manman4 <7023566+manman4@users.noreply.github.com>
2024-07-31 20:35:03 +0900

    Defined by: Sum_{i=1..n} a(i) / n^i = 1, n >= 1.

---
+++ 374/374562/374562_01.rb
@@ -0,0 +1,16 @@
+# a(n) = n^n - Sum_{i=1..n-1} n^(n-i)*a(i).
+def A(n)
+  ary = [0]
+  (1..n).each{|i| ary << i ** i - (1..i - 1).inject(0){|s, j| s + i ** (i - j) * ary[j]}}
+  ary
+end
+
+n = 500
+ary = A(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
