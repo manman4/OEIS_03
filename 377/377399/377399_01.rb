f380992e63b35aa8128c25a477fdec7d5ca94e9e
manman4 <7023566+manman4@users.noreply.github.com>
2024-10-28 12:29:42 +0900

    Expansion of e.g.f. (2 - exp(x))^4.

---
+++ 377/377399/377399_01.rb
@@ -0,0 +1,21 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(0) = 1; a(n) = Sum_{k=1..n} (1 - 5 * k/n) * binomial(n,k) * a(n-k).
+def A(n)
+  ary = [1]
+  (1..n).each{|i| ary << (1..i).inject(0){|s, j| s + (1 - 5r * j / i) * ncr(i, j) * ary[i - j]}.to_i}
+  ary
+end
+
+n = 1000
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
