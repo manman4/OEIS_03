6d47818abc1d1ff1807316143dcd23ff799179cf
manman4 <7023566+manman4@users.noreply.github.com>
2024-12-27 00:40:11 +0900

    Number of n-digit numbers that have exactly 5 divisors.

---
+++ 379/379568/379568_01.rb
@@ -0,0 +1,20 @@
+require 'prime'
+
+p_ary = Prime.each(10 ** 9).to_a
+ary = [ ]
+cnt = 0
+d = 10
+p_ary.each{|i|
+  j = i ** 4
+  if j < d
+    cnt += 1
+  elsif j < d * 10
+    ary << cnt
+    cnt = 1
+    d *= 10
+  else
+    print 'error'
+  end
+}
+p ary
+  
