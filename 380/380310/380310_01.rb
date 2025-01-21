66f6e5d9c6ef4996ddb3d70b51eb44dea8de6610
manman4 <7023566+manman4@users.noreply.github.com>
2025-01-21 17:55:11 +0900

    Expansion of e.g.f. exp( 1 - 1/(1-5*x)^(1/5) ).

---
+++ 380/380310/380310_01.rb
@@ -0,0 +1,20 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+def A(n, m)
+  a = [1]
+  (1..n).each{|i| a << -(1..i).inject(0){|s, j| s + (0..j - 1).inject(1){|t, k| t * (m * k + 1)} * ncr(i - 1, j - 1) * a[i - j]}}
+  a
+end
+
+n = 500
+ary = A(n, 5)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
