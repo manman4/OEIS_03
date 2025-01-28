8d69fadaea6445394f0be7d965c03f923e83767c
manman4 <7023566+manman4@users.noreply.github.com>
2025-01-28 21:42:01 +0900

    Choose two different numbers x and y from 2 to 9. a(n) is the number that consists solely of the digits x or y and is the smallest number divisible by both of them.

---
+++ 380/380568/380568_01.rb
@@ -0,0 +1,51 @@
+require 'set'
+
+def A(a, b)
+  queue = [a, b]
+  result = []
+  ans = []
+
+  # 100個の数字を作る
+  while queue.max < 10 ** 13
+    # キューから最小の値を取り出す
+    current = queue.shift
+
+    # 数字が 7 と 9 の両方を含む場合だけ結果に追加
+    if current.to_s.include?(a.to_s) && current.to_s.include?(b.to_s)
+      result << current
+      if current % (a * b) == 0
+        ans << current
+        break
+      end
+    end
+
+    # 新しい値を生成してキューに追加
+    queue << current * 10 + a
+    queue << current * 10 + b
+
+    # キューを小さい順に並べ替え
+    queue.sort!
+  end
+  ans
+end
+
+ans = []
+(2..8).each{|i|
+  (i + 1..9).each{|j|
+    p [i, j, A(i, j)]
+    if A(i, j).size > 0
+      ans << [A(i, j)[0], i, j, A(i, j)[0]/(i*j)]
+    end
+  }
+}
+p ans
+
+ans = []
+(2..8).each{|i|
+  (i + 1..9).each{|j|
+    if A(i, j).size > 0
+      ans << A(i, j)[0]
+    end
+  }
+}
+p ans.sort
