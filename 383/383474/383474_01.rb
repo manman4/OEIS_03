ddf206696c8d5345cc8846f95a8e54500b5ad06a
manman4 <7023566+manman4@users.noreply.github.com>
2025-04-28 23:45:23 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals downwards, where A(n,k) is the number of lattice paths from (0,0) to (n,k) using steps (1,0),(2,0),(3,0),(0,1),(0,2),(0,3).

---
+++ 383/383474/383474_01.rb
@@ -0,0 +1,35 @@
+# T(n,k) is the number of lattice paths from (0,0) to (k,n-k) using steps (1,0),(2,0),(3,0),(0,1),(0,2),(0,3).
+def A(n, k)
+  # 配列を初期化 (k+1)*(n+1) サイズ
+  dp = Array.new(k + 1){Array.new(n + 1, 0)}
+  dp[0][0] = 1
+
+  (0..k).each{|y|
+    (0..n).each{|x|
+      next if dp[y][x] == 0
+      # (1,0) のステップ
+      dp[y][x + 1] += dp[y][x] if x + 1 <= n
+      # (2,0) のステップ
+      dp[y][x + 2] += dp[y][x] if x + 2 <= n
+      # (3,0) のステップ
+      dp[y][x + 3] += dp[y][x] if x + 3 <= n
+      # (0,1) のステップ
+      dp[y + 1][x] += dp[y][x] if y + 1 <= k
+      # (0,2) のステップ
+      dp[y + 2][x] += dp[y][x] if y + 2 <= k
+      # (0,3) のステップ
+      dp[y + 3][x] += dp[y][x] if y + 3 <= k
+    }
+  }
+  dp[k][n]
+end
+
+ary = []
+(0..10).each{|i|
+  (0..i).each{|j|
+    ary << A(j, i-j)
+  }
+}
+p ary
+
+p (0..15).map{|i| A(i, i)}
