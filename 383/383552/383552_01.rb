df743148fcc17a506b58497f106ba3b9df07b922
manman4 <7023566+manman4@users.noreply.github.com>
2025-04-30 23:59:09 +0900

    Square array A(n,k), n >= 0, k >= 0, read by antidiagonals downwards, where A(n,k) is the number of lattice paths from (0,0) to (n,k) using steps (1,0),(0,1),(2,2).

---
+++ 383/383552/383552_01.rb
@@ -0,0 +1,30 @@
+# T(n,k) is the number of lattice paths from (0,0) to (k,n-k) using steps (1,0),(0,1),(2,2).
+def A(n, k)
+  # 配列を初期化 (k+1)*(n+1) サイズ
+  dp = Array.new(k + 1){Array.new(n + 1, 0)}
+  dp[0][0] = 1
+
+  (0..k).each{|y|
+    (0..n).each{|x|
+      next if dp[y][x] == 0
+      # (1,0) のステップ
+      dp[y][x + 1] += dp[y][x] if x + 1 <= n
+      # (0,1) のステップ
+      dp[y + 1][x] += dp[y][x] if y + 1 <= k
+      # (2,2) のステップ
+      dp[y + 2][x + 2] += dp[y][x] if y + 2 <= k && x + 2 <= n
+    }
+  }
+  dp[k][n]
+end
+
+ary = []
+(0..12).each{|i|
+  (0..i).each{|j|
+    ary << A(j, i-j)
+  }
+}
+p ary
+
+# Main diagonal gives A349713.
+p (0..25).map{|i| A(i, i)}
