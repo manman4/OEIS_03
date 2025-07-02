838b80161962709c2f7d313477e587b9f46bd2b2
manman4 <7023566+manman4@users.noreply.github.com>
2025-07-02 20:02:41 +0900

    E.g.f. A(x) satisfies A(x) = exp(x*A(4*x)).

---
+++ 385/385527/385527_01.rb
@@ -0,0 +1,13 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+def A(q, n)
+  ary = [1]
+  (1..n).each{|i| ary << (0..i - 1).inject(0){|s, j| s + (j + 1) * q ** j * ncr(i - 1, j) * ary[j] * ary[i - 1 - j]}}
+  ary
+end
+def A385527(n)
+  A(4, n)
+end
+p A385527(30)
