69452bafc5ce74f0f0d172cf42e2d440d4d38e8d
manman4 <7023566+manman4@users.noreply.github.com>
2025-07-06 20:25:45 +0900

    E.g.f. A(x) satisfies A(x) = exp(x * A(-x)^2).

--- 360/360987/360987_01.gp
+++ 360/360987/360987_01.gp
@@ -3,3 +3,4 @@ M=17;
 a(n) = sum(k=0, n, (2*n-2*k+1)^(k-1)*(-2*k)^(n-k)*binomial(n, k));
 for(n=0, M, print1(a(n),", "))  
 
+
---
+++ 360/360987/360987_02.rb
@@ -0,0 +1,28 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+# a(0) = 1; a(n) = (-1)^(n-1) * (n-1)! * Sum_{i,j,k>=0 and i+j+k=n-1} (-1)^i * (n-i) * a(i) * a(j) * a(k)/(i! * j! * k!)
+def a(n, k = 3)
+  ary = [1]
+  (1..n).each{|m|
+    s = 0
+    # i,j,k>=0 and i+j+k=m-1を列挙
+    (0..m - 1).to_a.repeated_permutation(k).each{|i|
+      s += (m - i[0]).to_r * ((-1) ** i[0]) * (1..k).inject(1r){|prod, j| prod * ary[i[j - 1]] / f(i[j - 1])} if i.sum == m - 1
+    }
+    ary[m] = ((-1) ** (m - 1) * f(m - 1) * s).to_i
+  }
+  ary
+end
+
+n = 20
+ary = a(n, 3)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
