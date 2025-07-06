4d9d43ca24289f129aa53607363f6037ff5e5c5d
manman4 <7023566+manman4@users.noreply.github.com>
2025-07-07 01:06:39 +0900

    E.g.f. A(x) satisfies A(x) = exp(x * A(-x)^2).

--- 360/360987/360987_02.rb
+++ 360/360987/360987_02.rb
@@ -3,12 +3,12 @@ def f(n)
   (1..n).inject(:*)
 end
 
-# a(0) = 1; a(n) = (-1)^(n-1) * (n-1)! * Sum_{i, j, k, l>=0 and i+j+k+l=n-1} (-1)^i * (n-i) * a(i) * a(j) * a(k) * a(l)/(i! * j! * k! * l!).
+# a(0) = 1; a(n) = (-1)^(n-1) * (n-1)! * Sum_{i, j, k>=0 and i+j+k=n-1} (-1)^i * (n-i) * a(i) * a(j) * a(k)/(i! * j! * k!). 
 def a(n, k)
   ary = [1]
   (1..n).each{|m|
     s = 0
-    # i,j,k,l>=0 and i+j+k+l>=m-1を列挙
+    # i,j,k>=0 and i+j+k>=m-1を列挙
     (0..m - 1).to_a.repeated_permutation(k).each{|i|
       s += (m - i[0]) * ((-1) ** i[0]) * (1..k).inject(1r){|prod, j| prod * ary[i[j - 1]] / f(i[j - 1])} if i.sum == m - 1
     }
@@ -18,7 +18,7 @@ def a(n, k)
 end
 
 n = 17
-p ary = a(n, 4)
+p ary = a(n, 3)
 (0..n).each{|i|
   j = ary[i]
   break if j.to_s.size > 1000
