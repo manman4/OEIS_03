f4b92be9cd025ee25aae41f5a8c68fafe883b56f
manman4 <7023566+manman4@users.noreply.github.com>
2025-07-08 20:53:56 +0900

    E.g.f. A(x) satisfies A(x) = exp( x*(A(x) + A(i*x) + A(-x) + A(-i*x))/4 ), where i is the imaginary unit.

---
+++ 385/385725/385725_01.rb
@@ -0,0 +1,23 @@
+def f(n)
+  return 1 if n < 2
+  (1..n).inject(:*)
+end
+
+# a(0) = 1; a(n) = Sum_{k=0..floor((n-1)/4)} (4*k+1) * binomial(n-1,4*k) * a(4*k) * a(n-1-4*k).
+def a(n, x)
+  xs = x.size
+  ary = [1]
+  (1..n).each{|m|
+    s = 0
+    (0..m - 1).to_a.repeated_permutation(xs).each{|i|
+      # x[k] * i[k]
+      y = i.map.with_index{|v, k| x[k] * v}
+      s += (m - y[0]) * (1..xs).inject(1r){|prod , j| prod * ary[y[j- 1]] / f(y[j - 1])} if y.sum == m - 1
+    }
+    ary << (f(m - 1) * s).to_i
+  }
+  ary
+end
+
+n = 25
+p a(n, [1, 4]) 
