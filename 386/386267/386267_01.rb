d014697bae5887deb088e7fd2a5808a79086784b
manman4 <7023566+manman4@users.noreply.github.com>
2025-07-17 21:45:53 +0900

    If k appears, k^2 does not.

--- 386/386267/386267_01.rb
+++ 386/386267/386267_01.rb
@@ -3,11 +3,18 @@ def A386267(n)
   ary = []
   while ary.size < n
     j = Math.sqrt(i).to_i
-    if j * j != i or !ary.include?(j)
+    if j * j != i || !ary.include?(j)
       ary << i
     end
     i += 1
   end
   ary
 end
-p A386267(100)
+
+n = 10000
+ary = A386267(n)
+(1..n).each{|i|
+  print i
+  print ' '
+  puts ary[i - 1]
+}
