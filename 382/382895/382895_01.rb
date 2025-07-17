674bc90b2dcb1db55b6ab59a8d52023f1dbe728a
manman4 <7023566+manman4@users.noreply.github.com>
2025-07-17 22:01:44 +0900

    Divide n successively by its nonzero digits from most to least significant, updating the result at each step and skipping any digit that doesn't divide the current value exactly.

--- 382/382895/382895_01.rb
+++ 382/382895/382895_01.rb
@@ -1,10 +1,11 @@
 def A(n)
   m = n
   n.to_s.split('').map(&:to_i).each{|i|
-    m /= i if i != 0 and m % i == 0
+    m /= i if i != 0 && m % i == 0
   }
   m
 end
+
 def A382895(n)
   (1..n).map{|i| A(i)}
 end
