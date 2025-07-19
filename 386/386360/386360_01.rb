6af2b1dd9c1caa427f4ea0b99ef7bdab338821c4
manman4 <7023566+manman4@users.noreply.github.com>
2025-07-19 23:58:37 +0900

    Number of partitions of n such that the least part occurs exactly (1/3)*(number of parts) times.

---
+++ 386/386360/386360_01.rb
@@ -0,0 +1,15 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+def A(n, k)
+  cnt = 0
+  partition(n, 1, n).each{|ary|
+    cnt += 1 if k * ary.count(ary.min) == ary.size
+  }
+  cnt
+end
+def A386360(n)
+  (1..n).map{|i| A(i, 3)}
+end
+p A386360(55)
