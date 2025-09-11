8867fbfc50aa08c80c89948cb60d16f2505263a7
manman4 <7023566+manman4@users.noreply.github.com>
2025-09-11 15:37:00 +0900

    a(n) = Sum_{k=0..n} binomial(4*n,5*k).

---
+++ 387/387848/387848_01.rb
@@ -0,0 +1,19 @@
+# a(n) = 5*a(n-1) + 130*a(n-2) + 740*a(n-3) - 65*a(n-4) + 16*a(n-5) for n > 4.
+def A(n)
+  ary = [1,1,57,859,12393]
+  (4..n).each{|i|
+    ary << 5 * ary[-1] + 130 * ary[-2] + 740 * ary[-3] - 65 * ary[-4] + 16 * ary[-5]
+  }
+  ary
+end
+
+n = 1100
+m = 1000
+ary = A(n)
+(0..m).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
