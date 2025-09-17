6df04550165dfc390c9189affc964c0e25643b64
manman4 <7023566+manman4@users.noreply.github.com>
2025-09-17 23:59:06 +0900

    a(n) = Sum_{k=0..n} 3^(n-k) * binomial(n,k) * binomial(n+4,k).

---
+++ 388/388411/388411_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=0..n} 3^(n-k) * binomial(n,k) * binomial(n+4,k).
+def A(n)
+  (0..n).inject(0){|sum, i| sum + 3**(n - i) * ncr(n, i) * ncr(n + 4, i)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.size > 1000
+  print i
+  print ' '
+  puts j
+}
