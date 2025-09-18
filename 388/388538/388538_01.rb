82e283abec586de02f704829774441cc5d81a6c6
manman4 <7023566+manman4@users.noreply.github.com>
2025-09-18 22:56:18 +0900

    Expansion of (1/x) * Series_Reversion( x / ((1+x)^6 + x^2) ).

---
+++ 388/388538/388538_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = (1/(n+1)) * Sum_{k=0..floor(n/2)} binomial(n+1,k) * binomial(6*n-6*k+6,n-2*k).
+def A(n)
+  (0..n / 2).inject(0){|sum, i| sum + ncr(n + 1, i) * ncr(6 * n - 6 * i + 6, n - 2 * i)} / (n + 1)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.size > 1000
+  print i
+  print ' '
+  puts j
+}
