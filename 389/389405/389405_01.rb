2bc8cca5d4eb339385fde7f17a47642ec5891a82
manman4 <7023566+manman4@users.noreply.github.com>
2025-10-03 22:41:20 +0900

    Expansion of (1/x) * Series_Reversion( x * (1 - x - x^5 / (1 - x)) ).

---
+++ 389/389405/389405_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = (1/(n+1)) * Sum_{k=0..floor(n/5)} binomial(n+k,k) * binomial(2*n-3*k,n-5*k).
+def A(n)
+  (0..n / 5).inject(0){|sum, k| sum + ncr(n + k, k) * ncr(2 * n - 3 * k, n - 5 * k)} / (n + 1)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
