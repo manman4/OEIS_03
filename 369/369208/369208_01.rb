7f1f97090d2be258897a89987e6643fcbec90b5e
manman4 <7023566+manman4@users.noreply.github.com>
2025-10-08 19:14:57 +0900

    Expansion of (1/x) * Series_Reversion( x * (1-x)^2 / (1+x^2) ).

---
+++ 369/369208/369208_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = (1/(n+1)) * Sum_{k=0..floor(n/2)} binomial(n+1,k) * binomial(3*n-2*k+1,n-2*k).
+def A(n)
+  (0..n / 2).inject(0){|sum, k| sum + ncr(n + 1, k) * ncr(3 * n - 2 * k + 1, n - 2 * k)} / (n + 1)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
