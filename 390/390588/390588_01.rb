cfcf78c34a62df37b09ec9ed41163dbad2524a80
manman4 <7023566+manman4@users.noreply.github.com>
2025-11-13 00:10:53 +0900

    a(n) = Sum_{k=0..n} (-1)^k * binomial(3*n-k+3,n-k).

---
+++ 390/390588/390588_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=0..floor(n/2)} binomial(3*n-2*k+2,n-2*k).
+def a(n)
+  (0..n / 2).inject(0){|s, k| s + ncr(3 * n - 2 * k + 2, n - 2 * k)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = a(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
