3444bb568adb7ce9d9f4f3cfaf2c41da7309ac03
manman4 <7023566+manman4@users.noreply.github.com>
2025-11-27 00:27:30 +0900

    Expansion of g/(1 - x^3*g^3), where g = 1+x*g^2 is the g.f. of A000108.

---
+++ 391/391033/391033_01.rb
@@ -0,0 +1,19 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = (1/(n+1)) * Sum_{k=0..floor(n/3)} (3*k+1) * binomial(2*n-3*k,n-3*k).
+def A391033(n)
+  (0..n / 3).inject(0){|s, i| s + (3 * i + 1) * ncr(2 * n - 3 * i, n - 3 * i)} / (n + 1)
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A391033(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+  
