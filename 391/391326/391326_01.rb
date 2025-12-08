1278fcf0c4362df8d3fb6830791b2e80083ba878
manman4 <7023566+manman4@users.noreply.github.com>
2025-12-08 23:56:44 +0900

    Expansion of g/(1 + x^3*g^3), where g = 1+x*g^4 is the g.f. of A002293.

---
+++ 391/391326/391326_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=0..floor(n/3)} (-1)^k * (3*k+1) * binomial(4*n-9*k+1,n-3*k)/(4*n-9*k+1).
+def a(n)
+  (0..n / 3).inject(0){|s, k| s + (-1)**k * (3 * k + 1) * ncr(4 * n - 9 * k + 1, n - 3 * k) / (4 * n - 9 * k + 1r)}.to_i
+end
+
+n = 1000
+(0..n).each{|i| 
+  j = a(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
