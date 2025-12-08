abb8f3354ec51fb11e6e221fee05f3ee648b6798
manman4 <7023566+manman4@users.noreply.github.com>
2025-12-08 23:51:38 +0900

    Expansion of g/(1 + x^2*g), where g = 1+x*g^4 is the g.f. of A002293.

---
+++ 387/387639/387639_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=0..floor(n/2)} (-1)^k * (k+1) * binomial(4*n-7*k+1,n-2*k)/(4*n-7*k+1).
+def a(n)
+  (0..n / 2).inject(0){|s, k| s + (-1)**k * (k + 1) * ncr(4 * n - 7 * k + 1, n - 2 * k) / (4 * n - 7 * k + 1r)}.to_i
+end
+
+n = 1000
+(0..n).each{|i| 
+  j = a(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
