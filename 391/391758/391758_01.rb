0f9d3ea202b30d8a45509b54f00047c8f4e724bb
manman4 <7023566+manman4@users.noreply.github.com>
2025-12-19 22:40:43 +0900

    a(n) = Sum_{k=0..floor(n/4)} (n-3*k+1) * 2^k * 3^(n-3*k) * binomial(2*(n-3*k),2*k).

---
+++ 391/391758/391758_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=0..floor(n/4)} (n-3*k+1) * 2^k * 3^(n-3*k) * binomial(2*(n-3*k),2*k).
+def a(n)
+  (0..n / 4).inject(0){|s, k| s + (n - 3 * k + 1) * 2**k * 3**(n - 3 * k) * ncr(2 * (n - 3 * k), 2 * k)}
+end
+
+n = 1000
+(0..n).each{|i| 
+  j = a(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
