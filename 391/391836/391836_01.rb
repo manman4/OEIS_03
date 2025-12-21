997c25bdcf0bd3f15ba99d7f2c7b5079d96846e5
manman4 <7023566+manman4@users.noreply.github.com>
2025-12-21 19:45:12 +0900

    a(n) = (1/4) * Sum_{k=0..floor(n/2)} (k+2) * 2^(n-2*k) * binomial(2*k+2,2*n-4*k+1).

---
+++ 391/391836/391836_01.rb
@@ -0,0 +1,18 @@
+# a(n) = 4*a(n-2) + 8*a(n-3) - 6*a(n-4) - 8*a(n-5) - 20*a(n-6) - 8*a(n-7) - 17*a(n-8) + 40*a(n-9) - 24*a(n-10) + 32*a(n-11) - 16*a(n-12).
+def a(n)
+  ary = [1, 0, 3, 6, 6, 40, 34, 140, 295, 440, 1533, 2210]
+  (12..n).each{|i|
+    ary << 4*ary[-2] + 8*ary[-3] - 6*ary[-4] - 8*ary[-5] - 20*ary[-6] - 8*ary[-7] - 17*ary[-8] + 40*ary[-9] - 24*ary[-10] + 32*ary[-11] - 16*ary[-12]
+  }
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
