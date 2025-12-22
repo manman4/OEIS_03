159fa02aa1838ee8afffc4e395bf10ef0825d4db
manman4 <7023566+manman4@users.noreply.github.com>
2025-12-23 00:26:56 +0900

    a(n) = Sum_{k=0..n} (k+1) * 2^(n-k) * binomial(2*k,2*(n-k)).

---
+++ 390/390732/390732_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=0..n} (k+1) * 2^(n-k) * binomial(2*k,2*(n-k)).
+def a(n)
+  (0..n).inject(0){|s, k| s + (k+1) * 2**(n-k) * ncr(2 * k, 2 * (n - k))}
+end
+
+n = 1000
+(0..n).each{|i| 
+  j = a(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
