926758ea6bdb9149a4b736070efaad8ae1976fd7
manman4 <7023566+manman4@users.noreply.github.com>
2025-12-23 19:18:42 +0900

    a(n) = Sum_{k=0..n} (k+1) * 2^k * binomial(2*k+1,2*n-2*k+1).

---
+++ 391/391894/391894_01.rb
@@ -0,0 +1,18 @@
+# a(n) = 8*a(n-1) - 16*a(n-2) + 16*a(n-3) - 72*a(n-4) + 32*a(n-5) - 64*a(n-6) + 64*a(n-7) - 16*a(n-8).
+def a(n)
+  ary = [1, 12, 64, 344, 1852, 9504, 47616, 235072]
+  (8..n).each{|i|
+    ary << 8*ary[-1] - 16*ary[-2] + 16*ary[-3] - 72*ary[-4] + 32*ary[-5] - 64*ary[-6] + 64*ary[-7] - 16*ary[-8]
+  }
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
