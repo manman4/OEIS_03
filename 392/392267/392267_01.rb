fbc50b1c07c7a368428d2ce75c2f50ff2d4a6d42
manman4 <7023566+manman4@users.noreply.github.com>
2026-01-06 18:55:53 +0900

    a(n) = Sum_{k=0..floor(2*n/3)} (k+1) * binomial(k,2*n-3*k).

---
+++ 392/392267/392267_01.rb
@@ -0,0 +1,18 @@
+# a(n) = 4*a(n-2) + 2*a(n-3) - 6*a(n-4) - 4*a(n-5) + 3*a(n-6) + 2*a(n-7) - a(n-8).
+def a(n)
+  ary = [1, 0, 2, 3, 3, 12, 9, 30]
+  (8..n).each{|i|
+    ary << 4*ary[-2] + 2*ary[-3] - 6*ary[-4] - 4*ary[-5] + 3*ary[-6] + 2*ary[-7] - ary[-8]
+  }
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
