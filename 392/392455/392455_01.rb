2cdaf4642af2e5d612cfa6a191026f805dbd99e7
manman4 <7023566+manman4@users.noreply.github.com>
2026-01-14 09:33:11 +0900

    a(n) = Sum_{k=0..floor(2*n/5)} binomial(2k+1,2*n-5*k).

---
+++ 392/392455/392455_01.rb
@@ -0,0 +1,18 @@
+# a(n) = 4*a(n-3) + a(n-5) - 2*a(n-6) + a(n-7).
+def a(n)
+  ary = [1, 0, 0, 3, 1, 1, 10]
+  (7..n).each{|i|
+    ary << 4*ary[-3] + ary[-5] - 2*ary[-6] + ary[-7]
+  }
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
