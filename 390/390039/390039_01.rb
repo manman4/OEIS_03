dd0f68ca37a63256f18a2b6d6ab50ad2fd1179e3
manman4 <7023566+manman4@users.noreply.github.com>
2026-01-15 19:25:35 +0900

    a(n) = Sum_{k=0..floor(4*n/7)} binomial(k+3,4*n-7*k).

---
+++ 390/390039/390039_01.rb
@@ -0,0 +1,18 @@
+# a(n) = 4*a(n-2) - 6*a(n-4) + 4*a(n-6) + a(n-7) - a(n-8).
+def a(n)
+  ary = [1, 0, 4, 0, 10, 0, 20, 1]
+  (8..n).each{|i|
+    ary << 4*ary[-2] - 6*ary[-4] + 4*ary[-6] + ary[-7] - ary[-8]
+  }
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
