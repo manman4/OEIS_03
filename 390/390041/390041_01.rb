b97b3e094ce7bf66574817466f63d51f686936d2
manman4 <7023566+manman4@users.noreply.github.com>
2026-01-15 19:25:05 +0900

    a(n) = Sum_{k=0..floor(4*n/11)} binomial(k+3,4*n-11*k).

---
+++ 390/390041/390041_01.rb
@@ -0,0 +1,18 @@
+# a(n) = 4*a(n-3) - 6*a(n-6) + 4*a(n-9) + a(n-11) - a(n-12).
+def a(n)
+  ary = [1, 0, 0, 4, 0, 0, 10, 0, 0, 20, 0, 1]
+  (12..n).each{|i|
+    ary << 4*ary[-3] - 6*ary[-6] + 4*ary[-9] + ary[-11] - ary[-12]
+  }
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
