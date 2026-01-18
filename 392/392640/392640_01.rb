07373a14378bef4cc4fab7b43f528bcbda6654bd
manman4 <7023566+manman4@users.noreply.github.com>
2026-01-19 08:34:24 +0900

    Expansion of 1 / sqrt((1-x)^4 - 4*x^3).

---
+++ 392/392640/392640_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=0..floor(n/3)} binomial(2*k,k) * binomial(n+k+1,n-3*k).
+def a(n)
+  (0..n / 3).inject(0){|s, k| s + ncr(2 * k, k) * ncr(n + k + 1, n - 3 * k)}
+end
+
+n = 1000
+(0..n).each{|i| 
+  j = a(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
