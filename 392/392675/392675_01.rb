06a81152848ae792b9fe5199a39d5c0fe9ccb6c8
manman4 <7023566+manman4@users.noreply.github.com>
2026-01-20 00:01:06 +0900

    a(n) = Sum_{k=0..floor(n/3)} binomial(k+1,3*n-9*k+1).

---
+++ 392/392675/392675_01.rb
@@ -0,0 +1,18 @@
+# a(n) = 3*a(n-3) - 3*a(n-6) + a(n-9) + a(n-10).
+def a(n)
+  ary = [1, 0, 0, 2, 0, 0, 3, 0, 0, 4]
+  (10..n).each{|i|
+    ary << 3*ary[-3] - 3*ary[-6] + ary[-9] + ary[-10]
+  }
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
