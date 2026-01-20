1c1c6e77e0239d41ab8d3f9ba3a2ed7e9434fa22
manman4 <7023566+manman4@users.noreply.github.com>
2026-01-20 21:20:16 +0900

    a(n) = Sum_{k=0..floor(n/2)} binomial(k+2,4*n-8*k+2).

---
+++ 390/390222/390222_01.rb
@@ -0,0 +1,18 @@
+# a(n) = 4*a(n-2) - 6*a(n-4) + 4*a(n-6) - a(n-8) + a(n-9).
+def a(n)
+  ary = [1, 0, 3, 0, 6, 0, 10, 0, 15]
+  (9..n).each{|i|
+    ary << 4*ary[-2] - 6*ary[-4] + 4*ary[-6] - ary[-8] + ary[-9]
+  }
+  ary
+end
+
+n = 1000
+ary = a(n)
+(0..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+} 
