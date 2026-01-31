1a632382bce1a2c078f5b575b42f2ddb1f1b7ffd
manman4 <7023566+manman4@users.noreply.github.com>
2026-01-31 20:09:29 +0900

    G.f. A(x) satisfies A(x) = x/(1-x) + Sum_{n>=1} A(x^n)^3.

---
+++ 382/382319/382319_01.rb
@@ -0,0 +1,33 @@
+# a(0) = 1
+# A382319(n) = Sum_{d|n} a(d)
+# a(n) = Sum_{i,j,k >= 1 and i + j + k = n} A382319(i) * A382319(j) * A382319(k) for n>1.
+def a(n)
+  ary =   [0, 1]
+  s_ary = [0, 1]
+  (2..n).each{|i|
+    s = 0
+    (1..i - 1).each{|j|
+      (1..i - 1 - j).each{|k|
+        s += s_ary[j] * s_ary[k] * s_ary[i - j - k]
+      }
+    }
+    ary << s
+    sum_div = 0
+    (1..i).each{|j|
+      sum_div += ary[j] if i % j == 0
+    }
+    s_ary << sum_div
+    # p [i, s_ary]
+  }
+  s_ary
+end
+
+n = 1000
+ary = a(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
