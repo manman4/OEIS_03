f24a907dd6cb458be5b137942da82fc7860b215e
manman4 <7023566+manman4@users.noreply.github.com>
2026-03-06 22:22:02 +0900

    Number of partitions p of n into distinct parts such that max(p) >= 3*min(p).

---
+++ 393/393986/393986_01.rb
@@ -0,0 +1,19 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i - 1).map{|rest| [i, *rest]}}
+end
+def A(n, k)
+  cnt = 0
+  partition(n, 1, n).each{|ary|
+    # max(ary) >= k * min(ary)
+    if ary.max >= k * ary.min
+      # p ary
+      cnt += 1 
+    end
+  }
+  cnt
+end
+def A393986(n)
+  (1..n).map{|i| A(i, 3)}
+end
+p A393986(38)
