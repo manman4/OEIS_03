89710589f54fa1278ae007293db11eaaf4def1b4
manman4 <7023566+manman4@users.noreply.github.com>
2026-03-16 23:03:06 +0900

    Number of partitions p of n such that (maximal multiplicity of the parts of p) <= 2 * (minimal part of p).

---
+++ 393/393320/393320_01.rb
@@ -0,0 +1,29 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+# Number of partitions p of n such that (maximal multiplicity of the parts of p) <= m * (minimal part of p).
+def A(n, m)
+  cnt = 0
+  partition(n, 1, n).each{|ary|
+    s = ary.min
+    max_multiplicity = ary.map{|x| ary.count(x)}.max
+    if max_multiplicity <= m * s
+      # p ary.join
+      cnt += 1
+    end
+  }
+  cnt
+end
+def A393320(n)
+  [0] + (1..n).map{|i| A(i, 2)}
+end
+n = 50
+p ary = A393320(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
