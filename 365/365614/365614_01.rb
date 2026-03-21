24dceb2aae0044b2a648774602595745ff142e88
manman4 <7023566+manman4@users.noreply.github.com>
2026-03-22 00:16:10 +0900

    a(n) = number of partitions p of n such that the least multiplicity of the parts of p is a part of p.

--- 365/365614/365614_01.rb
+++ 365/365614/365614_01.rb
@@ -6,7 +6,7 @@ end
 def A(n, m)
   cnt = 0
   partition(n, 1, n).each{|ary|
-    min_multiplicity = ary.group_by(&:itself).values.map(&:size).min
+    min_multiplicity = ary.tally.values.min
     if ary.include?(m * min_multiplicity)
       # p ary.join
       cnt += 1
@@ -14,20 +14,20 @@ def A(n, m)
   }
   cnt
 end
-def A394325(n)
-  [0] + (1..n).map{|i| A(i, 2)}
+def A365614(n)
+  [0] + (1..n).map{|i| A(i, 1)}
 end
-n = 40
-p ary = A394325(n)
-(1..n).each{|i|
-  j = ary[i]
-  break if j.to_s.size > 1000
-  print i
-  print ' '
-  puts j
-}
+n = 50
+p ary = A365614(n)
+# (1..n).each{|i|
+#   j = ary[i]
+#   break if j.to_s.size > 1000
+#   print i
+#   print ' '
+#   puts j
+# }
 
-p (1..n).map{|i| A(i, 1)}
-p (1..n).map{|i| A(i, 2)}
-p (1..n).map{|i| A(i, 3)}
-p (1..n).map{|i| A(i, 4)}
+# p (1..n).map{|i| A(i, 1)}
+# p (1..n).map{|i| A(i, 2)}
+# p (1..n).map{|i| A(i, 3)}
+# p (1..n).map{|i| A(i, 4)}
