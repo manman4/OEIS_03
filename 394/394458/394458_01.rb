c01834b3ef1f2dcad791d377d13c18184e346a20
manman4 <7023566+manman4@users.noreply.github.com>
2026-03-22 01:52:36 +0900

    Number of partitions p of n such that 2 * (the greatest multiplicity of the parts of p) is a part of p.

---
+++ 394/394458/394458_01.rb
@@ -0,0 +1,33 @@
+def partition(n, min, max)
+  return [[]] if n == 0
+  [max, n].min.downto(min).flat_map{|i| partition(n - i, min, i).map{|rest| [i, *rest]}}
+end
+# number of partitions p of n such that m * (the greatest multiplicity of the parts of p) is a part of p.
+def A(n, m)
+  cnt = 0
+  partition(n, 1, n).each{|ary|
+    max_multiplicity = ary.tally.values.max
+    if ary.include?(m * max_multiplicity)
+      # p ary.join
+      cnt += 1
+    end
+  }
+  cnt
+end
+def A394458(n)
+  [0] + (1..n).map{|i| A(i, 2)}
+end
+n = 50
+p ary = A394458(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
+
+# p (1..n).map{|i| A(i, 1)}
+# p (1..n).map{|i| A(i, 2)}
+# p (1..n).map{|i| A(i, 3)}
+# p (1..n).map{|i| A(i, 4)}
