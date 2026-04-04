b74bd3cb95c026d105a3c09a446fffe381e56ec4
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-04 21:32:26 +0900

    G.f. A(x) satisfies A(x) = 1 / (1 - x/4 * d/dx log(1 + 4*x*A(x))).

---
+++ 394/394845/394845_01.rb
@@ -0,0 +1,16 @@
+# a(0) = 1; a(n) = 4*a(n-1) + (n-7)/2 * Sum_{k=0..n-1} a(k) * a(n-1-k).
+def A(n)
+  ary = [1]
+  (1..n).each{|i| ary << 4 * ary[-1] + (0..i - 1).inject(0){|s, j| s + ary[j] * ary[i - 1 - j]} * (i - 7) / 2}
+  ary
+end
+
+n = 600
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
