1c06277255d8d784770f12255ec6bed842d5833a
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-06 07:43:17 +0900

    G.f. A(x) satisfies A(x) = 1 / (1 + x - x^2 + x^3 - x^4 - x * d/dx log(1 + x*A(x))).

---
+++ 394/394880/394880_01.rb
@@ -0,0 +1,26 @@
+# a(0) = 1, a(1) = a(2) = a(3) = a(4) = 0; a(n) = (n-1)/2 * a(n-1) + a(n-5) + (n-1)/2 * b(n-1) - b(n-2) + b(n-3) - b(n-4) + b(n-5), where b(n) = Sum_{k=1..n} a(k)*a(n-k).
+def a(n)
+  a = [1, 0, 0, 0, 0]
+  b = [ ]
+  (0..4).each{|i| b << (1..i - 1).inject(0){|s, k| s + a[k] * a[i - 1 - k]}}
+  (5..n).each{|i| 
+    b << (1..i - 1).inject(0){|s, k| s + a[k] * a[i - 1 - k]}
+    a << (i - 1) / 2r * a[-1] + a[-5] + ((i - 1) / 2r * b[-1] - b[-2] + b[-3] - b[-4] + b[-5])
+  }
+  a
+end
+
+n = 600
+ary = a(n)
+# b = [ ]
+(0..n).each{|i|
+  j = ary[i]
+  break if j.denominator > 1
+  j = j.to_i
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+  # b << j
+}
+# p b
