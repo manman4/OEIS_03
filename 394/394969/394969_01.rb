217219969378177d26b7295b56d3bee72843c15c
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-09 17:41:18 +0900

    G.f. A(x) satisfies [x^n] exp( n*x/(1-n^2*x) - n*A(x) ) = 0 for n >= 1.

--- 394/394969/394969_01.rb
+++ 394/394969/394969_01.rb
@@ -1,16 +1,18 @@
-# a(n)   =   n^(n-1) + (1/n) * Sum_{k=1..n-1} k * b_n(k) * e_n(n-k)
-# b_n(k) =   n^(k-1) - a(k)
-# e_n(0) = 1; e_n(k) = (n/k) * Sum_{j=1..k}   j * b_n(j) * e_n(k-j)
+# a(n) = n^(2*(n-1)) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = n^(2*(k-1)) - a(k) for 1 <= k <= n-1,
+# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
+
 def A394969(n)
   a = [0]
   (1..n).each{|i|
-    b = (0..i - 1).map{|k| i ** (2 * (k - 1)) - a[k]}
+    c = [0] + (1..i - 1).map{|k| i ** (2 * (k - 1)) - a[k]}
     e = [1]
-    (1..i - 1).each{|k| e << i.to_r / k * (1..k).inject(0){|s, j| s + j * b[j] * e[k - j]}}
-    a << i ** (2 * (i - 1)) + (1..i - 1).inject(0){|s, k| s + k * b[k] * e[i - k]}.to_i / i
+    (1..i - 1).each{|k| e << i.to_r / k * (1..k).inject(0){|s, j| s + j * c[j] * e[k - j]}}
+    a << i ** (2 * (i - 1)) + (1..i - 1).inject(0){|s, k| s + k * c[k] * e[i - k]}.to_i / i
   }
   a[1..-1]
 end
+
 p A394969(20)
 
 # p (1..n).map{|i| ary[i - 1] / i.to_r}
