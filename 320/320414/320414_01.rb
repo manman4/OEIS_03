144fa15f08df992d7b64f207f28dbf8c3cec97eb
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-10 21:38:59 +0900

    O.g.f. A(x) satisfies [x^n] exp(-n*A(x)) / (1 - n^2*x) = 0 for n >= 1.

--- 320/320414/320414_01.rb
+++ 320/320414/320414_01.rb
@@ -1,6 +1,6 @@
-# a(n) = n^(2*n-1) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
-# where c_n(k) = n^(2*k)/k - a(k) for 1 <= k <= n-1,
-# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
+# a(n) = n^(2*n-2) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = n^(2*k-1)/k - a(k) for 1 <= k <= n-1,
+# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1. 
 
 def A320414(n)
   a = [0]
@@ -12,7 +12,6 @@ def A320414(n)
   }
   a[1..-1]
 end
-
 p A320414(20)
 
 # n = 20
