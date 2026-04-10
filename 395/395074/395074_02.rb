53c9116172824342ced4a618c299b732c20ba84f
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-11 08:48:39 +0900

    G.f. A(x) satisfies [x^n] exp( -n*A(x) ) / (1-n^2*x)^(1/n) = 0, for n >= 1.

---
+++ 395/395074/395074_02.rb
@@ -0,0 +1,27 @@
+# a(n) = n^(2*n-1) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = n^(2*k)/k - a(k) for 1 <= k <= n-1,
+# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
+
+def A395024(n)
+  a = [0]
+  (1..n).each{|i|
+    c = [0] + (1..i - 1).map{|k| i ** (2 * k - 2) / k.to_r - a[k]}
+    e = [1]
+    (1..i - 1).each{|k| e << i.to_r / k * (1..k).inject(0){|s, j| s + j * c[j] * e[k - j]}}
+    a << i ** (2 * i - 3) + (1..i - 1).inject(0){|s, k| s + k * c[k] * e[i - k]} / i
+  }
+  a[1..-1]
+end
+n = 100
+ary = A395024(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  if j.denominator > 1
+    break
+  end
+  j = j.to_i.to_s
+  break if j.size > 1000
+  print i
+  print ' '
+  puts j
+}
