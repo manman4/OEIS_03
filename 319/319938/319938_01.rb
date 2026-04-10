56250cd2b49d60cfb3daabed9965141434a88481
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-10 21:29:12 +0900

    O.g.f. A(x) satisfies [x^n] exp(-n*A(x)) / (1 - n*x) = 0, for n > 0.

--- 319/319938/319938_01.rb
+++ 319/319938/319938_01.rb
@@ -1,6 +1,6 @@
 # a(n) = n^(n-2) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
 # where c_n(k) = n^(k-1)/k - a(k) for 1 <= k <= n-1,
-# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
+# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1. 
 
 def A319938(n)
   a = [0]
@@ -12,8 +12,7 @@ def A319938(n)
   }
   a[1..-1]
 end
-
-p A319938(30) 
+p A319938(30)
 
 # n = 20
 # p ary = A319938(n)
