5839468afbe7cf9f9eb2a9af769f3b3378e6673f
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-10 17:43:58 +0900

    G.f. A(x) satisfies [x^n] exp( -n*A(x) ) / (1 - n^2*x)^n = 0, for n >= 1.

--- 395/395024/395024_01.rb
+++ 395/395024/395024_01.rb
@@ -1,8 +1,8 @@
-# a(n) = n^(n-1) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
-# where c_n(k) = n^k/k - a(k) for 1 <= k <= n-1,
+# a(n) = n^(2*n-1) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = n^(2*k)/k - a(k) for 1 <= k <= n-1,
 # and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
 
-def A319940(n)
+def A395024(n)
   a = [0]
   (1..n).each{|i|
     c = [0] + (1..i - 1).map{|k| i ** (2 * k) / k.to_r - a[k]}
@@ -13,10 +13,10 @@ def A319940(n)
   a[1..-1]
 end
 
-p A319940(20)
+p A395024(20)
 
 # n = 20
-# p ary = A319940(n)
+# p ary = A395024(n)
 # (1..n).each{|i|
 #   j = ary[i - 1].denominator
 #   if j > 1
