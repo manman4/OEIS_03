7d3fd3e21ea416be317af545c961cd2e1628931d
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-12 02:11:06 +0900

    a(0) = 1; a(n) = Sum_[k=0..n-1} (-1)^(n-k-1) * binomial(n,k) * (k+3)^(3*(n-k)) * a(k).

---
+++ 395/395086/395086_01.rb
@@ -0,0 +1,21 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(0) = 1; a(n) = Sum_[k=0..n-1} (-1)^(n-k-1) * binomial(n,k) * (k+3)^(3*(n-k)) * a(k).
+def A(n)
+  ary = [1]
+  (1..n).each{|i| ary << (0..i-1).inject(0){|s, j| s + (-1)**(i-j-1) * ncr(i, j) * (j+3)**(3*(i-j)) * ary[j]}}
+  ary
+end
+
+n = 300
+ary = A(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
