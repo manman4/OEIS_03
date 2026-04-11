26ff0879d75643f72081e1653dde81060f7ea656
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-11 13:39:59 +0900

    G.f. A(x) satisfies [x^n] exp( -n*A(x) ) / (1-n^2*x)^(1/n) = 0, for n >= 1.

--- 395/395074/395074_01.rb
+++ 395/395074/395074_01.rb
@@ -1,5 +1,5 @@
-# a(n) = n^(2*n-1) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
-# where c_n(k) = n^(2*k)/k - a(k) for 1 <= k <= n-1,
+# a(n) = n^(2*n-3) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = n^(2*k-2)/k - a(k) for 1 <= k <= n-1,
 # and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
 
 def A395074(n)
