9c44f13cb508ab991b161d8cec44951cabef1695
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-12 22:12:18 +0900

    O.g.f. A(x) satisfies [x^n] exp( n^3 * A(x) ) = n^3 * [x^(n-1)] exp( n^3 * A(x) ) for n >= 1.

--- 300/300595/300595_01.rb
+++ 300/300595/300595_01.rb
@@ -1,6 +1,6 @@
-# a(n) = n^(2*n-3) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
-# where c_n(k) = n^(2*k-2)/k - a(k) for 1 <= k <= n-1,
-# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
+# a(n) = n^(3*n-4) - (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = a(k) - n^(3*k-3)/k for 1 <= k <= n-1,
+# and e_n(0) = 1, e_n(k) = (n^3/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
 
 def A300595(n)
   a = [0]
