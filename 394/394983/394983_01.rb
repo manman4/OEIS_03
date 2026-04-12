2b70169581e0db7368101b6c4c8ff51e76b29afd
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-12 20:45:02 +0900

    G.f. A(x) satisfies [x^n] exp( n*x*(1+n^2*x+(n^2*x)^2) - n*A(x) ) = 0 for n >= 1.

--- 394/394983/394983_01.rb
+++ 394/394983/394983_01.rb
@@ -4,7 +4,7 @@ def A394983(n)
     b = [0] + (1..i).map{|k| k > 3 ? 0 : i ** (2 * (k - 1))}
     c = [0] + (1..i - 1).map{|k| b[k] - a[k]}
     e = [1]
-    (1..i - 1).each{|k| e << i.to_r / k * (1..k).inject(0){|s, j| s + j * c[j] * e[k - j]}}
+    (1..i - 1).each{|k| e << i / k.to_r * (1..k).inject(0){|s, j| s + j * c[j] * e[k - j]}}
     a << b[i] + (1..i - 1).inject(0){|s, k| s + k * c[k] * e[i - k]}.to_i / i
   }
   a[1..-1]
