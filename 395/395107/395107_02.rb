2d766ff2a8aab125eb1d85f66eac563624fab47a
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-12 19:03:03 +0900

    G.f. A(x) satisfies [x^n] exp( n*A(x) ) * (1-n^3*x)^(1/n^2) = 0, for n >= 1.

---
+++ 395/395107/395107_02.rb
@@ -0,0 +1,28 @@
+# a(n) = n^(3*n-4) - (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = a(k) - n^(3*k-3)/k for 1 <= k <= n-1,
+# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
+
+def A395107(n)
+  a = [0]
+  (1..n).each{|i|
+    c = [0] + (1..i - 1).map{|k| a[k] - i ** (3 * k - 3) / k.to_r}
+    e = [1]
+    (1..i - 1).each{|k| e << i.to_r / k * (1..k).inject(0){|s, j| s + j * c[j] * e[k - j]}}
+    a << i ** (3 * i - 4) - (1..i - 1).inject(0){|s, k| s + k * c[k] * e[i - k]} / i
+  }
+  a[1..-1]
+end
+n = 200
+ary = A395107(n)
+(1..n).each{|i|
+  j = ary[i - 1]
+  if j.denominator > 1
+    puts "a(#{i}) is not an integer."
+    break
+  end
+  j = j.to_i.to_s
+  break if j.size > 1000
+  print i
+  print ' '
+  puts j
+}
