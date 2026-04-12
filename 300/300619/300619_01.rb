b8f544d45b292c9df857b95dca481d7141154537
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-12 13:24:17 +0900

    O.g.f. A(x) satisfies [x^n] exp( n * A(x) ) = n^3 * [x^(n-1)] exp( n * A(x) ) for n>=1.

---
+++ 300/300619/300619_01.rb
@@ -0,0 +1,24 @@
+# a(n) = n^(2*n-3) + (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = n^(2*k-2)/k - a(k) for 1 <= k <= n-1,
+# and e_n(0) = 1, e_n(k) = (n/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
+
+def A300619(n)
+  a = [0]
+  (1..n).each{|i|
+    c = [0] + (1..i - 1).map{|k| a[k] - i ** (3 * k - 1) / k.to_r}
+    e = [1]
+    (1..i - 1).each{|k| e << i.to_r / k * (1..k).inject(0){|s, j| s + j * c[j] * e[k - j]}}
+    a << i ** (3 * i - 2) - (1..i - 1).inject(0){|s, k| s + k * c[k] * e[i - k]}.to_i / i
+  }
+  a[1..-1]
+end
+p A300619(20)
+
+# n = 20
+# p ary = A300619(n)
+# (1..n).each{|i|
+#   j = ary[i - 1].denominator
+#   if j > 1
+#     puts i
+#   end
+# }
