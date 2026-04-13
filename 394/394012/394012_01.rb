e2f2d5cd4769730d7f8b769736cde2d49a065faf
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-13 20:34:29 +0900

    Triangle of coefficients T(n,k) arising from the odd-order derivatives of the solution to the differential equation S'(x) = (1+S(x)^2)^2.

--- 394/394012/394012_01.rb
+++ 394/394012/394012_01.rb
@@ -1,32 +1,37 @@
+# T(1,0) = 1;
+# T(n,k) = s(k) * T(n-1,k+1) + t_{n-1}(k) * T(n-1,k) + u_{n-1}(k) * T(n-1,k-1);
+# T(n,k) = 0 if k < 0 or k >= n, 
+# where s(k) = 2*(k+1)*(2*k+1), t_n(k) = 4*(4*n*k + 2*k^2 + n + k), u_n(k) = 2*(4*n + 2*k + 1)*(2*n + k - 1).
+
 def compute_all_d_values(limit)
   # d[j] は現在の m における d_{m, j}
   # 初期条件 d_{1, 0} = 1
-  d = [1]
+  a = [1]
   
   (1..limit).each do |m|
     # m ステップ目の全係数 d_{m, j} を表示
     # puts "--- m = #{m} ---"
-    d.each_with_index do |val, j|
+    a.each_with_index do |val, j|
       # puts "d(#{m}, #{j}) = #{val}"
       print("#{val}, ")
     end
     # puts "a(#{m}) = #{d[0]}" # これが求める一般項
     # puts ""
 
-    next_d = Array.new(m + 1, 0)
+    next_a = Array.new(m + 1, 0)
     (0..m).each do |j|
       # 1. α係数: d_{m, j+1} からの寄与
-      alpha_term = (j + 1 < d.size) ? 2 * (j + 1) * (2 * j + 1) * d[j + 1] : 0
+      alpha_term = (j + 1 < a.size) ? 2 * (j + 1) * (2 * j + 1) * a[j + 1] : 0
       
       # 2. β係数: d_{m, j} からの寄与
-      beta_term = (j < d.size) ? 4 * (4 * m * j + 2 * j**2 + m + j) * d[j] : 0
+      beta_term = (j < a.size) ? 4 * (4 * m * j + 2 * j**2 + m + j) * a[j] : 0
       
       # 3. γ係数: d_{m, j-1} からの寄与
-      gamma_term = (j > 0) ? 2 * (4 * m + 2 * j + 1) * (2 * m + j - 1) * d[j - 1] : 0
+      gamma_term = (j > 0) ? 2 * (4 * m + 2 * j + 1) * (2 * m + j - 1) * a[j - 1] : 0
       
-      next_d[j] = alpha_term + beta_term + gamma_term
+      next_a[j] = alpha_term + beta_term + gamma_term
     end
-    d = next_d
+    a = next_a
   end
 end
 
