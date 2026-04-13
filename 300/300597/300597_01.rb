d4e99efda9727a55782a890f5295d2bf4f66d7a2
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-13 18:17:44 +0900

    O.g.f. A(x) satisfies [x^n] exp( n^4 * A(x) ) = n^4 * [x^(n-1)] exp( n^4 * A(x) ) for n >= 1.

---
+++ 300/300597/300597_01.rb
@@ -0,0 +1,24 @@
+# a(n) = n^(4*n-5) - (1/n) * Sum_{k=1..n-1} k * c_n(k) * e_n(n-k),
+# where c_n(k) = a(k) - n^(4*k-4)/k for 1 <= k <= n-1,
+# and e_n(0) = 1, e_n(k) = (n^4/k) * Sum_{j=1..k} j * c_n(j) * e_n(k-j) for 1 <= k <= n-1.
+
+def A300597(n)
+  a = [0]
+  (1..n).each{|i|
+    c = [0] + (1..i - 1).map{|k| a[k] - i ** (4 * k - 4) / k.to_r}
+    e = [1]
+    (1..i - 1).each{|k| e << i ** 4 / k.to_r * (1..k).inject(0){|s, j| s + j * c[j] * e[k - j]}}
+    a << i ** (4 * i - 5) - (1..i - 1).inject(0){|s, k| s + k * c[k] * e[i - k]}.to_i / i
+  }
+  a[1..-1]
+end
+p A300597(20)
+
+# n = 20
+# p ary = A300597(n)
+# (1..n).each{|i|
+#   j = ary[i - 1].denominator
+#   if j > 1
+#     puts i
+#   end
+# }
