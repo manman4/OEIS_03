dbf9943d9ad9aceb6fd66362810cc91c9ee6cfd3
manman4 <7023566+manman4@users.noreply.github.com>
2026-04-26 18:20:27 +0900

    Number of derangements of [2*n] with n ascents.

---
+++ 395/395500/395500_02.rb
@@ -0,0 +1,45 @@
+$memo = {}
+
+def t(n, k)
+  key = [n, k]
+  return $memo[key] if $memo.key?(key)
+
+  val = if n == 0
+    k == 0 ? 1 : 0
+  else
+    term1 = (k + 1) * t(n - 1, k)
+    term2 = (n - k) * t(n - 1, k - 1)
+    term3 = ((-1) ** n) * (k == n - 1 ? 1 : 0)
+    term1 + term2 + term3
+  end
+
+  $memo[key] = val
+end
+
+# for(n=0, 12, for(k=0, n, print1(T(n,k),", ")));
+# (0..12).each{|n|
+#   (0..n).each{|k|
+#     print "#{t(n, k)}, "
+#   }
+# }
+# puts
+
+# vector(13, n, T(n-1,0..3))
+# (0..5).each{|k|
+#   arr = (1..33).map{|n| t(n - 1, k)}
+#   p arr
+# }
+def a395500(n)
+  # a(n) = A219836(2*n,n-1).
+  [1] + (1..n).map{|k| t(2 * k, k - 1)}
+end
+
+n = 300
+ary = a395500(n)
+(0..n).each{|i|
+  j = ary[i]
+  break if j.to_s.length > 1000
+  print i
+  print " "
+  puts j
+}
