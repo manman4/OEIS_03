eda2397222148558f9d76c1ec8ec5bd32c847ede
manman4 <7023566+manman4@users.noreply.github.com>
2026-05-02 16:15:42 +0900

    Coefficient of x^(2*n) in the expansion of ( (1+x)^3 * (1+x^2) )^n.

---
+++ 394/394709/394709_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = Sum_{k=0..n} binomial(n,k) * binomial(3*n,n+2*k).
+def a(n)
+  (0..n).inject(0){|s, k| s + ncr(n, k) * ncr(3*n, n + 2*k)}
+end
+
+n = 1000
+(0..n).each{|i|
+  j = a(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
