7a53b43a6d2b079af3403cbfd4b5bf6bd7812334
manman4 <7023566+manman4@users.noreply.github.com>
2026-05-13 16:11:42 +0900

    Number of 1's in the partitions of n into 4 parts.

--- 335/335583/335583_01.rb
+++ 335/335583/335583_01.rb
@@ -30,6 +30,7 @@ p (0..n).map{|i| A(i, 1)}
 p (0..n).map{|i| A(i, 2)}
 p (0..n).map{|i| A(i, 3)}
 p (0..n).map{|i| A(i, 4)}
+p (0..n).map{|i| A(i, 5)}
 
 p (1..20).map{|i| (1..i).map{|j| A(i, j)}}
 p (1..20).map{|i| (1..i).map{|j| A(i, j)}}.flatten
