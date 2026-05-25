6089c983bcacf23daaaee76bfee39dca4974c65b
manman4 <7023566+manman4@users.noreply.github.com>
2026-05-25 23:49:59 +0900

    Square array A(n,k), n>=0, k>=0, read by antidiagonals downwards, where A(n,k) = [x^n] F_k(x)/x and F_k(x) is the k-th iteration of x*G3(x) with G3(x) = 1 + x*G3(x)^3.

---
+++ 396/396412/396412_01.rb
@@ -0,0 +1,23 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# A(n,k) = Sum_{0 = x_0 <= x_1 <= ... <= x_{k-1} <= x_k = n} Product_{j=0..k-1} (x_j + 1) * binomial(3*x_{j+1} - 2*x_j + 1,x_{j+1} - x_j)/(3*x_{j+1} - 2*x_j + 1).
+# x_0 = 0, x_k = nに注意
+def a(n, k)
+  return n == 0 ? 1 : 0 if k == 0
+  sum = 0
+  (0..n).each{|x|
+    sum += a(x, k-1) * (x + 1) * ncr(3*n - 2*x + 1, n - x) / (3*n - 2*x + 1r)
+  }
+  sum.to_i
+end
+
+n = 10
+(0..n).each{|i|
+  (0..i).each{|j|
+    print a(j, i - j)
+    print ', '
+  }
+}
