cb24a784b1db7ba21d3726bbebd89b82f6512276
manman4 <7023566+manman4@users.noreply.github.com>
2026-05-30 22:20:21 +0900

    Expansion of e.g.f. F_2(x)/x and F_k(x) is the k-th iteration of x*G(x) with G(x) = exp(x*G(x)^2).

--- 396/396589/396589_01.rb
+++ 396/396589/396589_01.rb
@@ -3,9 +3,9 @@ def ncr(n, r)
   (n - r + 1..n).inject(:*) / (1..r).inject(:*)
 end
 
-# a(n) = Sum_{k=0..n} (k+1) * (3*k+1)^(k-1) * (3*n-2*k+1)^(n-k-1) * binomial(n,k).
+# a(n) = Sum_{k=0..n} (k+1) * (2*k+1)^(k-1) * (2*n-k+1)^(n-k-1) * binomial(n,k).
 def a(n)
-  (0..n).inject(0){|s, k| s + (k + 1) * (3*k + 1)**(k - 1) * (3*n - 2*k + 1)**(n - k - 1) * ncr(n, k)}.to_i
+  (0..n).inject(0){|s, k| s + (k + 1) * (2*k + 1)**(k - 1) * (2*n - k + 1)**(n - k - 1) * ncr(n, k)}.to_i
 end
 
 n = 17
