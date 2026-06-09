3b74ea431981c5f181347b36bdbc19d8b292c896
manman4 <7023566+manman4@users.noreply.github.com>
2026-06-10 07:11:53 +0900

    E.g.f. satisfies A(x) = exp(-x) * A(A(x)).

---
+++ 396/396799/396799_01.py
@@ -0,0 +1,14 @@
+from math import comb
+
+def a_list(n: int) -> list[int]:
+    T = [[0] * (n + 1) for _ in range(n + 1)]
+    a = [0] * n; a[0] = 1
+    for m in range(0, n):
+        g = [0] + [j * a[j - 1] * comb(m - 1, j - 1) for j in range(1, m + 1)]
+        T[m][1] = g[m]
+        for k in range(2, m + 1):
+            T[m][k] = sum(g[j] * T[m - j][k - 1] for j in range(1, m - k + 2))
+        a[m] = 1 - sum(a[k] * T[m][k] for k in range(1, m))
+    return [T[m][1] for m in range(1, n)]
+
+print(a_list(10)) 
