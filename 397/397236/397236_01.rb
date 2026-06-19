5f36a8c34c6286308d0b041bd5e190515f313bd5
manman4 <7023566+manman4@users.noreply.github.com>
2026-06-20 02:09:22 +0900

    Expansion of 1/(1 - x^3 - x^4 - 2*x^5 - x^7).

---
+++ 397/397236/397236_01.rb
@@ -0,0 +1,9 @@
+# a(n) = a(n-3) + a(n-4) + 2*a(n-5) + a(n-7).
+def a(n) 
+  ary = [1, 0, 0, 1, 1, 2, 1]
+  (7..n).each{|i| ary << ary[i-3] + ary[i-4] + 2*ary[i-5] + ary[i-7]}
+  ary
+end
+
+n = 47
+p a(n)
