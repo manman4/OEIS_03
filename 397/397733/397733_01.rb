3172c46423ed1fef8aa01ab42ecdc83a216fb629
manman4 <7023566+manman4@users.noreply.github.com>
2026-07-08 18:56:32 +0900

    a(n) = Sum_{k=1..n} gcd(k^5,n).

---
+++ 397/397733/397733_01.rb
@@ -0,0 +1,29 @@
+def a_number(n, s, t)
+  return 0 if n <= 0
+
+  nt = n**t
+  total = 0
+  1.upto(n){|k|
+    total += (k**s).gcd(nt)
+  }
+  total
+end
+
+def sequence(limit, s, t)
+  (0..limit).map{|n| a_number(n, s, t)}
+end
+
+if __FILE__ == $PROGRAM_NAME
+  limit = (ARGV[0] || 10000).to_i
+  s = (ARGV[1] || 5).to_i
+  t = (ARGV[2] || 1).to_i
+
+  ary = sequence(limit, s, t)
+  (1..limit).each{|n|
+    j = ary[n]
+    break if j.to_s.size > 1000
+    print n
+    print ' '
+    puts j
+  }
+end
