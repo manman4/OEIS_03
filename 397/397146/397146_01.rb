f6b5d379f6089fe53ea45026f612bbc5874b6e2d
manman4 <7023566+manman4@users.noreply.github.com>
2026-07-15 22:29:57 +0900

    G.f. A(x), with A(0) = 0, satisfies A(x-x^2) = x + A(x)^4.

---
+++ 397/397146/397146_01.rb
@@ -0,0 +1,32 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(1) = 1; a(n) = Sum_{i, j, k, l>=1 and i+j+k+l=n} a(i)*a(j)*a(k)*a(l) + Sum_{r=1..floor(n/2)} (-1)^(r+1) * binomial(n-r,r) * a(n-r).
+def a(n)
+  ary = [0, 1]
+  (2..n).each{|m|
+    sum = 0
+    (1..m-3).each{|i|
+      (1..m-2-i).each{|j|
+        (1..m-1-i-j).each{|k|
+          l = m - i - j - k
+          sum += ary[i] * ary[j] * ary[k] * ary[l]
+        }
+      } 
+    }
+    ary << (1..m / 2).inject(sum){|s, r| s + (-1) ** (r + 1) * ncr(m - r, r) * ary[m - r]}
+  }
+  ary
+end
+
+n = 40
+p ary = a(n)
+(1..n).each{|i| 
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
