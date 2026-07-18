a057ea0c311c421485ee3bc5f3986d14363c0347
manman4 <7023566+manman4@users.noreply.github.com>
2026-07-18 20:34:45 +0900

    G.f. A(x) satisfies (1-3*x)*A(x) + A(x^4) = x.

--- 398/398036/398036_01.rb
+++ 398/398036/398036_01.rb
@@ -1,22 +1,16 @@
 # a(n) = 3^(n-1) - Sum_{k=1..floor(n/4)} 3^(n-4*k) * a(k).
-def A(n)
-  ary = [0]
+def A398036(n)
+  a = [0]
   (1..n).each{|i|
-    s = 3 ** (i - 1)
-    (1..(i / 4)).each{|k|
-      s -= 3 ** (i - 4 * k) * ary[k]
-    }
-    ary << s
-  }
-  ary
+  a << (1..i / 4).inject(3 ** (i - 1)){|s, k| s - 3 ** (i - 4 * k) * a[k]}}
+  a[1..-1]
 end
 
-n = 50
-p ary = A(n)
-(1..n).each{|i|
-  j = ary[i]
-  break if j.to_s.size > 1000
-  print i
-  print ' '
-  puts j
-}
+p A398036(30)
+# (1..n).each{|i|
+#   j = ary[i]
+#   break if j.to_s.size > 1000
+#   print i
+#   print ' '
+#   puts j
+# }
