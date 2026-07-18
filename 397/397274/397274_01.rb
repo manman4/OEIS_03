33ac6dd75f64aebc51795293292aa6f9d87e65fd
manman4 <7023566+manman4@users.noreply.github.com>
2026-07-18 16:40:49 +0900

    G.f. A(x) satisfies (1-3*x)*A(x) + A(x^3) = x.

---
+++ 397/397274/397274_01.rb
@@ -0,0 +1,22 @@
+# a(n) = 3^(n-1) - Sum_{k=1..floor(n/3)} 3^(n-3*k) * a(k).
+def A(n)
+  ary = [0]
+  (1..n).each{|i|
+    s = 3 ** (i - 1)
+    (1..(i / 3)).each{|k|
+      s -= 3 ** (i - 3 * k) * ary[k]
+    }
+    ary << s
+  }
+  ary
+end
+
+n = 50
+p ary = A(n)
+(1..n).each{|i|
+  j = ary[i]
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
