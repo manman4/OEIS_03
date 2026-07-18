ffef559fc654a8c858873cca9c5d523c274f1488
manman4 <7023566+manman4@users.noreply.github.com>
2026-07-18 20:30:43 +0900

    G.f. A(x) satisfies (1-2*x)*A(x) + A(x^4) = x.

--- 397/397213/397213_01.rb
+++ 397/397213/397213_01.rb
@@ -1,22 +1,16 @@
-# a(n) = 2^(n-1) - Sum_{k=1..floor(n/4)} 2^(n-4*k) * a(k).
-def A(n)
-  ary = [0]
+# a(n) = 2^(n-1) - Sum_{k=1..floor(n/3)} 2^(n-3*k) * a(k).
+def A397064(n)
+  a = [0]
   (1..n).each{|i|
-    s = 2 ** (i - 1)
-    (1..(i / 4)).each{|k|
-      s -= 2 ** (i - 4 * k) * ary[k]
-    }
-    ary << s
-  }
-  ary
+  a << (1..i / 3).inject(2 ** (i - 1)){|s, k| s - 2 ** (i - 3 * k) * a[k]}}
+  a[1..-1]
 end
 
-n = 50
-p ary = A(n)
-(1..n).each{|i|
-  j = ary[i]
-  break if j.to_s.size > 1000
-  print i
-  print ' '
-  puts j
-}
+p A397064(40)
+# (1..n).each{|i|
+#   j = ary[i]
+#   break if j.to_s.size > 1000
+#   print i
+#   print ' '
+#   puts j
+# }
