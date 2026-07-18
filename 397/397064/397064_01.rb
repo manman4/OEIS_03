630c6c430eb1d9f0039aba886495deb465cc0805
manman4 <7023566+manman4@users.noreply.github.com>
2026-07-18 20:24:51 +0900

    G.f. A(x) satisfies (1-2*x)*A(x) + A(x^3) = x.

--- 397/397064/397064_01.rb
+++ 397/397064/397064_01.rb
@@ -1,22 +1,15 @@
 # a(n) = 2^(n-1) - Sum_{k=1..floor(n/3)} 2^(n-3*k) * a(k).
 def A397064(n)
   ary = [0]
-  (1..n).each{|i|
-    s = 2 ** (i - 1)
-    (1..(i / 3)).each{|k|
-      s -= 2 ** (i - 3 * k) * ary[k]
-    }
-    ary << s
-  }
-  ary
+  (1..n).each{|i| 
+  ary << (1..i / 3).inject(2 ** (i - 1)){|s, k| s - 2 ** (i - 3 * k) * ary[k]}}
+  ary[1..-1]
 end
-
-n = 50
-p ary = A397064(n)
-(1..n).each{|i|
-  j = ary[i]
-  break if j.to_s.size > 1000
-  print i
-  print ' '
-  puts j
-}
+p A397064(40)
+# (1..n).each{|i|
+#   j = ary[i]
+#   break if j.to_s.size > 1000
+#   print i
+#   print ' '
+#   puts j
+# }
