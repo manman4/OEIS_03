579a679f1aaec5283caff881aadc6403d4b68c80
manman4 <7023566+manman4@users.noreply.github.com>
2026-07-18 20:29:53 +0900

    G.f. A(x) satisfies (1-3*x)*A(x) + A(x^3) = x.

--- 397/397274/397274_01.rb
+++ 397/397274/397274_01.rb
@@ -1,12 +1,12 @@
-# a(n) = 2^(n-1) - Sum_{k=1..floor(n/3)} 2^(n-3*k) * a(k).
-def A397064(n)
+# a(n) = 3^(n-1) - Sum_{k=1..floor(n/3)} 3^(n-3*k) * a(k).
+def A397274(n)
   a = [0]
   (1..n).each{|i|
-  a << (1..i / 3).inject(2 ** (i - 1)){|s, k| s - 2 ** (i - 3 * k) * a[k]}}
+  a << (1..i / 3).inject(3 ** (i - 1)){|s, k| s - 3 ** (i - 3 * k) * a[k]}}
   a[1..-1]
 end
 
-p A397064(40)
+p A397274(30)
 # (1..n).each{|i|
 #   j = ary[i]
 #   break if j.to_s.size > 1000
