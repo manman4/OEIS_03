cd522906c2d0045218dafea86a4ac47bf2e67c51
manman4 <7023566+manman4@users.noreply.github.com>
2026-08-25 17:01:06 +0900

    Number of nonnegative integer solutions to the equation x^2 + 8*y^2 = n.

---
+++ 399/399266/399266_01.rb
@@ -0,0 +1,25 @@
+require 'prime'
+
+# Number of nonnegative integer solutions to the equation x^2 + 4*y^2 = n.
+def A(n)
+  m = Math.sqrt(n).to_i + 1
+  ary = Array.new(n + 1, 0)
+  (0..m).each{|i|
+    (0..m).each{|j|
+      k = i * i + 8 * j * j
+      if k <= n
+        ary[k] += 1
+      end
+    }
+  }
+  ary
+end
+
+n = 10000
+ary = A(n)
+(0..n).each{|i|
+  print i
+  print ' '
+  puts ary[i]
+}
+ 
