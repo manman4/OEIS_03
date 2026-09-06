640db072b43ec1359ec60c3723f38a55de57d437
manman4 <7023566+manman4@users.noreply.github.com>
2026-09-06 11:21:21 +0900

    Expansion of g^7/(5-4*g)^4, where g = 1+x*g^5 is the g.f. of A002294.

---
+++ 399/399643/399643_01.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = ((n+1)/2) * (binomial(5*n+4,n) + Sum_{k=0..n} 4^(n-k) * binomial(5*n+5,k)).
+def A(n)
+  (0..n).inject(ncr(5 * n + 4, n)){|sum, k| sum + 4**(n-k) * ncr(5*n+5, k) } * (n + 1) / 2
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
