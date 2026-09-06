05c0a3bcba06363dd784ad40d65de0358d55177d
manman4 <7023566+manman4@users.noreply.github.com>
2026-09-06 11:17:43 +0900

    Expansion of g^8/(6-5*g)^4, where g = 1+x*g^6 is the g.f. of A002295.

---
+++ 399/399644/399644_02.rb
@@ -0,0 +1,18 @@
+def ncr(n, r)
+  return 1 if r == 0
+  (n - r + 1..n).inject(:*) / (1..r).inject(:*)
+end
+
+# a(n) = ((n+1)/12) * Sum_{k=0..n+1} 5^(n+1-k) * binomial(6*n+7,k).
+def A(n)
+  (0..n+1).inject(0){|sum, k| sum + 5**(n+1-k) * ncr(6*n+7, k) } * (n + 1) / 12
+end
+
+n = 1000
+(0..n).each{|i|
+  j = A(i)
+  break if j.to_s.size > 1000
+  print i
+  print ' '
+  puts j
+}
