f2a31f2821c465f1e5d87cf4fd79113001bfed15
manman4 <7023566+manman4@users.noreply.github.com>
2026-09-09 13:08:03 +0900

    Maximum number of integers having exactly one representation as s*t, with s in S and t in T, over all subsets S, T of {1, ..., n}.

--- 399/399711/399711_01.rb
+++ 399/399711/399711_01.rb
@@ -1,14 +1,14 @@
 def max_f(n)
-  subs = (1...(1 << n)).map { |m| (1..n).select { |v| m[v - 1] == 1 } }
+  subs = (1...(1 << n)).map{|m| (1..n).select{|v| m[v - 1] == 1}}
   best = [0, [], []]
-  subs.each_with_index do |a, i|
-    subs[i..].each do |b|                    
+  subs.each_with_index{|a, i|
+    subs[i..].each{|b|                    
       c = Hash.new(0)
-      a.each { |x| b.each { |y| c[x * y] += 1 } }
-      f = c.count { |_, v| v == 1 }
+      a.each{|x| b.each{|y| c[x * y] += 1}}
+      f = c.count{|_, v| v == 1}
       best = [f, a, b] if f > best[0]
-    end
-  end
+    }
+  }
   best
 end
 
