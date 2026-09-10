782f4e8ad336823ea2a5f2e9660794421adf7746
manman4 <7023566+manman4@users.noreply.github.com>
2026-09-10 15:59:33 +0900

    Maximum, over all subsets S,T of {1,...,n}, of the number of positive integers m for which m = s*t has exactly one solution (s,t) in S X T.

--- 399/399711/399711_01.rb
+++ 399/399711/399711_01.rb
@@ -1,17 +1,19 @@
 def max_f(n)
-  subs = (1..(1 << n) - 1).map{|m| (1..n).select{|v| m[v - 1] == 1}}
+  subs = (1..(1 << n) - 1).map{|a| (1..n).select{|v| a[v - 1] == 1}}
   best = [0, [], []]
   subs.each_with_index{|s, i|
     subs[i..-1].each{|t|
-      u = Hash.new(0)
-      s.each{|x| t.each{|y| u[x * y] += 1}}
-      f = u.values.count(1)
+      r = Hash.new(0)
+      s.each{|x| t.each{|y| r[x * y] += 1}}
+      f = r.values.count(1)
       best = [f, s, t] if f > best[0]
     }
   }
   best
 end
+
 def A399711(n)
-  (1..n).map{|k| max_f(k)[0]}
+  (1..n).map{|i| max_f(i)[0]}
 end
+
 p A399711(10)
